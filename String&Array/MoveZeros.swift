c4ccfb5d9c7fe4eb174febc5bc82b62212e39cfd
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-02-03 23:34:26 +0900

    feat : MoveZeros 문제 풀이

---
+++ String&Array/MoveZeros.swift
@@ -0,0 +1,31 @@
+//
+//  MoveZeros.swift
+//  Question Link: https://leetcode.com/problems/move-zeroes/
+//  Primary idea:  데이타 [int]를 for문으로 모두 순회하기
+//
+//  Time Complexity: O(n)
+//  Space Complexity : O(1) --> 상수는 하나만 사용했으니까
+//  Created by gunhyeong on 2020/02/03.
+//
+
+import Foundation
+
+class MoveZeros {
+    func moveZeros(nums: [Int]) -> [Int] {
+        var currentIndex = 0;
+        var index = 0
+        
+        while index != nums.endIndex {
+            if nums[index] != 0 {
+                nums[currentIndex] = nums[index];
+                currentIndex += 1
+            }
+            index += 1
+        }
+        
+        while (currentIndex < nums.endIndex) {
+            nums[currentIndex] = 0;
+            currentIndex += 1;
+        }
+    }
+}
