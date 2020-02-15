e16ebc9cfd80343db8fe80d89e6d062c3a18d3a6
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-02-15 15:34:04 +0900

    fix : Perfect Squares.swift 다른 방법(DP)으로 문제 풀이
    결과적으로 Time Limit Exceeded!
    Input의 값만큼의 size를 가진 배열을 선언 후, 순차적으로 채워가는 식으로 풀어갔지만
    Time Limit Exceeded가 되었다.
    
    # resolve : #48 ~ # 65

--- Dynamic/Perfect Squares.swift
+++ Dynamic/Perfect Squares.swift
@@ -2,7 +2,8 @@
 //  Perfect Squares.swift
 //  Question Link: https://leetcode.com/problems/perfect-squares/
 //  Primary idea:   1. Lagrange's four-square theorem을 이용한다. (즉 8로 나누었을때, 나머지가 7인 수는 모두 4개) n = 4^k(8k+7)
-//
+//                  2. dp를 이용, 배열에 해당에 하는 Index를 대입한다.
+
 //  Time Complexity :
 //  Space Complexity :
 //
@@ -13,12 +14,22 @@ import Foundation
 
 class Solution {
     func numSquares(_ n: Int) -> Int {
+        var n = n
+        var result: Int
+//        result = LagrangeFourSquareTheorem(&n)
+//        result = _DP(&n)
+        
+        return result
+    }
+    
+    ///Mark. 4ms
+    func LagrangeFourSquareTheorem(_ n: inout Int) -> Int {
         guard n >= 4 else {
             return n
         }
         
         var n = n
-        while (n % 4 == 0) { // Lagrange's four-square theorem
+        while (n % 4 == 0) {
             n = n/4
         }
         
@@ -33,5 +44,23 @@ class Solution {
             }
         }
         return 3
+    }
+    
+    /// Mark. Time Limit Exceeded
+    func _DP(_ n: inout Int) -> Int {
+        guard n >= 4 else {
+            return n
+        }
+        
+        var result = [Int](repeating: INTPTR_MAX, count: n+1)
+        result[0] = 0
+        
+        for i in 1..<n+1{
+            for j in 1..<n where j*j<=i {
+                result[i] = min(result[i], result[i-j*j]+1)
+            }
+        }
+        
+        return result.last!
     }
 }
