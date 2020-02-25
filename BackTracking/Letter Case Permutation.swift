b14c3e9243812aa379e4de3c554385d99341df87
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-02-25 20:58:07 +0900

    fix : Letter Case Permutation(DFS) 문제 풀이
    Primary idea
    1. DFS방식의 stack을 이용한다.
     2. 문자한개를 추출하여 숫자인지 문자인지 isNumber를 통해 판단
    3. 다음 문자를 upper와 lower를 통해 재귀함수로 돌림
    
    이 문제는 Backtracking으로 풀어야하지만 DFS문제로 풀어보았다.

--- BackTracking/Letter Case Permutation.swift
+++ BackTracking/Letter Case Permutation.swift
@@ -2,6 +2,8 @@
 //  Letter Case Permutation.swift
 //  Question Link: https://leetcode.com/problems/letter-case-permutation/
 //  Primary idea:   1. DFS방식의 stack을 이용한다.
+//                  2. 문자한개를 추출하여 숫자인지 문자인지 isNumber를 통해 판단
+//                  3. 다음 문자를 upper와 lower를 통해 재귀함수로 돌림
 //
 
 //  Time Complexity :
@@ -12,6 +14,34 @@
 
 class Solution {
     func letterCasePermutation(_ S: String) -> [String] {
+        var inputString = Array(S)
+        guard inputString.count >= 0 else {
+            return []
+        }
         
+        var result = [String]()
+    
+        dfsSolution(&inputString, &result, 0)
+        
+        return result
+    }
+    
+    private func dfsSolution(_ input: inout [Character], _ result: inout [String], _ currentIndex: Int) {
+        if input.count == currentIndex { // end 조건
+            result.append(String(input))
+            return
+        }
+        
+        let character = input[currentIndex]
+        
+        if character.isNumber { // 숫자인지 아닌지 판별 -> guard
+            dfsSolution(&input, &result, currentIndex+1)
+            return
+        }
+        
+        input[currentIndex] = Character(input[currentIndex].uppercased())
+        dfsSolution(&input, &result, currentIndex+1) // 다음 upper 문자 판단
+        input[currentIndex] = Character(input[currentIndex].lowercased())
+        dfsSolution(&input, &result, currentIndex+1) // 다음 lower 문자 판단
     }
 }
