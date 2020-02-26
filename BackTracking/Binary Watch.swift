cdf75816636ed64819e7d349fa63ff49bd52bf53
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-02-26 22:50:46 +0900

    feat : Binary Watch 문제 풀이(backtracking)

---
+++ BackTracking/Binary Watch.swift
@@ -0,0 +1,23 @@
+//
+//  Binary Watch.swift
+//  Question Link: https://leetcode.com/problems/letter-case-permutation/
+//  Primary idea:   <BackTracking>
+//                  1. 문자한개를 추출하여 숫자인지 문자인지 isNumber를 통해 판단
+//                  2. 다음 문자를 upper와 lower를 통해 재귀함수로 돌림
+//                  3. 조건에 맞을 때까지 계속 전진
+//                  4. 더이상 나아갈 길이 없으면 한칸 후퇴
+//                  5. 계속 반복
+
+//  Time Complexity :
+//  Space Complexity :
+//
+//  Created by gunhyeong on 2020/02/26.
+//
+
+import Foundation
+
+class Solution {
+    func readBinaryWatch(_ num: Int) -> [String] {
+        
+    }
+}
