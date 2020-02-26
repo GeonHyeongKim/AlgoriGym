1d79250ba89d13aa2c1f6fdf51da51dd57812af2
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-02-26 22:50:09 +0900

    fix ; Primary idea 수정

--- BackTracking/Letter Case Permutation.swift
+++ BackTracking/Letter Case Permutation.swift
@@ -1,10 +1,12 @@
 //
 //  Letter Case Permutation.swift
 //  Question Link: https://leetcode.com/problems/letter-case-permutation/
-//  Primary idea:   1. DFS방식의 stack을 이용한다.
-//                  2. 문자한개를 추출하여 숫자인지 문자인지 isNumber를 통해 판단
-//                  3. 다음 문자를 upper와 lower를 통해 재귀함수로 돌림
-//
+//  Primary idea:   <BackTracking>
+//                  1. 문자한개를 추출하여 숫자인지 문자인지 isNumber를 통해 판단
+//                  2. 다음 문자를 upper와 lower를 통해 재귀함수로 돌림
+//                  3. 조건에 맞을 때까지 계속 전진
+//                  4. 더이상 나아갈 길이 없으면 한칸 후퇴
+//                  5. 계속 반복
 
 //  Time Complexity :
 //  Space Complexity :
