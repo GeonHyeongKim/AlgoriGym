ae7462885fdf0c784d330a345b9d175e0b130965
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-02-27 17:45:14 +0900

    feat : Letter Tile Possibilities 문제 풀이 (Backtracking)

---
+++ BackTracking/Letter Tile Possibilities.swift
@@ -0,0 +1,21 @@
+//
+//  Letter Tile Possibilities.swift
+//  Question Link: https://leetcode.com/problems/letter-tile-possibilities/
+//  Primary idea:   <BackTracking>
+//                  1. 기본적으로 시간과 분을 함수로 파라미터로 보낸 뒤 계수를 반환한다.
+//                  2. String(Int, radix: 진수) 함수를 이용해 각 숫자(시 분)의 계수를 구한다.
+//                  3. Array의 filter를 이용하여 1인 것만 다시 배열에 집어 넣는다.
+
+//  Time Complexity :
+//  Space Complexity :
+//
+//  Created by gunhyeong on 2020/02/27.
+//
+
+import Foundation
+
+class Solution {
+    func numTilePossibilities(_ tiles: String) -> Int {
+        
+    }
+}
