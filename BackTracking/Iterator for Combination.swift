b709ed15fd0747c2aea4cf789b2eafb53231078a
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-02-28 17:35:24 +0900

    feat : Iterator for Combination 문제 풀이 (Backtacking)

---
+++ BackTracking/Iterator for Combination.swift
@@ -0,0 +1,36 @@
+//
+//  Iterator for Combination.swift
+//  Question Link: https://leetcode.com/problems/iterator-for-combination/
+//  Primary idea:   <BackTracking>
+//                  1.
+
+
+//  Runtime :
+//  Memory :
+//
+//  Created by gunhyeong on 2020/02/28.
+//
+
+import Foundation
+
+class CombinationIterator {
+
+    init(_ characters: String, _ combinationLength: Int) {
+        
+    }
+    
+    func next() -> String {
+        
+    }
+    
+    func hasNext() -> Bool {
+        
+    }
+}
+
+/**
+ * Your CombinationIterator object will be instantiated and called as such:
+ * let obj = CombinationIterator(characters, combinationLength)
+ * let ret_1: String = obj.next()
+ * let ret_2: Bool = obj.hasNext()
+ */
