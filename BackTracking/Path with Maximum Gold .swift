1af70ed8fcb529e18404c08bfd021348331fb8da
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-02-29 19:54:15 +0900

    fix : Runtime Error
    한번 갔던 곳에 다시 접근한다. ex) 6 : 6 -> 8 -> 6 -> 8 -> 6 -> 8 -> 6 -> ...
    1. temp에 값을 잠깐 보관하고, 해당값을 0으로 변경
    2. 그럴려면 input값인 grid를 새롭게 변수 선언
    3. 새로운 max값을 변수로 잡는다.
    
    resolves : #47, #58
    see : #27, #44, #45, #52 ~ # 55, #68, #63 ~ # 64

--- BackTracking/Path with Maximum Gold .swift
+++ BackTracking/Path with Maximum Gold .swift
@@ -5,8 +5,8 @@
 //                  1. guard를 통해 grid의 기본적인 nil 체크 및 dfs로 grid의 크기를 벗어났는지를 check
 //                  2. 현재 grid[i][j]를 sum 변수에 누적해서 더하고 dfs로 다시 재귀
 
-//  Runtime :
-//  Memory :
+//  Runtime : 844 ms
+//  Memory : 21 MB
 //
 //  Created by gunhyeong on 2020/02/29.
 //
@@ -24,6 +24,7 @@ class Solution {
             return 0
         }
         
+        var grid = grid
         var result = 0 // 결과를 닶을 변수
         row = grid.count
         col = grid[0].count
@@ -31,11 +32,8 @@ class Solution {
         for i in 0..<row {
             for j in 0..<col {
                 if grid[i][j] != 0{ // 0이 아닌곳을 start로 지정\
-                    print("\(grid[i][j]) -> ", terminator :" ")
-                    let sum = dfs(grid, i, j, 0)
+                    let sum = dfs(&grid, i, j, 0)
                     result = max(result, sum)
-                    print(": \(result)", terminator :" ")
-                    print("\n")
                 }
             }
         }
@@ -43,21 +41,26 @@ class Solution {
         return result
     }
     
-    func dfs(_ grid: [[Int]], _ i: Int, _ j: Int, _ sum: Int) -> Int{
-        guard i >= 0  && i < row && j >= 0 && j < col else { // grid의 크기를 벗어날 경우
+    func dfs(_ grid: inout [[Int]], _ i: Int, _ j: Int, _ sum: Int) -> Int{
+        guard i >= 0 && i < row && j >= 0 && j < col && grid[i][j] != 0 else { // grid의 크기를 벗어날 경우
             return sum // 지금까지의 합을 반환한다.
         }
-        print("\(grid[i][j]) -> ", terminator :" ")
+        
         var sum = sum
         sum += grid[i][j] // 현재 grid[i][j] 값을 sum에 누적
         
+        let temp = grid[i][j] // 다시 갔던곳을 다시 다시 않음
+        grid[i][j] = 0
+        
+        var maxValue = 0
         for dir in dirs {
             let dx = i + dir[0]
             let dy = j + dir[1]
             
-            sum = max(sum, dfs(grid, dx, dy, sum))
+            maxValue = max(maxValue, dfs(&grid, dx, dy, sum))
         }
         
-        return sum
+        grid[i][j] = temp
+        return maxValue
     }
 }
