26d2a8427f1c3068a52de1c79789026f04f99d41
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-02-29 19:05:00 +0900

    fix : Path with Maximum Gold 문제 풀이 (기본적인 문제 format 형성)
    Primary idea
    1. guard를 통해 grid의 기본적인 nil 체크 및 dfs로 grid의 크기를 벗어났는지를 check
    
    resolves : #17 ~ # 50

--- BackTracking/Path with Maximum Gold .swift
+++ BackTracking/Path with Maximum Gold .swift
@@ -2,7 +2,7 @@
 //  Path with Maximum Gold .swift
 //  Question Link: https://leetcode.com/problems/path-with-maximum-gold/
 //  Primary idea:   <BackTracking>
-//                  1. 
+//                  1. guard를 통해 grid의 기본적인 nil 체크 및 dfs로 grid의 크기를 벗어났는지를 check
 
 
 //  Runtime :
@@ -14,7 +14,39 @@
 import Foundation
 
 class Solution {
+    
+    let dir = [[-1,0], [1,0], [0,1], [0,-1]]
+    var row: Int = 0
+    var col: Int = 0
+    
     func getMaximumGold(_ grid: [[Int]]) -> Int {
+        guard grid.count > 0 || grid[0].count > 0 else { // input value에 예외처리
+            return 0
+        }
         
+        var result = 0 // 결과를 닶을 변수
+        row = grid.count
+        col = grid[0].count
+
+        for i in 0..<row {
+            for j in 0..<col {
+                if grid[i][j] != 0{ // 0이 아닌곳을 start로 지정
+                    let sum = dfs(grid, i, j, 0)
+                    result = max(result, sum)
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    func dfs(_ grid: [[Int]], _ i: Int, _ j: Int, _ sum: Int) -> Int{
+        guard i > 0  && i < row && j > 0 && j < col else { // grid의 크기를 벗어날 경우
+            return sum // 지금까지의 합을 반환한다.
+        }
+        
+        
+        
+        return 0
     }
 }
