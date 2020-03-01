29371171b9570087be56dd541d5e5c21a87a6bc1
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-01 16:09:10 +0900

    feat : Permutations 문제 풀이 시작 (BackTracking)

---
+++ BackTracking/Permutations.swift
@@ -0,0 +1,23 @@
+//
+//  Permutations.swift
+//  Question Link: https://leetcode.com/problems/permutations/
+//  Primary idea:   <BackTracking>
+//                  1.
+
+//  Runtime :
+//  Memory :
+//
+//  Created by gunhyeong on 2020/03/01.
+//
+
+import Foundation
+
+class Solution {
+    func permute(_ nums: [Int]) -> [[Int]] {
+        var result = [[Int]]()
+
+        
+        return result
+    }
+
+}
