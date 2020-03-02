a21a5edeab76360c1d9bc30e9f9a092fc0bf49ac
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-02 19:18:08 +0900

    feat : Generate Parentheses 문제 풀이 (BackTracking)

---
+++ BackTracking/Generate Parentheses.swift
@@ -0,0 +1,19 @@
+//
+//  Generate Parentheses.swift
+//  Question Link: https://leetcode.com/problems/generate-parentheses/
+//  Primary idea:   <BackTracking>
+//                  1.
+
+//  Runtime :
+//  Memory :
+//
+//  Created by gunhyeong on 2020/03/02.
+//
+
+import Foundation
+
+class Solution {
+    func generateParenthesis(_ n: Int) -> [String] {
+        
+    }
+}
