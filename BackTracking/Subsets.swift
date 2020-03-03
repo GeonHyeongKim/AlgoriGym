c8589b23de5092812679291ff7c56ee81a940cbe
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-03 13:41:36 +0900

    fix : Subsets 문제 풀이 (결과를 담기, for문으로 저장하고 탈출하기)
    Primary idea:   <BackTracking>
    1. 결과를 담기
    2. for 저장, 탈출

--- BackTracking/Subsets.swift
+++ BackTracking/Subsets.swift
@@ -2,7 +2,8 @@
 //  Subsets.swift
 //  Question Link: https://leetcode.com/problems/subsets/
 //  Primary idea:   <BackTracking>
-//
+//                  1. 결과를 담기
+//                  2. for 저장, 탈출
 
 //  Runtime :
 //  Memory :
@@ -13,10 +14,29 @@
 import Foundation
 
 class Solution {
+    var result = [[Int]]()
+    var nums = [Int]()
+    
     func subsets(_ nums: [Int]) -> [[Int]] {
-        var result = [[Int]]()
-
+        guard nums.count > 0 else {
+            return result
+        }
+        
+        self.nums = nums.sorted(by : <)
+        var num = [Int]()
+        
+        backTracking(&num, 0)
         
         return result
+    }
+    
+    func backTracking(_ num: inout [Int], _ index: Int) {
+        result.append(num) // 1. 결과 담기
+        
+        for i in 0..<index { // 2. for 저장, 탈출
+            num.append(nums[i])
+            backTracking(&num, i+1)
+            num.removeLast()
+        }
     }
 }
