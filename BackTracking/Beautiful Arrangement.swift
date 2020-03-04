65b633ea8d6fc0b345327dd37157a420e2d0cc67
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-04 18:09:27 +0900

    fix : Beautiful Arrangement 문제 풀이 수정 3 (Time Limit Exceeded)
    Primary idea
    4 - 2) 배열에 접근을 최소화 ( ex) N크기의 배열을 제거)
    
    resolves : #21, #29, # 36

--- BackTracking/Beautiful Arrangement.swift
+++ BackTracking/Beautiful Arrangement.swift
@@ -7,6 +7,7 @@
 //                  3. branch : 계산하는 배열(arrangement)에 동일한 값을 가지고 있을때
 //                  4. N(input value)가 15일때, Time Limit Exceeded 발생
 //                      1) 결과값을 append하지 않고 Int로 선언하여 값을 증가시키기
+//                      2) 배열에 접근을 최소화 ( ex) N크기의 배열을 제거)
 
 //  Runtime :
 //  Memory :
@@ -17,7 +18,7 @@
 import Foundation
 
 class Solution {
-    var N: [Int] = []
+    var N = Int(0)
     var beautifulArrangmentCnt: Int = 0
     
     func countArrangement(_ N: Int) -> Int {
@@ -25,25 +26,25 @@ class Solution {
             return 0
         }
         
-        self.N = Array(1...N)
+        self.N = N
         var arrangement = [Int]()
         backTracking(&arrangement, 1) // 1. 기존의 방식
         return beautifulArrangmentCnt
     }
     
     func backTracking(_ arrangement: inout [Int], _ index: Int){
-        if arrangement.count == N.count { // 2. 종료조건
+        if arrangement.count == N { // 2. 종료조건
             beautifulArrangmentCnt += 1
             return
         }
         
-        for i in 0..<N.count {
-            if arrangement.contains(N[i]) { // 3. 건너띄기! 동일한 값을 가지고 있으면 계산 하지 않기
+        for i in 1...N {
+            if arrangement.contains(i) { // 3. 건너띄기! 동일한 값을 가지고 있으면 계산 하지 않기
                 continue
             }
             
-            if (index % N[i] == 0 || N[i] % index == 0) { // 4. 서로 나누어 떨어질때만
-                arrangement.append(N[i])
+            if (index % i == 0 || i % index == 0) { // 4. 서로 나누어 떨어질때만
+                arrangement.append(i)
                 backTracking(&arrangement, index+1)
                 arrangement.removeLast()
             }
