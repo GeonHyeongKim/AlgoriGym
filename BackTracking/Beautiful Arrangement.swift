1c8c50f5ef6425ee51bbf8179b1af15b9d0f4f89
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-04 17:50:28 +0900

    fix : Beautiful Arrangement 문제 풀이 수정 (Time Limit Exceeded 발생)
    input이 15일 때, 정답은 맞지만 시간 초과

--- BackTracking/Beautiful Arrangement.swift
+++ BackTracking/Beautiful Arrangement.swift
@@ -4,8 +4,8 @@
 //  Primary idea:   <BackTracking>
 //                  1.
 
-//  Runtime : 12 ms
-//  Memory : 21.3 MB
+//  Runtime :
+//  Memory :
 //
 //  Created by gunhyeong on 2020/03/04.
 //
@@ -13,13 +13,25 @@
 import Foundation
 
 class Solution {
-    var beautifulArrangments: [[String]] = 0
+    var beautifulArrangmentList: [[String]] = 0
     
     func countArrangement(_ N: Int) -> Int {
         guard N > 0 && N < 15 else { // N is a positive integer and will not exceed 15.
             return 0
         }
+        var arrangement: [Character](rpeating: 0, count: N)
+        backTracking(&arrangement, 0)
         
-        return beautifulArrangments.count
+        return beautifulArrangmentList.count
+    }
+    
+    func backTracking(_ arrangement: inout [Character], _ index: Int){
+        if index == N {
+            beautifulArrangmentList.append(arrangement)
+        }
+        
+        for i in 0..<N {
+            if (index%)
+        }
     }
 }
