a219e4ba7483fe4af5c2c97f1be8c646d580cb51
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-06 09:08:17 +0900

    feat : Combinations 문제 풀이 (BackTracking)

---
+++ BackTracking/Combinations.swift
@@ -0,0 +1,21 @@
+//
+//  Combinations.swift
+//  Question Link: https://leetcode.com/problems/combinations/
+//  Primary idea:   <BackTracking>
+//                  1.
+//
+//  Runtime:
+//  Memory Usage:
+//
+//  Created by gunhyeong on 2020/03/06.
+//
+
+import Foundation
+
+class Solution {
+    var result = [[Int]]()
+    
+    func combine(_ n: Int, _ k: Int) -> [[Int]] {
+
+    }
+}
