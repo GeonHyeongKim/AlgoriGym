1982ba4c6a32a1dfd15950780b5c3934f95ff070
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-06 09:27:37 +0900

    fix : Combinations 문제 풀이 (기본적인 Backtracking문제 풀이 포멧으로 품)
    Primary idea :   <BackTracking>
    1. 계산된 combination
    2. 저장 조건
    3. 반복문에서 upperIndex > lowerIndex를 방지

--- BackTracking/Combinations.swift
+++ BackTracking/Combinations.swift
@@ -2,8 +2,10 @@
 //  Combinations.swift
 //  Question Link: https://leetcode.com/problems/combinations/
 //  Primary idea:   <BackTracking>
-//                  1.
-//
+//                  1. 계산된 combination
+//                  2. 저장 조건
+//                  3. 반복문에서 upperIndex > lowerIndex를 방지
+
 //  Runtime:
 //  Memory Usage:
 //
@@ -14,8 +16,33 @@ import Foundation
 
 class Solution {
     var result = [[Int]]()
+    var n = Int()
+    var k = Int()
+    var permutation = [Int]()
     
     func combine(_ n: Int, _ k: Int) -> [[Int]] {
-
+        self.n = n
+        self.k = k
+        
+        var combination = [Int]() // 1. 계산된 combination
+        backTracking(&combination, 1)
+        return result
+    }
+    
+    func backTracking(_ combination: inout [Int], _ startIndex: Int){
+        if combination.count == k { // 2. 저장 조건
+            result.append(combination.sorted())
+            return
+        }
+        
+        if startIndex > n { // 3. 반복문에서 upperIndex > lowerIndex를 방지
+            return
+        }
+        
+        for i in startIndex...n {
+            combination.append(i)
+            backTracking(&combination, i+1)
+            combination.removeLast()
+        }
     }
 }
