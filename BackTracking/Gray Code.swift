05454bcdf0d4baca49a57094e6d5a78651af4b12
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-08 19:44:11 +0900

    feat : Gray Code 문제 (1. 공식, 2. Backtracking)
    1. 공식 : https://johngrib.github.io/wiki/gray-code/을 참고(오른쪽으로 비트 쉬프트 한 다음, 본래 값과 논리합을 하면 끝난다는 것)
    2. Backtracking

---
+++ BackTracking/Gray Code.swift
@@ -0,0 +1,26 @@
+//
+//  Gray Code.swift
+//  Question Link: https://leetcode.com/problems/gray-code/
+//  Primary idea:   <BackTracking>
+//                  1. 공식 : 'https://johngrib.github.io/wiki/gray-code/'을 참고
+//                      - 오른쪽으로 비트 쉬프트 한 다음, 본래 값과 논리합을 하면 끝난다는 것
+//                  2. backTracking
+
+//  Runtime:
+//  Memory Usage:
+//
+//  Created by gunhyeong on 2020/03/08.
+//
+
+import Foundation
+
+class Solution {
+    func grayCode(_ n: Int) -> [Int] {
+        guard n > 0 else {
+            return [0]
+        }
+        var result = [Int]()
+
+        return result
+    }
+}
