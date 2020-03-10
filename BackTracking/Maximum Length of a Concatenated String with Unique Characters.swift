e20fddb69e0a834bbb2b1396e27557a239313ecf
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-10 13:25:57 +0900

    feat : Maximum Length of a Concatenated String with Unique Characters 문제 (Backtracking)

---
+++ BackTracking/Maximum Length of a Concatenated String with Unique Characters.swift
@@ -0,0 +1,20 @@
+//
+//  Maximum Length of a Concatenated String with Unique Characters.swift
+//  Question Link: https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+//  Primary idea:   <BackTracking>
+//                  1.
+//
+
+//  Runtime:
+//  Memory Usage:
+//
+//  Created by gunhyeong on 2020/03/10.
+//
+
+import Foundation
+
+class Solution {
+    func maxLength(_ arr: [String]) -> Int {
+        
+    }
+}
