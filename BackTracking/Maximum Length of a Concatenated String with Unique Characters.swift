ba9e5ba375c967803a6bcdc652e7b6c315b132d0
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-10 14:37:45 +0900

    fix : Maximum Length of a Concatenated String with Unique Characters 문제 풀이 수정 (Runtime, Memory Usage 줄이기)
    문자 자체가 Unique한지 check 하는 부분을 임시 배열에서 제거하고 찾는것이아니라
    한개씩 포함시킨뒤 contains로 검사하기
    
    Runtime : 364 ms -> 96ms
    Memory Usage : 21.5 MB -> 21.3MB
    
    #resolves : #40, #42, #43, #53

--- BackTracking/Maximum Length of a Concatenated String with Unique Characters.swift
+++ BackTracking/Maximum Length of a Concatenated String with Unique Characters.swift
@@ -2,11 +2,15 @@
 //  Maximum Length of a Concatenated String with Unique Characters.swift
 //  Question Link: https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
 //  Primary idea:   <BackTracking>
-//                  1.
+//                  1. 이중 반복문을 통해 해결
+//                      1) 1번째 반복문 : 배열의 모든 값을 탐색
+//                      2) 2번째 반복문 : characters에 대해 spelling(Character) 하나하나를 탐색
+//                          * 1 : spelling(Character)가 최대 길이에 포함되어 있는지 check
+//                          * 2 : 문자 자체가 Unique한지 check
 //
 
-//  Runtime:
-//  Memory Usage:
+//  Runtime: 96 ms
+//  Memory Usage: 21.3 MB
 //
 //  Created by gunhyeong on 2020/03/10.
 //
@@ -33,25 +37,25 @@ class Solution {
     
     func backTracking(_ maxLen: String, _ startIndex: Int){
         for i in startIndex..<sortedArr.count {
-            var sortedCharacters = sortedArr[i].sorted()
+            var checkUniqueChar = ""
             
-            for character in sortedCharacters {
-                sortedCharacters.remove(at: sortedCharacters.firstIndex(of: character)!) // 첫 번째 character 제거
-                
-                if sortedCharacters.contains(character) { // character 자체가 unique지 check
+            for character in sortedArr[i] {
+                if checkUniqueChar.contains(character){ // 1-2)-2 : 문자 자체가 Unique한지 check
                     isContinue = true
                     continue
                 }
                 
-                if maxLen.contains(character) { // maxLenth에 포함되어 있지 않은 charater인지 check
+                if maxLen.contains(character) { //  1-2)-1 :maxLenth에 포함되어 있지 않은 charater인지 check
                     isContinue = true
                     continue
                 }
+                
+                checkUniqueChar += String(character)
             }
             
             if !isContinue {
                 let concatenations = maxLen+sortedArr[i]
-                maxCnt = max(maxCnt, concatenations.count) // 최댓값 저장
+                maxCnt = max(maxCnt, concatenations.count) // 최댓값
                 backTracking(concatenations, i + 1)
             }
             
@@ -59,3 +63,4 @@ class Solution {
         }
     }
 }
+
