0ca8de3c5abf86e0b0b1fb897173950ac0032843
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-10 14:29:12 +0900

    fix : Maximum Length of a Concatenated String with Unique Characters 문제 풀이 수정 (Primary idea)
    Primary idea
    1. isContinue(type : Bool)을 통해 계속 진행햐야하는지 확인
    2. maxLen(현재 최대 길이를 가진 String)에 character가 들어 있는지 확인
    
    Runtime : 368 ms, Memory Usage :21.5 MB

--- BackTracking/Maximum Length of a Concatenated String with Unique Characters.swift
+++ BackTracking/Maximum Length of a Concatenated String with Unique Characters.swift
@@ -14,7 +14,48 @@
 import Foundation
 
 class Solution {
+    var sortedArr = [String]()
+    var isContinue: Bool = false
+    var maxCnt = Int(0)
+    
     func maxLength(_ arr: [String]) -> Int {
-        
+        guard arr.count >= 1 && arr.count <= 26 else { // Contrains
+            return maxCnt
+        }
+        self.sortedArr = arr.sorted(by: { (a, b) -> Bool in // 정렬
+            a.count < b.count
+        })
+                
+        backTracking("", 0)
+                
+        return maxCnt
+    }
+    
+    func backTracking(_ maxLen: String, _ startIndex: Int){
+        for i in startIndex..<sortedArr.count {
+            var sortedCharacters = sortedArr[i].sorted()
+            
+            for character in sortedCharacters {
+                sortedCharacters.remove(at: sortedCharacters.firstIndex(of: character)!) // 첫 번째 character 제거
+                
+                if sortedCharacters.contains(character) { // character 자체가 unique지 check
+                    isContinue = true
+                    continue
+                }
+                
+                if maxLen.contains(character) { // maxLenth에 포함되어 있지 않은 charater인지 check
+                    isContinue = true
+                    continue
+                }
+            }
+            
+            if !isContinue {
+                let concatenations = maxLen+sortedArr[i]
+                maxCnt = max(maxCnt, concatenations.count) // 최댓값 저장
+                backTracking(concatenations, i + 1)
+            }
+            
+            isContinue = false
+        }
     }
 }
