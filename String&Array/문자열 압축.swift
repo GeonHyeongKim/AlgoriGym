6ada25cfa0516328fc2999025b81f4a5a9ba75bd
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-12 13:44:23 +0900

    feat : 문자열 자르기 문제 풀이 시작 (2020 카카오 공채)

---
+++ String&Array/문자열 압축.swift
@@ -0,0 +1,23 @@
+/*
+*  문자열 압축.swift
+*  Question Link: https://programmers.co.kr/learn/courses/30/lessons/60057
+*  Primary idea:   <Brute force>
+*                  1. 문자열을 다룰 수 있고, 아래 예시와 같이 문자열과 관련된 다양한 작업을 할 수 있는지 파악
+*                       1) 문자열 자르기
+*                       2) 부분 문자열 얻기
+*                       3) 문자열 비교하기
+*                       4) 문자열 길이 얻기
+*
+*  Time Complexity:
+*  Space Complexity:
+*  Runtime:  ms
+*  Memory Usage:  MB
+*
+*  Created by gunhyeong on 2020/03/12.
+*/
+
+import Foundation
+
+func solution(_ s:String) -> Int {
+    return 0
+}
