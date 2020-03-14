d0df2fdb16f338a0965deebaef7dedb6fa5b8b2e
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-14 22:26:17 +0900

    fix : 실패율 문제 풀이 수정 (점수 올리기) - 81.5점
    1) 분자 계산 - stage의 갯수
    2) 분모가 0일때 - if문으로 처리
    
    resolves : #40, #41~#47

--- String&Array/실패율.swift
+++ String&Array/실패율.swift
@@ -3,8 +3,8 @@
 *  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42889
 *  Primary idea:   <Brute force>
 *                  1. 실패율구하기
-*                      1) filter를 사용하여 분자 구하기 : stage - 1 < x < stage + 1
-*                      2) filter를 이용해서 분모 구하기 : stage이상일 경우
+*                      1) filter를 사용하여 분자 구하기 : stage의 갯수
+*                      2) filter를 이용해서 분모 구하기 : stage이상 갯수, 0일때 처리
 *                      3) key : stage, value : 분자 / 분모
 *                  2. 다중 정렬 - lambda식으로 정렬
 *                      1) value
@@ -37,10 +37,14 @@ func solution(_ N:Int, _ stages:[Int]) -> [Int] {
     var clear = [Int]()
     
     for stageCnt in 1...N {
-        notClear = sortedStages.filter{(($0 < stageCnt + 1) && ($0 > stageCnt - 1))} // 1-1. 분자(clear 못한사람)
+        notClear = sortedStages.filter{($0 == stageCnt)} // 1-1. 분자(clear 못한사람)
         clear = sortedStages.filter{($0 >= stageCnt)} // 1-2. 분모(clear 사람)
         
-        failureRateListByStage[stageCnt] = (Double(notClear.count) / Double(clear.count)) // 1-3. 실패율
+        if clear.count == 0 {
+            failureRateListByStage[stageCnt] = 0 // 분모가 0일때
+        } else {
+            failureRateListByStage[stageCnt] = (Double(notClear.count) / Double(clear.count)) // 1-3. 실패율
+        }
     }
     
     let sort = failureRateListByStage.sorted(by: { // 2. 다중 정렬 - lambda
@@ -56,3 +60,4 @@ func solution(_ N:Int, _ stages:[Int]) -> [Int] {
     
     return result.reversed()
 }
+
