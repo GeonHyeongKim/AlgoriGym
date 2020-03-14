121bfeab432cf1b5cbb55059f84a1edc936e263e
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-14 18:43:01 +0900

    feat : 실패율 문제 풀이 시작 (2019 카카오 공채)

---
+++ String&Array/실패율.swift
@@ -0,0 +1,20 @@
+/*
+*  실패율.swift
+*  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42889
+*  Primary idea:   <Brute force>
+*                  1.
+*
+*  Time Complexity:
+*  Space Complexity:
+*  result :
+*
+*  Created by gunhyeong on 2020/03/14.
+*/
+
+import Foundation
+
+import Foundation
+
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    return []
+}
