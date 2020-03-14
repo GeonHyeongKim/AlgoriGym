6f5e4f75eb2484ae76a201d1b549dd71f7dc19fd
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-14 16:36:56 +0900

    feat : 다트게임 문제 풀이 시작 (2018 카카오 공채)

---
+++ String&Array/다트게임.swift
@@ -0,0 +1,18 @@
+/*
+*  다트게임.swift
+*  Question Link: https://programmers.co.kr/learn/courses/30/lessons/17682
+*  Primary idea:   <Brute force>
+*                  1.
+*
+*  Time Complexity:
+*  Space Complexity:
+*  result :
+*
+*  Created by gunhyeong on 2020/03/14.
+*/
+
+import Foundation
+
+func solution(_ dartResult:String) -> Int {
+    return 0
+}
