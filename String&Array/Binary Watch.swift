5fdbf1431a422a58d70ac203406a2e323f2cb096
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-17 15:55:09 +0900

    finish : Binary Watch 문제 풀이 끝 (Runtime, Memory Usage, Time&Space Complexity)
    *  Time Complexity : O(n^2)
    *  Space Complexity : O(n)
    *  Runtime : 32ms
    *  Memory Usage : 21.1 MB

--- String&Array/Binary Watch.swift
+++ String&Array/Binary Watch.swift
@@ -1,16 +1,18 @@
-//
-//  Binary Watch.swift
-//  Question Link: https://leetcode.com/problems/binary-watch/
-//  Primary idea:   <BackTracking>
-//                  1. 기본적으로 시간과 분을 함수로 파라미터로 보낸 뒤 계수를 반환한다.
-//                  2. String(Int, radix: 진수) 함수를 이용해 각 숫자(시 분)의 계수를 구한다.
-//                  3. Array의 filter를 이용하여 1인 것만 다시 배열에 집어 넣는다.
-
-//  Time Complexity :
-//  Space Complexity :
-//
-//  Created by gunhyeong on 2020/02/26.
-//
+/*
+*  Binary Watch.swift
+*  Question Link: https://leetcode.com/problems/binary-watch/
+*  Primary idea:   <BackTracking>
+*                  1. 기본적으로 시간과 분을 함수로 파라미터로 보낸 뒤 계수를 반환한다.
+*                  2. String(Int, radix: 진수) 함수를 이용해 각 숫자(시 분)의 계수를 구한다.
+*                  3. Array의 filter를 이용하여 1인 것만 다시 배열에 집어 넣는다.
+*
+*  Time Complexity : O(n^2)
+*  Space Complexity : O(n)
+*  Runtime : 32ms
+*  Memory Usage : 21.1 MB
+*
+*  Created by gunhyeong on 2020/02/26.
+*/
 
 import Foundation
 
