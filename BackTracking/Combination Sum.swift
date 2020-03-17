45a4e808db389d33676d89dbde6fc3cb0b8240a4
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-17 16:07:23 +0900

    finish : Combination Sum 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    *  Time Complexity : O(n^n)
    *  Space Complexity : O(2^n - 1)
    *  Runtime: 164 ms
    *  Memory Usage: 21.2 MB

--- BackTracking/Combination Sum.swift
+++ BackTracking/Combination Sum.swift
@@ -1,15 +1,18 @@
-//
-//  Combination Sum.swift
-//  Question Link: https://leetcode.com/problems/combination-sum/
-//  Primary idea:   <BackTracking>
-//                  1. 계산된 candidate
-//                  2. 방금 포함된 candidate의 중복 허용 조건 - 반복문으로 startIndex가 i+1이 아닌 i이기 때문에 한계점을 정함
-//                  3. 결과값 저장 - 계산된 candidate의 합이 target과 같을 때
-//
-//  Runtime: 164 ms
-//  Memory Usage: 21.2 MB
-//  Created by gunhyeong on 2020/03/05.
-//
+/*
+*  Combination Sum.swift
+*  Question Link: https://leetcode.com/problems/combination-sum/
+*  Primary idea:   <BackTracking>
+*                  1. 계산된 candidate
+*                  2. 방금 포함된 candidate의 중복 허용 조건 - 반복문으로 startIndex가 i+1이 아닌 i이기 때문에 한계점을 정함
+*                  3. 결과값 저장 - 계산된 candidate의 합이 target과 같을 때
+*
+*  Time Complexity : O(n^n)
+*  Space Complexity : O(2^n - 1)
+*  Runtime: 164 ms
+*  Memory Usage: 21.2 MB
+*
+*  Created by gunhyeong on 2020/03/05.
+*/
 
 import Foundation
 
