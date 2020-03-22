6024b7be1e4a9abcca8588396489e0b9c00ec31f
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-22 23:07:34 +0900

    finish : Climbing Stairs 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    *  Time Complexity : O(n)
    *  Space Complexity : O(1)
    *  Runtime: 4 ms
    *  Memory Usage: 20.9 MB

--- Dynamic/ClimbStairs.swift
+++ Dynamic/ClimbStairs.swift
@@ -1,14 +1,15 @@
-//
-//  ClimbStairs.swift
-//  Question Link: https://leetcode.com/problems/climbing-stairs/
-//  Primary idea:   1. dp[3] = dp[2] + dp[1] --> dp[i] = dp[i-1] + dp[i-2]
-
-//
-//  Time Complexity:
-//  Space Complexity :
-//
-//  Created by gunhyeong on 2020/02/06.
-//
+/*
+*  ClimbStairs.swift
+*  Question Link: https://leetcode.com/problems/climbing-stairs/
+*  Primary idea:   1. dp[3] = dp[2] + dp[1] --> dp[i] = dp[i-1] + dp[i-2]
+*
+*  Time Complexity : O(n)
+*  Space Complexity : O(1)
+*  Runtime: 4 ms
+*  Memory Usage: 20.9 MB
+*
+*  Created by gunhyeong on 2020/02/06.
+*/
 
 import Foundation
 
@@ -20,7 +21,7 @@ class Solution {
             return 0
         }
         
-        if n == 1 {
+        if n == 0 || n == 1 {
             return 1
         }
         
@@ -32,7 +33,7 @@ class Solution {
         dp[1] = 1
         dp[2] = 2
     
-        for i in 3...dp.count {
+        for i in 3..<dp.count {
             dp[i] = dp[i-1] + dp[i-2]
         }
         
