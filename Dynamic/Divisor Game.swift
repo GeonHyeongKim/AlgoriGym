065b5c7d948345b2f09ffe20e8fb1b90a290cca7
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-23 16:59:55 +0900

    fix : Divisor Game 문제 풀이 끝 (Dynamic Programming)
    *  Primary idea:   <Dynamic Proramming>
    1. dp - index 0을 접근하지 않기 위해 N+1로 문제를 풀이 (top-down)
    2. 범위
    	1) n의 범위 : 1 <= N <= 1000, 즉 2부터 n을 포함한 자연수
    	2) x의 범위 : 0 < x < N
    3. 조건 - n % x == 0이면서 n - x의 값이 false인 경우

--- Dynamic/Divisor Game.swift
+++ Dynamic/Divisor Game.swift
@@ -1,10 +1,15 @@
 /*
 *  Divisor Game.swift
-*  Question Link: https://leetcode.com/problems/climbing-stairs/
-*  Primary idea:   1.
+*  Question Link: https://leetcode.com/problems/divisor-game/
+*  Primary idea:   <Dynamic Proramming>
+*                  1. dp - index 0을 접근하지 않기 위해 N+1로 문제를 풀이 (top-down)
+*                  2. 범위
+*                      1) n의 범위 : 1 <= N <= 1000, 즉 2부터 n을 포함한 자연수
+*                      2) x의 범위 : 0 < x < N
+*                  3. 조건 - n % x == 0이면서 n - x의 값이 false인 경우,
 *
-*  Time Complexity : O()
-*  Space Complexity : O()
+*  Time Complexity : O(n^2)
+*  Space Complexity : O(n)
 *  Runtime:  ms
 *  Memory Usage:  MB
 *
@@ -15,6 +20,16 @@ import Foundation
 
 class Solution {
     func divisorGame(_ N: Int) -> Bool {
+        var dp = [Bool](repeating: false, count: N+1) // 1. dp
+        for n in 2..<(N+1) { // 2-1. n의 범위
+            for x in 1..<n { // 2-2. x의 범위
+                if n%x == 0 && !dp[n-x]{ // 3. 조건
+                    dp[n] = true
+                    break
+                }
+            }
+        }
         
+        return dp[N]
     }
 }
