34510f7cb45660c263879552d500916a9bcb6c5f
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-23 17:41:41 +0900

    feat : Min Cost Climbing Stairs 문제 풀이 시작

---
+++ Dynamic/Min Cost Climbing Stairs.swift
@@ -0,0 +1,21 @@
+/*
+*  Min Cost Climbing Stairs.swift
+*  Question Link: https://leetcode.com/problems/min-cost-climbing-stairs/
+*  Primary idea:   <Dynamic Programming>
+*                  1.
+*
+*  Time Complexity : O()
+*  Space Complexity : O()
+*  Runtime:  ms
+*  Memory Usage:  MB
+*
+*  Created by gunhyeong on 2020/03/23.
+*/
+
+import Foundation
+
+class Solution {
+    func minCostClimbingStairs(_ cost: [Int]) -> Int {
+        
+    }
+}
