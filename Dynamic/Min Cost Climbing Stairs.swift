884d14edd6b14d6c2a39d7a530078996c0fd5905
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-23 18:29:55 +0900

    fix : Min Cost Climbing Stairs 문제 풀이 수정 (Dynamic Programminga>
    *  Primary idea:   <Dynamic Programming>
    1. dp : d[i] = min(d[i-1], d[i-2])
    2. 계산 하려는 현재 값

--- Dynamic/Min Cost Climbing Stairs.swift
+++ Dynamic/Min Cost Climbing Stairs.swift
@@ -2,7 +2,8 @@
 *  Min Cost Climbing Stairs.swift
 *  Question Link: https://leetcode.com/problems/min-cost-climbing-stairs/
 *  Primary idea:   <Dynamic Programming>
-*                  1.
+*                  1. dp : d[i] = min(d[i-1], d[i-2])
+*                  2. 계산 하려는 현재 값
 *
 *  Time Complexity : O()
 *  Space Complexity : O()
@@ -16,6 +17,15 @@ import Foundation
 
 class Solution {
     func minCostClimbingStairs(_ cost: [Int]) -> Int {
+        var minCost_dp = [Int](repeating: 0, count: cost.count + 1)
+        minCost_dp[0] = cost[0]
+        minCost_dp[1] = cost[1]
         
+        for i in 2..<cost.count+1 {
+            let currentCost = ( i == cost.count ) ? 0 : cost[i] // 2. 계산 하려는 현재 값
+            minCost_dp[i] = currentCost + min(minCost_dp[i-1], minCost_dp[i-2]) // 1. dp
+        }
+        
+        return minCost_dp[cost.count]
     }
 }
