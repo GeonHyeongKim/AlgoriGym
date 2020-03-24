a45404271107dc76543cdebf30e49727b9ca8619
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-24 16:34:42 +0900

    fix : Range Sum Query - Immutable 문제 풀이 (Brute Force)
    *  Primary idea:   <Brute Force>
    init() : input(nums)을 저장
    sumRange(i, j) : i~j까지 누적

--- Dynamic/Range Sum Query - Immutable.swift
+++ Dynamic/Range Sum Query - Immutable.swift
@@ -1,28 +1,36 @@
 /*
 *  Range Sum Query - Immutable.swift
 *  Question Link: https://leetcode.com/problems/range-sum-query-immutable/
-*  Primary idea:   <Dynamic Programming>
-*                  1.
-*
-*  Time Complexity : O()
-*  Space Complexity : O()
-*  Runtime:  ms
-*  Memory Usage:  MB
-*
+
 *  Created by gunhyeong on 2020/03/24.
 */
 
 import Foundation
 
+/*
+*  Primary idea:   <Brute Force>
+*                  1. init() : input(nums)을 저장
+*                  2. sumRange(i, j) : i~j까지 누적
+*
+*  Time Complexity : O(n)
+*  Space Complexity : O(1)
+*  Runtime: 1500 ms
+*  Memory Usage: 22.1 MB
+*/
+ 
 class NumArray {
-
+    let nums: [Int]
+    
     init(_ nums: [Int]) {
-        
+        self.nums = nums
     }
     
     func sumRange(_ i: Int, _ j: Int) -> Int {
-        
-    }
+        var sum = 0
+        for x in i...j {
+            sum += nums[x]
+        }
+        return sum
 }
 
 /**
