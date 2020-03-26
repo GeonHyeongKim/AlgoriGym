d9595ea5dcd4b8e82fb7f89d011d03481e71ae5f
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-27 00:11:46 +0900

    fix : House Robber 문제 풀이 수정 (Dynamic Programming)
    *  Primary idea:   <Dynamic Programming>
    1. dp
    2. 점화식 : 3개 이상일 경우부터 dp[i] = max(dp[i-2] + nums[i], dp[i-1]) 규칙

--- Dynamic/House Robber.swift
+++ Dynamic/House Robber.swift
@@ -1,7 +1,9 @@
 /*
 *  House Robber.swift
 *  Question Link: https://leetcode.com/problems/house-robber/
-*  Primary idea:   1.
+*  Primary idea:   <Dynamic Programming>
+*                  1. dp
+*                  2. 점화식 : 3개 이상일 경우부터 dp[i] = max(dp[i-2] + nums[i], dp[i-1]) 규칙
 *
 *  Time Complexity : O()
 *  Space Complexity : O()
@@ -15,6 +17,21 @@ import Foundation
 
 class Solution {
     func rob(_ nums: [Int]) -> Int {
+        guard nums.count > 0 else { 
+            return 0
+        }
         
+        if nums.count == 1 {
+            return nums[0]
+        }
+        
+        var dp = [Int](repeating: 0, count: nums.count) // 1. dp
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in 2..<nums.count {
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1]) // 2. 점화식
+        }
+        return dp[nums.count-1]
     }
 }
