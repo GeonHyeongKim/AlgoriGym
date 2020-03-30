b8d4650c2e387d156fa2de2952e9a9133b59ff5a
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-03-30 16:20:33 +0900

    fix : Counting Bits 문제 풀이 수정 (Brute force)
    *  Primary idea:   <Brute force>
    binary 표현 후, 1개의 bit씩을 계산
    
    #resolve : #17 ~ #37

--- Dynamic/Counting Bits.swift
+++ Dynamic/Counting Bits.swift
@@ -14,8 +14,26 @@
 
 import Foundation
 
+
+/*
+*  Primary idea:   <Brute force>
+*  Time Complexity : O(n^2)
+*  Space Complexity : O(n)
+*  Runtime:  ms
+*  Memory Usage:  MB
+*/
 class Solution {
     func countBits(_ num: Int) -> [Int] {
+        var dp = [Int](repeating: 0, count: num+1)
         
+        for binary in 0...num {
+            for bit in String(binary, radix: 2) { // binary 표현 후, 1개의 bit씩을 개산
+                if bit == "1"{
+                    dp[binary] += 1
+                }
+            }
+        }
+        
+        return dp
     }
 }
