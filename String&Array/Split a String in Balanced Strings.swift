1452206a0ed3a6ee75b6663f7eaac2e40e77509f
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-01 16:15:38 +0900

    fix : Split a String in Balanced Strings 문제 풀이 수정 (String&Array)
    *  Primary idea:   <String&Array>
    *  1. check : "R"일때 1증가, "L"일떄 1감소
    *  2. count : check 변수가 0일때 증가

--- String&Array/Split a String in Balanced Strings.swift
+++ String&Array/Split a String in Balanced Strings.swift
@@ -2,7 +2,8 @@
 *  Split a String in Balanced Strings.swift
 *  Question Link: https://leetcode.com/problems/split-a-string-in-balanced-strings/
 *  Primary idea:   <String&Array>
-*                  1.
+*                  1. check : "R"일때 1증가, "L"일떄 1감소
+*                  2. count : check 변수가 0일때 증가
 *
 *  Time Complexity :
 *  Space Complexity :
@@ -14,3 +15,26 @@
 
 import Foundation
 
+class Solution {
+    func balancedStringSplit(_ s: String) -> Int {
+        var checkBalance = 0 // 1. check
+        var countBalance = 0 // 2. count
+        
+        for c in s {
+            switch c {
+            case "R":
+                checkBalance += 1
+            case "L":
+                checkBalance -= 1
+            default:
+                print("error")
+            }
+            
+            if checkBalance == 0 {
+                countBalance += 1
+            }
+        }
+        
+        return countBalance
+    }
+}
