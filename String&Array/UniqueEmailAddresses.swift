2bb6e199d9f8b0c09ce8101d2022e466a28dff34
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-01 17:11:14 +0900

    finish : Unique Email Addresses 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    *  Time Complexity : O(n)
    *  Space Complexity : O(1)
    *  Runtime: 244 ms
    *  Memory Usage: 22.3 MB

--- String&Array/UniqueEmailAddresses.swift
+++ String&Array/UniqueEmailAddresses.swift
@@ -1,24 +1,28 @@
-//
-//  UniqueEmailAddresses.swift
-//  Question Link: https://leetcode.com/problems/unique-email-addresses/
-//  Primary idea:   1. Set을 통해 email 분리
-//                  2. filter 함수 이용
-//
-//  Created by gunhyeong on 2020/02/05.
-//
+/*
+*  UniqueEmailAddresses.swift
+*  Question Link: https://leetcode.com/problems/unique-email-addresses/
+*  Primary idea:   1. Set을 통해 email 분리
+*                  2. filter 함수 이용
+*
+*  Time Complexity : O(n)
+*  Space Complexity : O(1)
+*  Runtime: 244 ms
+*  Memory Usage: 22.3 MB
+*
+*  Created by gunhyeong on 2020/02/05.
+*/
 
 import Foundation
 
-class NumUniqueEmails {
+class Solution {
     func numUniqueEmails(_ emails: [String]) -> Int {
         var emailSet:Set<String> = []
-        for s in emails {
+        for s in emails{
             let address = s.components(separatedBy: "@")
             let name = address[0].components(separatedBy: "+").first?.filter({$0 != "."})
             
-            emailSet.insert(name!+address.last!)
+            emailSet.insert(name!+"@"+address.last!)
         }
-        
         return emailSet.count
     }
 }
