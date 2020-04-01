13b36a110150ed31dc4ca41b38ae0ae2b8325518
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-01 15:40:06 +0900

    fix : Defanging an IP Address 문제 풀이 수정 (String&Array)
    *  Primary idea:   <String&Array>
    *  1. 문자열을 대체하는 내부 함수를 사용(대체, 제거) - replacingOccurrences

--- String&Array/Defanging an IP Address.swift
+++ String&Array/Defanging an IP Address.swift
@@ -2,7 +2,7 @@
 *  Defanging an IP Address.swift
 *  Question Link: https://leetcode.com/problems/defanging-an-ip-address/
 *  Primary idea:   <String&Array>
-*                  1.
+*                  1. 문자열을 대체하는 내부 함수를 사용(대체, 제거) - replacingOccurrences
 *
 *  Time Complexity :
 *  Space Complexity :
@@ -16,6 +16,6 @@ import Foundation
 
 class Solution {
     func defangIPaddr(_ address: String) -> String {
-        
+        return address.replacingOccurrences(of: ".", with: "[.]")
     }
 }
