2809974d78e229d5a3135c23e5e8adb7a69b2a32
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-01 16:56:13 +0900

    finish : Move Zeroes 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    *  Time Complexity : O(n)
    *  Space Complexity : O(1)
    *  Runtime: 44 ms
    *  Memory Usage: 21.4 MB

--- String&Array/PlusOne.swift
+++ String&Array/PlusOne.swift
@@ -1,18 +1,24 @@
-//
-//  PlusOne.swift
-//  Question Link: https://leetcode.com/problems/plus-one/
-//  Primary idea:   1. 뒷자리부터 9가 있는지 체크
-//                  2. input[0], input[1], input[2]
-//                  3. input[2] = 0
-//                  4. 새로운 int[]의 크기를 증가
-//                  5. 새로운 배열 res[0] = 1
-//                  6. 9가 아닌경우, 해당자리수 값을 +1
-//  Created by gunhyeong on 2020/02/02.
-//
+/*
+*  PlusOne.swift
+*  Question Link: https://leetcode.com/problems/plus-one/
+*  Primary idea:   1. 뒷자리부터 9가 있는지 체크
+*                  2. input[0], input[1], input[2]
+*                  3. input[2] = 0
+*                  4. 새로운 int[]의 크기를 증가
+*                  5. 새로운 배열 res[0] = 1
+*                  6. 9가 아닌경우, 해당자리수 값을 +1
+*
+*  Time Complexity : O(n)
+*  Space Complexity : O(1)
+*  Runtime: 44 ms
+*  Memory Usage: 21.4 MB
+*
+*  Created by gunhyeong on 2020/02/02.
+*/
 
 import Cocoa
 
-class PlusOne {
+class Solution {
     func plusOne(digits: [Int]) -> [Int] {
         var digits = digits
         var index = digits.count - 1
@@ -29,5 +35,4 @@ class PlusOne {
         
         digits.insert(1, at: 0)
         return digits
-    }
 }
