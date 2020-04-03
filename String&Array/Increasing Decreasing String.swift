0a3d8d18309293710df84d5dd22c3ca0543ecac3
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-03 14:36:23 +0900

    feat : Increasing Decreasing String 문제 풀이 시작

---
+++ String&Array/Increasing Decreasing String.swift
@@ -0,0 +1,17 @@
+//
+//  Increasing Decreasing String.swift
+*  Question Link: https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
+*  Primary idea:   <String & Array>
+*                  1. 아나그램(Anagram) : 일종의 말장난으로 어떠한 단어의 문자를 재배열하여 다른 뜻을 가지는 다른 단어로 바꾸는 것을 말한다.
+*                  2. s의 alphabet의 각 갯수
+*                  3. t의 alphabet의 각 갯수가 0보다 클경우, 사용했으므로 한개씩 감소
+*
+*  Time Complexity : O(n)
+*  Space Complexity : O(n)
+*  Runtime: 304 ms
+*  Memory Usage: 21 MB
+//
+//  Created by gunhyeong on 2020/04/03.
+//
+
+import Foundation
