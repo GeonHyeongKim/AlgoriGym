c58203ebc0a6c7ad3dc8e0e9730cd38d1c471886
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-03 16:03:30 +0900

    fix : Increasing Decreasing String 문제 풀이 수정 (String & Array)
    *  Primary idea:   <String & Array>
    *  1. dictionary : reduce를 이용해, [char:int]로 alphabet과 각 갯수(cnt)를 저장해놓는다.
    *  2. alphabet의 갯수가 1이상일때, 1개씩 감소
    *  3. sort : i(index)의 홀짝을 판별하면서, smallest, largest를 표현

--- String&Array/Increasing Decreasing String.swift
+++ String&Array/Increasing Decreasing String.swift
@@ -1,17 +1,46 @@
-//
-//  Increasing Decreasing String.swift
-*  Question Link: https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
+/*
+*  Increasing Decreasing String.swift
+*  Question Link: https://leetcode.com/problems/increasing-decreasing-string/
 *  Primary idea:   <String & Array>
-*                  1. 아나그램(Anagram) : 일종의 말장난으로 어떠한 단어의 문자를 재배열하여 다른 뜻을 가지는 다른 단어로 바꾸는 것을 말한다.
-*                  2. s의 alphabet의 각 갯수
-*                  3. t의 alphabet의 각 갯수가 0보다 클경우, 사용했으므로 한개씩 감소
+*                  1. dictionary : reduce를 이용해, [char:int]로 alphabet과 각 갯수(cnt)를 저장해놓는다.
+*                  2. alphabet의 갯수가 1이상일때, 1개씩 감소
+*                  3. sort : i(index)의 홀짝을 판별하면서, smallest, largest를 표현
 *
-*  Time Complexity : O(n)
-*  Space Complexity : O(n)
-*  Runtime: 304 ms
-*  Memory Usage: 21 MB
-//
-//  Created by gunhyeong on 2020/04/03.
-//
+*  Time Complexity :
+*  Space Complexity :
+*  Runtime:  ms
+*  Memory Usage:  MB
+*
+*  Created by gunhyeong on 2020/04/03.
+*/
 
 import Foundation
+
+class Solution {
+    func sortString(_ s: String) -> String {
+        var dict = s.reduce(into: [Character: Int](), { $0[$1] = $0[$1, default: 0] + 1 }) // 1. dictionary
+        var index = 0
+        var subStrings = [String]()
+        
+        while !dict.isEmpty{
+            subStrings.append("")
+
+            for (char, cnt) in dict {
+                subStrings[index].append(char)
+
+                if cnt > 1 { // 2. 1개 이상일때
+                    dict[char] = cnt - 1
+                } else {
+                    dict[char] = nil
+                }
+            }
+            index += 1
+        }
+        var result = ""
+        for i in 0..<subStrings.count {
+            result += i % 2 == 0 ? subStrings[i].sorted(by : <) : subStrings[i].sorted(by : >) // 3. sort
+        }
+        
+        return result
+    }
+}
