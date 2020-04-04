3530525cef9fcffc3f1f9dbe201f4fa6c6b9c066
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-05 07:57:16 +0900

    fix : Unique Morse Code Words 문제 풀이 수정 (String & Array)
    *  Primary idea:   <String & Array>
    *  1. morse : 미리 dictionary를 만들어 놓는다
    *  2. 이중 for문 : 단어와 일치하는 모스부호를 입력해 결과(result<set>)에 넣는다.

--- String&Array/Unique Morse Code Words.swift
+++ String&Array/Unique Morse Code Words.swift
@@ -2,7 +2,8 @@
 *  Unique Morse Code Words.swift
 *  Question Link: https://leetcode.com/problems/unique-morse-code-words/
 *  Primary idea:   <String & Array>
-*                  1.
+*                  1. morse : 미리 dictionary를 만들어 놓는다
+*                  2. 이중 for문 : 단어와 일치하는 모스부호를 입력해 결과(result<set>)에 넣는다.
 *
 *  Time Complexity :
 *  Space Complexity :
@@ -15,7 +16,21 @@
 import Foundation
 
 class Solution {
+    let morse: [Character : String] = ["a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.", "g": "--.", "h": "....", "i": "..", "j": ".---", "k": "-.-", "l": ".-..", "m": "--", "n": "-.", "o": "---", "p": ".--.", "q": "--.-", "r": ".-.", "s": "...", "t": "-", "u": "..-", "v": "...-", "w": ".--", "x": "-..-", "y": "-.--", "z": "--.."] // 1. more
+
     func uniqueMorseRepresentations(_ words: [String]) -> Int {
+        var someSet: Set<String> = Set()
+        for s in words {
+            var result: String = ""
+            for char in Array(s) { // 2.이중 for문
+                
+                if let data = morse[char] {
+                    result.append(data)
+                }
+            }
+            someSet.insert(result)
+        }
         
+        return someSet.count
     }
 }
