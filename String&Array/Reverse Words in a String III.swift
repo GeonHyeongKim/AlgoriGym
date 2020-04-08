760e0aa7059ee9995161b0e2ea3f55f2329ff50a
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-08 22:04:40 +0900

    fix : Reverse Words in a String III 문제 풀이 수정 (String & Array)
    *  Primary idea:   <String & Array>
    *  1. 분리 : Split()으로 " "를 인식하면 분리
    *  2. 마지막 문자열 제거 : dropLast()를 이용해 마지막 문자열(" ") 제거

--- String&Array/Reverse Words in a String III.swift
+++ String&Array/Reverse Words in a String III.swift
@@ -1,7 +1,9 @@
 /*
 *  Reverse Words in a String III.swift
 *  Question Link: https://leetcode.com/problems/reverse-words-in-a-string-iii/
-*  Primary idea:   1.
+*  Primary idea:   <String & Array>
+*                  1. 분리 : Split()으로 " "를 인식하면 분리
+*                  2. 마지막 문자열 제거 : dropLast()를 이용해 마지막 문자열(" ") 제거
 *
 *  Time Complexity :
 *  Space Complexity :
@@ -15,6 +17,14 @@ import Foundation
 
 class Solution {
     func reverseWords(_ s: String) -> String {
+        var result = ""
+        let words = s.split(separator: " ") // 1. 분리
         
+        for word in words {
+            result += word.reversed()
+            result += " "
+        }
+        
+        return String(result.dropLast()) // 2. 마지막 제거
     }
 }
