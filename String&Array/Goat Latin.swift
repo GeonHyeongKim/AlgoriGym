06fe71d5c5c6ecb421481e018d4e8d6fac218c25
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-09 15:07:56 +0900

    fix : Goat Latin 문제 풀이 수정 (String & Array)
    *  Primary idea:   <String & Array>
    *  1. vowel 배열 선언 : 모음의 대,소문자를 미리 선언
    *  2. 분리 : space를 기준으로 분리
    *  3. 첫번째 알파벳 : 첫번쨰 알파벳 추출
    *  4. contrains : vowel에 포함되어 있는지 확인
    *     4-1 : 포함되어 있을 경우, 첫번째 알파벳 + Latin
    *     4-2 : 포함되어 있지 않을 경우, Latin만
    *  5. Latin : 문자열에 "a" 추가

--- String&Array/Goat Latin.swift
+++ String&Array/Goat Latin.swift
@@ -2,7 +2,13 @@
 *  Goat Latin.swift
 *  Question Link: https://leetcode.com/problems/goat-latin/
 *  Primary idea:   <String & Array>
-*                  1.
+*                  1. vowel 배열 선언 : 모음의 대,소문자를 미리 선언
+*                  2. 분리 : space를 기준으로 분리
+*                  3. 첫번째 알파벳 : 첫번쨰 알파벳 추출
+*                  4. contrains : vowel에 포함되어 있는지 확인
+*                      4-1 : 포함되어 있을 경우, 첫번째 알파벳 + Latin
+*                      4-2 : 포함되어 있지 않을 경우, Latin만
+*                  5. Latin : 문자열에 "a" 추가
 *
 *  Time Complexity :
 *  Space Complexity :
@@ -16,6 +22,26 @@ import Foundation
 
 class Solution {
     func toGoatLatin(_ S: String) -> String {
+        let vowel = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"] // 1. vawel
+        var result = ""
+        let words = S.split(separator: " ") // 2. 분리
+        var latin = "maa"
+
+        for word in words {
+            var newWord = word
+            let first = String(word.first!) // 3. 첫번째 알파벳
+            
+            if !vowel.contains(first) { // 4. contains
+                newWord = newWord.dropFirst() // 4-1
+                newWord += first + latin //
+            } else {
+                newWord += latin // 4-2
+            }
+            
+            latin += "a" // 5. Latin
+            result += newWord + " "
+        }
         
+        return String(result.dropLast())
     }
 }
