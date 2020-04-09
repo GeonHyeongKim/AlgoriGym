a1349e12b5ce2ce1e047f93c1db6290af01758be
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-10 00:50:18 +0900

    fix : Maximum Number of Balloons 문제 풀이 수정 (String & Array)
    *  Primary idea:   <String & Array>
    *  1. balloon : balloon의 알파벳에 대한 갯수를 나타내는 dictionary를 선언
    *  2. 갯수 계산 : 알파벳의 갯수를 전부 돌면서 1개씩 누적
    *  3. 최솟값 : min(알파벳의 갯수 중 가장 작은 것, 알파벳 "l"을 2로 나눈 값, 알파벳 "o"을 2로 나눈 값)을 구한다.

--- String&Array/Maximum Number of Balloons.swift
+++ String&Array/Maximum Number of Balloons.swift
@@ -2,7 +2,9 @@
 *  Maximum Number of Balloons.swift
 *  Question Link: https://leetcode.com/problems/maximum-number-of-balloons/
 *  Primary idea:   <String & Array>
-*                  1.
+*                  1. balloon : balloon의 알파벳에 대한 갯수를 나타내는 dictionary를 선언
+*                  2. 갯수 계산 : 알파벳의 갯수를 전부 돌면서 1개씩 누적
+*                  3. 최솟값 : min(알파벳의 갯수 중 가장 작은 것, 알파벳 "l"을 2로 나눈 값, 알파벳 "o"을 2로 나눈 값)을 구한다.
 *
 *  Time Complexity :
 *  Space Complexity :
@@ -16,6 +18,14 @@ import Foundation
 
 class Solution {
     func maxNumberOfBalloons(_ text: String) -> Int {
+        var dic: [Character:Int] = ["b":0, "a":0, "l":0, "o":0, "n":0] // 1. balloon
         
+        for alphabet in text {
+            if dic.keys.contains(alphabet) { // 2. 갯수 계산
+                dic[alphabet]! += 1
+            }
+        }
+        
+        return min(dic.values.min()!, dic["l"]!/2, dic["o"]!/2) // 3. 최솟값
     }
 }
