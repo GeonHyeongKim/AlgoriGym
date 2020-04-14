f3b16103193de01a894b7e92eb38749327898d51
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-14 15:32:48 +0900

    fix : Minimum Subsequence in Non-Increasing Order 문제 풀이 수정 (Greedy)
    *  Primary idea:   <Greedy>
    *  1. 조건 : 부분합(sum)이 전체의 2분의 1보다 작을때까지

--- Greedy/Minimum Subsequence in Non-Increasing Order.swift
+++ Greedy/Minimum Subsequence in Non-Increasing Order.swift
@@ -2,7 +2,7 @@
 *  Minimum Subsequence in Non-Increasing Order.swift
 *  Question Link: https://leetcode.com/problems/minimum-subsequence-in-non-increasing-order/
 *  Primary idea:   <Greedy>
-*                  1.
+*                  1. 조건 : 부분합(sum)이 전체의 2분의 1보다 작을때까지 
 *
 *  Time Complexity :
 *  Space Complexity :
@@ -16,6 +16,18 @@ import Foundation
 
 class Solution {
     func minSubsequence(_ nums: [Int]) -> [Int] {
+        var result = [Int]()
+        let sortedNums = nums.sorted(by: >)
+        var i = 0
+        var sum = 0
+        let totalSum = nums.reduce(0, +)
         
+        while sum <= totalSum/2 { // 1. 조건
+            sum += sortedNums[i]
+            result.append(sortedNums[i])
+            i += 1
+        }
+        
+        return result
     }
 }
