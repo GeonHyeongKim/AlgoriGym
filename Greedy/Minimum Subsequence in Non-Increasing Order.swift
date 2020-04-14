76964369c1164f131437448037c65cb6317dea29
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-14 13:13:37 +0900

    feat : Minimum Subsequence in Non-Increasing Order 문제 풀이 시작

---
+++ Greedy/Minimum Subsequence in Non-Increasing Order.swift
@@ -0,0 +1,21 @@
+/*
+*  Minimum Subsequence in Non-Increasing Order.swift
+*  Question Link: https://leetcode.com/problems/minimum-subsequence-in-non-increasing-order/
+*  Primary idea:   <Greedy>
+*                  1.
+*
+*  Time Complexity :
+*  Space Complexity :
+*  Runtime:  ms
+*  Memory Usage:  MB
+*
+*  Created by gunhyeong on 2020/04/14.
+*/
+
+import Foundation
+
+class Solution {
+    func minSubsequence(_ nums: [Int]) -> [Int] {
+        
+    }
+}
