99b1f0aff81aa10b03f9d5af9eee37a9b84d3f10
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-15 13:05:18 +0900

    feat : Partition Labels 문제 풀이 시작

---
+++ Greedy/Partition Labels.swift
@@ -0,0 +1,21 @@
+/*
+*  Partition Labels.swift
+*  Question Link: https://leetcode.com/problems/partition-labels/
+*  Primary idea:   <Greedy>
+*                  1.
+*
+*  Time Complexity :
+*  Space Complexity :
+*  Runtime:  ms
+*  Memory Usage:  MB
+*
+*  Created by gunhyeong on 2020/04/15.
+*/
+
+import Foundation
+
+class Solution {
+    func partitionLabels(_ S: String) -> [Int] {
+        
+    }
+}
