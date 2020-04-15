b87f298afb0de475cbeb66aa0a70633fe9c332b7
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-15 13:51:37 +0900

    fix : Partition Labels 문제 풀이 수정 (Greedy)
    *  Primary idea:   <Greedy>
    *  1. end index : directory를 이용하여, 마지막 index 저장
    *  2. 최댓값 : 파티션에 속한 문자들의 마지막 index를 알아낸다.
    *  3. 조건 : 최댓값과 파티션의 모든 문자를 탐색하는 index(i)가 같아 졌을때, parition으로 구분

--- Greedy/Partition Labels.swift
+++ Greedy/Partition Labels.swift
@@ -2,7 +2,9 @@
 *  Partition Labels.swift
 *  Question Link: https://leetcode.com/problems/partition-labels/
 *  Primary idea:   <Greedy>
-*                  1.
+*                  1. end index : directory를 이용하여, 마지막 index 저장
+*                  2. 최댓값 : 파티션에 속한 문자들의 마지막 index를 알아낸다.
+*                  3. 조건 : 최댓값과 파티션의 모든 문자를 탐색하는 index(i)가 같아 졌을때, parition으로 구분
 *
 *  Time Complexity :
 *  Space Complexity :
@@ -16,6 +18,24 @@ import Foundation
 
 class Solution {
     func partitionLabels(_ S: String) -> [Int] {
+        var partition = [Int]()
+        var endIndex = [Character:Int]()
         
+        for (i, char) in S.enumerated() { // 1. end index
+            endIndex[char] = i
+        }
+        
+        var maxLast = 0
+        var anchor = 0
+        for (i, char) in S.enumerated() {
+            maxLast = max(maxLast, endIndex[char]!) // 2. 최댓값
+            
+            if i == maxLast { // 3. 조건
+                partition.append(i-anchor+1)
+                anchor = i+1
+            }
+        }
+        
+        return partition
     }
 }
