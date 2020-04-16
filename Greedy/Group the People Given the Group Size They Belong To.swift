32ae93925c87aaa792b947d6b60c1c7c72355757
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-16 18:23:25 +0900

    fix : Group the People Given the Group Size They Belong To 문제 풀이 수정 (Greedy)
    *  Primary idea:   <Greedy>
    *  1. size 구분 : groupSizes의 원소별로 구분하여 [Int] 형태로 저장
    *  2. 새로운 그룹 : groupSizes의 원소(index)의 크기와 지금 만들고 있는 그룹(group)의 키기가 같으면 결과값에 저장후, group 초기화(removeAll)

--- Greedy/Group the People Given the Group Size They Belong To.swift
+++ Greedy/Group the People Given the Group Size They Belong To.swift
@@ -2,7 +2,8 @@
 *  Group the People Given the Group Size They Belong To.swift
 *  Question Link: https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to/
 *  Primary idea:   <Greedy>
-*                  1.
+*                  1. size 구분 : groupSizes의 원소별로 구분하여 [Int] 형태로 저장
+*                  2. 새로운 그룹 : groupSizes의 원소(index)의 크기와 지금 만들고 있는 그룹(group)의 키기가 같으면 결과값에 저장후, group 초기화(removeAll)
 *
 *  Time Complexity :
 *  Space Complexity :
@@ -16,6 +17,25 @@ import Foundation
 
 class Solution {
     func groupThePeople(_ groupSizes: [Int]) -> [[Int]] {
+        var result = [[Int]]()
+        var dict = [Int: [Int]]()
         
+        for (index, value) in groupSizes.enumerated() { // 1. size별 구분
+            dict[value, default: []].append(index)
+        }
+        
+        for (index, value) in dict {
+            var group = [Int]()
+            
+            for v in value {
+                group.append(v)
+                if group.count == index { // 2. 새로운 그룹
+                    result.append(group)
+                    group.removeAll()
+                }
+            }
+        }
+                
+        return result
     }
 }
