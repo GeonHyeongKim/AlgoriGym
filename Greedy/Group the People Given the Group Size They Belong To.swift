9ceac8a34917d0c9ba9a0c6d1e06b00f3744811b
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-16 18:24:44 +0900

    finish : Group the People Given the Group Size They Belong To 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    *  Time Complexity : O(n)
    *  Space Complexity : O(n)
    *  Runtime: 68 ms
    *  Memory Usage: 21.3MB

--- Greedy/Group the People Given the Group Size They Belong To.swift
+++ Greedy/Group the People Given the Group Size They Belong To.swift
@@ -5,10 +5,10 @@
 *                  1. size 구분 : groupSizes의 원소별로 구분하여 [Int] 형태로 저장
 *                  2. 새로운 그룹 : groupSizes의 원소(index)의 크기와 지금 만들고 있는 그룹(group)의 키기가 같으면 결과값에 저장후, group 초기화(removeAll)
 *
-*  Time Complexity :
-*  Space Complexity :
-*  Runtime:  ms
-*  Memory Usage:  MB
+*  Time Complexity : O(n)
+*  Space Complexity : O(n)
+*  Runtime: 68 ms
+*  Memory Usage: 21.3MB
 *
 *  Created by gunhyeong on 2020/04/16.
 */
