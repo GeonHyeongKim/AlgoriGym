a5f29165effb92a8381245c9cda7bcc2282b494f
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-17 18:03:54 +0900

    fix : Minimum Add to Make Parentheses Valid 문제 풀이 수정 (Greedy)
    *  Primary idea:   <Greedy>
    *  1. "(" : 왼족 소괄호가 나올때, count를 1 증가 시키고 ")"일때 1 감소 시킨다.
    *  2. count = 0 : "( )" 짝을 이룰때 0이다.

--- Greedy/Minimum Add to Make Parentheses Valid .swift
+++ Greedy/Minimum Add to Make Parentheses Valid .swift
@@ -2,7 +2,8 @@
 *  Minimum Add to Make Parentheses Valid .swift
 *  Question Link: https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
 *  Primary idea:   <Greedy>
-*                  1.
+*                  1. "(" : 왼족 소괄호가 나올때, count를 1 증가 시키고 ")"일때 1 감소 시킨다.
+*                  2. count = 0 : "( )" 짝을 이룰때 0이다.
 *
 *  Time Complexity :
 *  Space Complexity :
@@ -16,6 +17,22 @@ import Foundation
 
 class Solution {
     func minAddToMakeValid(_ S: String) -> Int {
+        var result = 0
+        var count = 0
         
+        for char in S {
+            if char == "(" { // 1. "("
+                count += 1
+                continue
+            }
+            
+            if count == 0 { // 2. count = 0
+                result += 1 // 2-1. 결과값 증가
+            } else {
+                count -= 1 // 2-2. count 감소
+            }
+        }
+        
+        return result + count
     }
 }
