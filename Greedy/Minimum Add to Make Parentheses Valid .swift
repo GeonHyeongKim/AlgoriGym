b27a814e12dffe01fa1d6b3a5048e020fc1ebbfe
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-17 18:05:10 +0900

    finish : Minimum Add to Make Parentheses Valid 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    *  Time Complexity : O(n)
    *  Space Complexity : O(1)
    *  Runtime: 8 ms
    *  Memory Usage: 20.6 MB

--- Greedy/Minimum Add to Make Parentheses Valid .swift
+++ Greedy/Minimum Add to Make Parentheses Valid .swift
@@ -5,10 +5,10 @@
 *                  1. "(" : 왼족 소괄호가 나올때, count를 1 증가 시키고 ")"일때 1 감소 시킨다.
 *                  2. count = 0 : "( )" 짝을 이룰때 0이다.
 *
-*  Time Complexity :
-*  Space Complexity :
-*  Runtime:  ms
-*  Memory Usage:  MB
+*  Time Complexity : O(n)
+*  Space Complexity : O(1)
+*  Runtime: 8 ms
+*  Memory Usage: 20.6 MB
 *
 *  Created by gunhyeong on 2020/04/17.
 */
