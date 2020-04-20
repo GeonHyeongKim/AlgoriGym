aff5bd165e8c53cbeaaeedaffbd9b622dfffadee
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-20 13:04:11 +0900

    feat : 수열의 값 문제 풀이 시작

---
+++ String&Array/수열의 값.swift
@@ -0,0 +1,15 @@
+//
+//  수열의 값.swift
+*  Question Link: https://www.acmicpc.net/problem/2867
+*  Primary idea:   <String & Array>
+*                  1.
+*
+*  Time Complexity :
+*  Space Complexity :
+*  Runtime:  ms
+*  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/04/20.
+//
+
+import Foundation
