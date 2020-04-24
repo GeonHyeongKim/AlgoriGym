5c730fc9f1441d5436f16727fc424243208269a5
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-24 14:05:51 +0900

    feat : Best Time to Buy and Sell Stock 문제 풀이 (Brute force)
    2중 for 문을 통해 max값 구하기
    (1번째 for : 살때, 2번째 for : 팔때)
    result = max(result, prices[j] - prices[i])
    
    문제점 : 시간이 너무 오래 걸린다.

--- String&Array/Best Time to Buy and Sell Stock.swift
+++ String&Array/Best Time to Buy and Sell Stock.swift
@@ -2,7 +2,7 @@
 *  Best Time to Buy and Sell Stock.swift
 *  Question Link: https://leetcode.com/problems/jewels-and-stones/
 *  Primary idea:    <String & Array>
-*                   1.
+*                   1. 2중 for문을 통해 풀기 -> Time Complexity가 O(n^2)
 *
 *  Time Complexity :
 *  Space Complexity :
@@ -16,6 +16,14 @@ import Foundation
 
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
+        var result = 0
         
+        for i in 0..<prices.count {
+            for j in (i+1)..<prices.count {
+                result = max(result, prices[j] - prices[i])
+            }
+        }
+        
+        return result
     }
 }
