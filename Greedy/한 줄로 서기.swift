4f1c5a989dedec389ffd1da02ccbf7e190b7d20a
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-04-30 23:17:09 +0900

    faet : 한 줄로 서기 문제풀이 시작

---
+++ Greedy/한 줄로 서기.swift
@@ -0,0 +1,15 @@
+/*
+*  한 줄로 서기.swift
+*  Question Link: https://www.acmicpc.net/problem/1138
+*  Primary idea:    <Greedy>
+*                    1.
+*
+*  Time Complexity :
+*  Space Complexity :
+*  Runtime:  ms
+*  Memory Usage:
+*
+*  Created by gunhyeong on 2020/04/30.
+*/
+
+import Foundation
