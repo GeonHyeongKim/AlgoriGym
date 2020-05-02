ea9043db5454fd9630e98162562383e938a03d0f
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-02 22:13:08 +0900

    feat : DNA 문제 풀이 시작

---
+++ Greedy/DNA.swift
@@ -0,0 +1,14 @@
+/*
+*  DNA.swift
+*  Question Link: https://www.acmicpc.net/problem/1969
+*  Primary idea:    <Greedy>
+*                    1.
+*  Time Complexity :
+*  Space Complexity :
+*  Runtime:  ms
+*  Memory Usage:
+*
+*  Created by gunhyeong on 2020/05/02.
+*/
+
+import Foundation
