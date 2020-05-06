6c53173b2989cd2a8f8b4876c3e37215aef847ab
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-06 20:37:56 +0900

    feat: 문자열 문제 풀이 시작

---
+++ Greedy/문자열.swift
@@ -0,0 +1,15 @@
+/*
+*  문자열.swift
+*  Question Link: https://www.acmicpc.net/problem/1120
+*  Primary idea:        <Greedy>
+*                       1.
+*
+*  Time Complexity :
+*  Space Complexity :
+*  Runtime:  ms
+*  Memory Usage:  MB
+*
+*  Created by gunhyeong on 2020/05/06.
+*/
+
+import Foundation
