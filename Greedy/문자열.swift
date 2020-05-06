0f6a51b566df82327e4aaf79fdaa135368b50284
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-06 21:45:29 +0900

    fix : 문자열 문제 풀이 수정 (Greedy)
    *  Primary idea:        <Greedy>
    *  1. 앞/뒤로는 무조건 긴 문자열과 같은 값이 되도록 값을 넣는다고 가정
    *  2. a의 길이 < b의 길이 : a[i] != b[j]의 개수가 가장 짧은 구간으로 선택

--- Greedy/문자열.swift
+++ Greedy/문자열.swift
@@ -2,7 +2,8 @@
 *  문자열.swift
 *  Question Link: https://www.acmicpc.net/problem/1120
 *  Primary idea:        <Greedy>
-*                       1.
+*                       1. 앞/뒤로는 무조건 긴 문자열과 같은 값이 되도록 값을 넣는다고 가정
+*                       2. a의 길이 < b의 길이 : a[i] != b[j]의 개수가 가장 짧은 구간으로 선택
 *
 *  Time Complexity :
 *  Space Complexity :
@@ -13,3 +14,24 @@
 */
 
 import Foundation
+
+var input = readLine()!.split(separator : " ")
+var A = Array(input[0])
+var B = Array(input[1])
+var diff = B.count
+
+for i in 0..<B.count-A.count + 1 {
+    var count = 0
+    for j in 0..<A.count {
+        if A[j] != B[i+j] {
+            count += 1
+        }
+    }
+    
+    if diff > count {
+        diff = count
+    }
+}
+
+print(diff)
+
