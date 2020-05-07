97ad3e6b35ba9eda10ef1206faf7948371fb8096
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-07 15:45:25 +0900

    fix : 토너먼트 만들기 문제 풀이 수정 (Greedy)
    *  Primary idea:        <Greedy>
    *  1. 랭킹이 가장 낮은 선수부터 찾고 좌,우 선수 중 랭킹 차이가 적은 선수와 시합 하도록 한다.
    *  ex) 1 6 2 5 3 4 (6 - 2 = 4)
    *        1 2 5 3 4 (5 - 3 = 2)
    *        1 2 3 4 (4 - 3 = 1)
    *        1 2 3 (3 - 2 = 1)
    *        1 2 (2 - 1 = 1)

--- Greedy/토너먼트 만들기.swift
+++ Greedy/토너먼트 만들기.swift
@@ -2,7 +2,12 @@
 *  토너먼트 만들기.swift
 *  Question Link: https://www.acmicpc.net/problem/1120
 *  Primary idea:        <Greedy>
-*                       1.
+*                       1. 랭킹이 가장 낮은 선수부터 찾고 좌,우 선수 중 랭킹 차이가 적은 선수와 시합 하도록 한다.
+*                            ex) 1 6 2 5 3 4 (6 - 2 = 4)
+*                                1 2 5 3 4 (5 - 3 = 2)
+*                                1 2 3 4 (4 - 3 = 1)
+*                                1 2 3 (3 - 2 = 1)
+*                                1 2 (2 - 1 = 1)
 *
 *  Time Complexity :
 *  Space Complexity :
@@ -14,3 +19,26 @@
 
 import Foundation
 
+var n = Int(readLine()!)!
+var ranks = Array(readLine()!.split(separator: " ").map{Int($0)!})
+
+var maxRank = n
+var diff = 0
+
+for _ in 0..<ranks.count - 1 {
+    let maxIndex = ranks.firstIndex(of: maxRank)!
+    if maxIndex == 0 { // 가장 왼쪽일 경우
+        diff += ranks[maxIndex] - ranks[maxIndex + 1]
+    } else if maxIndex == ranks.count - 1 { // 가장 오른쪽일 경우
+        diff += ranks[maxIndex] - ranks[maxIndex - 1]
+    } else {
+        let left = ranks[maxIndex] - ranks[maxIndex + 1]
+        let right = ranks[maxIndex] - ranks[maxIndex - 1]
+        diff += min(left, right) // 좌,우 선수 중 랭킹 차이가 적은 선수와 시합
+    }
+    
+    ranks.remove(at: maxIndex) // 시합 후 퇴장
+    maxRank -= 1
+}
+
+print(diff)
