fab648382dfe5e78ec62baf9aa3ea0cb627661d1
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-12 15:43:49 +0900

    feat: Heap sort의 init() & 최대 heap 계삼 나머지 node 재배열
    max heap을 구현하기 위한 반복문

--- Data structure/Heap Sort.swift
+++ Data structure/Heap Sort.swift
@@ -11,3 +11,17 @@
 //
 
 import Foundation
+
+func heapSort(array arr: inout [Int]) {
+    
+    let len = arr.count
+
+    // init, max heap
+    for i in stride(from: len/2-1 , through: 0, by: -1) { // len ~ 0 (0 포함 o)
+    }
+    
+    // for extract max element from heap
+    for i in stride(from: len-1, to: 0, by: -1) { // len ~ <0 (0 포함 x)
+        arr.swapAt(0, i) // 배열의 마지막 element가 계산된 max 값
+    }
+}
