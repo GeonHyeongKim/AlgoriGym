3c8e612a358bb2c091afdff8290d5c546480a58b
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-13 21:20:21 +0900

    feat : 최단거리 문제 중 가장 최소 거리를 가지고 있는 정점을 반환 하는 함수 ( func minDistance(distance dist: inout [Int], isVisted: inout [Bool]) -> Int )

--- Dijkstra/최단경로.swift
+++ Dijkstra/최단경로.swift
@@ -12,3 +12,18 @@
 */
 
 import Foundation
+
+// 가장 최소 거리를 가지고 있는 정점을 반환
+func minDistance(distance dist: inout [Int], isVisted: inout [Bool]) -> Int{
+    var min = Int.max
+    var index = 0
+    
+    for (i, visit) in isVisted.enumerated() {
+        if !visit && dist[i] < min {
+            min = dist[i]
+            index = i
+        }
+    }
+    
+    return index
+}
