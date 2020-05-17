9419ff18f684d53e1a81bc8c62091ddacae8e181
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-17 16:49:52 +0900

    fix : 회의실배정 문제 풀이 아이디어 (Greedy)
    //  Primary idea:       <Greedy>
    //                      1. sort : 회의 시작 시간을(start) 기준으로 오름차순으로 정렬
    //                      2. priority queue 개념 응용 : 회의 종료 시간(end)을 기준으로 먼저 넣는다.
    //                      3. end <= start 합치기

--- Greedy/회의실배정.swift
+++ Greedy/회의실배정.swift
@@ -2,7 +2,9 @@
 //  회의실배정.swift
 //  Question Link: https://www.acmicpc.net/problem/1931
 //  Primary idea:       <Greedy>
-//                      1. 
+//                      1. sort : 회의 시작 시간을(start) 기준으로 오름차순으로 정렬
+//                      2. priority queue 개념 응용 : 회의 종료 시간(end)을 기준으로 먼저 넣는다.
+//                      3. end <= start 합치기
 //
 //  Time Complexity :
 //  Space Complexity :
