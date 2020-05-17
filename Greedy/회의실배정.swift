4c29a00387accbb5cf5a36f69141cca8ca27861c
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-17 16:55:11 +0900

    finish : 회의실배정 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    //  Time Complexity : O(n logn) // sort 정렬
    //  Space Complexity : O(1)
    //  Runtime: 346 ms
    //  Memory Usage: 66.408 MB

--- Greedy/회의실배정.swift
+++ Greedy/회의실배정.swift
@@ -6,10 +6,10 @@
 //                      2. priority queue 개념 응용 : 회의 종료 시간(end)을 기준으로 먼저 넣는다.
 //                      3. end <= start 합치기
 //
-//  Time Complexity :
-//  Space Complexity :
-//  Runtime:  ms
-//  Memory Usage:  MB
+//  Time Complexity : O(n logn) // sort 정렬
+//  Space Complexity : O(1)
+//  Runtime: 346 ms
+//  Memory Usage: 66.408 MB
 //
 //  Created by gunhyeong on 2020/05/17.
 //
