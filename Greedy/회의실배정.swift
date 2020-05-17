a0db1481d831f0f034f741fbaca82633e6420a7b
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-17 16:51:49 +0900

    fix : 회의실배정 문제 풀이 구현
    1. 입력 받은 후, 끝나는 시간(end)을 기준으로 정렬
    2. 현재시간(끝난시간)이 다음 회의의 시작시간보다 작거나 같다면 count 증가 및 다음 비교를 위해 저장

--- Greedy/회의실배정.swift
+++ Greedy/회의실배정.swift
@@ -15,3 +15,44 @@
 //
 
 import Foundation
+
+struct Interval {
+    var start: Int = 0
+    var end: Int = 0
+    
+    init(start: Int, end: Int) {
+        self.start = start
+        self.end = end
+    }
+}
+
+var N = Int(readLine()!)!
+var meetings = [Interval] (repeating: Interval.init(start: 0, end: 0), count: N)
+var count = 0
+
+for i in 0..<N {
+    let input = readLine()!.split(separator: " ")
+    meetings[i].start = Int(input[0])! // start
+    meetings[i].end = Int(input[1])! // end
+}
+
+// 끝나는 시간을 기준으로 정렬
+meetings.sort(by: {
+    if $0.end == $1.end {
+        return $0.start < $1.start
+    } else {
+        return $0.end < $1.end
+    }
+})
+
+var endTime = -1
+
+for meet in meetings {
+    // 현재시간(끝난시간)이 다음 회의의 시작시간보다 작거나 같다면
+    if endTime <= meet.start {
+        count += 1
+        endTime = meet.end
+    }
+}
+
+print(count)
