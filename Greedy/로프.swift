da14558b851fc01fc60a48692e32d7a9fc3d3f40
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-17 14:21:56 +0900

    fix : 로프 문제 풀이 수정 (Greedy)
    //  Primary idea:       <Greedy>
    //  1. 각각의 로프에 모두 동일한 중량 : 각 로프를 선택했을 때 최대로 걸리는 중량을 계산
    //  2. preValue = max(preValue, str[index] * (N - index)

--- Greedy/로프.swift
+++ Greedy/로프.swift
@@ -1,8 +1,9 @@
 //
 //  로프.swift
 //  Question Link: https://www.acmicpc.net/problem/1120
-//  Primary idea:        <Greedy>
-//                       1.
+//  Primary idea:       <Greedy>
+//                      1. 각각의 로프에 모두 동일한 중량 : 각 로프를 선택했을 때 최대로 걸리는 중량을 계산
+//                      2. preValue = max(preValue, str[index] * (N - index)
 //
 //  Time Complexity :
 //  Space Complexity :
