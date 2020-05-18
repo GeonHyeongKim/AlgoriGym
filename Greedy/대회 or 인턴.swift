a761bef94585dd7488230e7bc89fcdd982d49093
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-18 19:06:33 +0900

    feat : 대회 or 인턴 문제 풀이 시작

---
+++ Greedy/대회 or 인턴.swift
@@ -0,0 +1,15 @@
+//
+//  대회 or 인턴.swift
+//  Question Link: https://www.acmicpc.net/problem/2875
+//  Primary idea:       <Greedy>
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/05/18.
+//
+
+import Foundation
