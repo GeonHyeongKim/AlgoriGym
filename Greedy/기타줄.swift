6a692e5fe1923f2988bbecc16598f2c26110f47b
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-19 16:16:09 +0900

    feat : 기타줄 문제 풀이 시작

---
+++ Greedy/기타줄.swift
@@ -0,0 +1,15 @@
+//
+//  기타줄.swift
+//  Question Link: https://www.acmicpc.net/problem/1049
+//  Primary idea:       <Greedy>
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/05/19.
+//
+
+import Foundation
