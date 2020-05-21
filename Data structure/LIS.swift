b39495d4e95354efcf301460c567ee53efd0a2c8
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-21 21:27:54 +0900

    feat : LIS을 BinarySearch로 구현 중 lower Bound 구현
    찾고자하는 정확한 값이 없더라도 찾고자 하는 값보다 큰 가장 작은 값 찾기

--- Data structure/LIS.swift
+++ Data structure/LIS.swift
@@ -83,5 +83,23 @@ func lisWithDp(_ array: inout [Int]) -> Int {
 // 3) case3, array[i]가 중간항에 비교해서 작을 경우. -> lower_bound(이분 탐색을 통해 해당하는 위치를 return) 이용
 
 func lisWithBinarySearch(_ array: inout [Int]) -> Int {
+    
+}
 
+// Binary search
+func lowerBound(arr: inout [Int], start: Int, end: Int, target: Int) -> Int{
+    var mid: Int
+    var start = start
+    var end = end
+    
+    while (end - start > 0) {  // 주어진 범위[start,end]에서 탐색하도록 한다. start == end이면 반복 종료
+        mid = (start + end) / 2  // 주어진 범위의 중간 위치를 계산한다
+        
+        if (arr[mid] < target){ // 찾고자 하는 값보다 작으면 오른쪽으로 한 칸만 더 시작 구간 갱신
+            start = mid + 1
+        } else {// 찾고자 하는 값보다 크면 거기까지 끝 구간 갱신
+            end = mid;
+        }
+    }
+    return end + 1; // 찾는 구간에 없는 경우 가장 마지막 +1 위치 전달
 }
