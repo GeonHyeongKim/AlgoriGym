15fbd5e6664c135461c06b7593e8c7b442405db4
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-26 14:30:10 +0900

    feat : 병든 나이트 문제 풀이 시작

---
+++ Greedy/병든 나이트.swift
@@ -0,0 +1,15 @@
+//
+//  병든 나이트.swift
+//  Question Link: https://www.acmicpc.net/problem/1783
+//  Primary idea:       <Greedy>
+//                      1)
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/05/26.
+//
+
+import Foundation
