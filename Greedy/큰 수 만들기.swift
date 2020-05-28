39edb8fcba234c430bf5eb52cc5b16b9347e1ba1
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-28 16:33:06 +0900

    fix : 큰 수 만들기 문제 풀이 아이디어 (Greedy)
    //  Primary idea:       <Greedy>
    //                      1) 입력된 숫자가 모두 0일 경우 예외처리를 한다
    //                      2) 큰 수를 만드는 문자들을 모으는 String(result, number의 갯수 - k 크기의 길이)을 선언
    //                      3) 각 자리 숫자 하나 하나 뽑을 때마다 그때의 최대값을 선택해야 한다

--- Greedy/큰 수 만들기.swift
+++ Greedy/큰 수 만들기.swift
@@ -2,7 +2,9 @@
 //  큰 수 만들기.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42883
 //  Primary idea:       <Greedy>
-//                      1)
+//                      1) 입력된 숫자가 모두 0일 경우 예외처리를 한다
+//                      2) 큰 수를 만드는 문자들을 모으는 String(result, number의 갯수 - k 크기의 길이)을 선언
+//                      3) 각 자리 숫자 하나 하나 뽑을 때마다 그때의 최대값을 선택해야 한다
 //
 //  Time Complexity :
 //  Space Complexity :
