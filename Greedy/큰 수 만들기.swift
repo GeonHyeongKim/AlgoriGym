f3b4ea4ccfdab7ef8ab896e14756e9d51481f61a
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-28 16:33:52 +0900

    fix : 큰 수 만들기 문제 풀이 구현 (Greedy) - * 테스트 케이스 10 통과하지 못함

--- Greedy/큰 수 만들기.swift
+++ Greedy/큰 수 만들기.swift
@@ -17,5 +17,28 @@
 import Foundation
 
 func solution(_ number:String, _ k:Int) -> String {
-    return ""
+    guard number != "0" else { // 입력된 숫자가 모두 0일 경우 예외처리
+        return "0"
+    }
+    
+    let arrNumber = Array(number)
+    let len = arrNumber.count - k
+    var result = ""
+    
+    var startIndex = 0
+    for i in 0..<len {
+        var maxNumber: Character = "0"
+
+        for j in startIndex...k+i {
+            if maxNumber < arrNumber[j] { // 각 자리 숫자 하나 하나 뽑을 때마다 그때의 최대값을 선택
+                maxNumber = arrNumber[j]
+                startIndex = j + 1
+            }
+        }
+        
+        //제일 큰수를 정답에 추가
+        result += String(maxNumber)
+    }
+    
+    return result
 }
