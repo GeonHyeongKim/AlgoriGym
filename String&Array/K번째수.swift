8872a2c6f7128caab1a592b609a0d80f70317a3e
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-29 20:21:43 +0900

    fix : K번째수 문제 풀이 아이디어 (Greedy)
    //  Primary idea:       <String & Array>
    //                      1. commands를 반복문으로 돌린다
    //                      2. command의 3가지 값(start, end, target)에서 index조절 값 1을 빼기
    //                      3. 정렬 후 결과값에 append

--- String&Array/K번째수.swift
+++ String&Array/K번째수.swift
@@ -1,7 +1,10 @@
 //
 //  K번째수.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42748
-//  Primary idea:       <Greedy>
+//  Primary idea:       <String & Array>
+//                      1. commands를 반복문으로 돌린다
+//                      2. command의 3가지 값(start, end, target)에서 index조절 값 1을 빼기
+//                      3. 정렬 후 결과값에 append
 //
 //
 //  Time Complexity :
