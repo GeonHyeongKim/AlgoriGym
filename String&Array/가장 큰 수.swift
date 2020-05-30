caf1f5531dcc8432e560f760a8422a7f2c8da55e
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-05-30 15:31:30 +0900

    fix : 가장 큰 수 문제 풀이 구현 (String & Array) - sorted
    swift하게 문제 풀이

--- String&Array/가장 큰 수.swift
+++ String&Array/가장 큰 수.swift
@@ -21,5 +21,11 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> String {
-    return ""
+    let sorted: [Int] = numbers.sorted {Int("\($0)\($1)")! > Int("\($1)\($0)")!}
+
+    if sorted[0] == 0 {
+        return "0"
+    }
+    
+    return sorted.reduce("", {$0 + "\($1)" })
 }
