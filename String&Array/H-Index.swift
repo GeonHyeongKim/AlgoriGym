be43d728409ea364e433ca0f30365e2633a90377
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-01 19:33:09 +0900

    fix : H-index 문제 풀이 아이디어 (String & Array - sorted)
    //  Primary idea:       <String & Array> - sorted
    //                      1. 내림차순으로 정렬한다.
    //                      2. 정렬한 값에서 내림차순 값이 index의 값 보다 작아 질때 return 한다.

--- String&Array/H-Index.swift
+++ String&Array/H-Index.swift
@@ -2,7 +2,8 @@
 //  H-Index.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42747?language=swift
 //  Primary idea:       <String & Array> - sorted
-//                      1.
+//                      1. 내림차순으로 정렬한다.
+//                      2. 정렬한 값에서 내림차순 값이 index의 값 보다 작아 질때 return 한다.
 //
 //  Time Complexity :
 //  Space Complexity : 
