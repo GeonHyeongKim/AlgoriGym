f2ae08e01ee5aacba66a39c6a60f0ed798c6b764
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-02 18:18:12 +0900

    feat : 소수 찾기 문제 풀이 시작 (코딩테스트 고득점 Kit - 프로그래머스)

---
+++ String&Array/소수 찾기.swift
@@ -0,0 +1,19 @@
+//
+//  소수 찾기.swift
+//  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42839?language=swift
+//  Primary idea:       <String & Array> - Brute force
+//                      1.
+//
+//  Time Complexity : O(n)
+//  Space Complexity : O(n)
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/06/02.
+//
+
+import Foundation
+
+func solution(_ numbers:String) -> Int {
+    return 0
+}
