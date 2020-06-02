bd2e4af18516195e43ffe14f60d5f569ac8f8890
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-02 16:47:55 +0900

    feat : 모의고사 문제 풀이 시작 (코딩테스트 고득점 Kit - 프로그래머스)

---
+++ String&Array/모의고사.swift
@@ -0,0 +1,19 @@
+//
+//  모의고사.swift
+//  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42840
+//  Primary idea:       <String & Array> - Brute force
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/06/02.
+//
+
+import Foundation
+
+func solution(_ answers:[Int]) -> [Int] {
+    return []
+}
