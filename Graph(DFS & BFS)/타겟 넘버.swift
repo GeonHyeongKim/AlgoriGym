9954b6568ca822f9d5fda88feef0699583133754
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-07 20:30:26 +0900

    feat : 타겟 넘버 문제 풀이 아이디어
    //  Primary idea:       <DFS & BFS>
    //                      1. dfs로 문제 풀이
    //                      2. node의 크기(index)와 numbers의 갯수가 같을때, sum == target이 같은지 확인
    //                          i) 같으면 1을 return
    //                          ii) 다르면 0을 return
    //                      3. dfs에 sum에 해당값을 더했을경우(왼쪽) + sum에 해당값을 뺏했을경우(오른쪽)값을 return

--- Graph(DFS & BFS)/타겟 넘버.swift
+++ Graph(DFS & BFS)/타겟 넘버.swift
@@ -2,7 +2,11 @@
 //  타겟 넘버.swift
 //  Question Link:https://programmers.co.kr/learn/courses/30/lessons/43165
 //  Primary idea:       <DFS & BFS>
-//                      1.
+//                      1. dfs로 문제 풀이
+//                      2. node의 크기(index)와 numbers의 갯수가 같을때, sum == target이 같은지 확인
+//                          i) 같으면 1을 return
+//                          ii) 다르면 0을 return
+//                      3. dfs에 sum에 해당값을 더했을경우(왼쪽) + sum에 해당값을 뺏했을경우(오른쪽)값을 return
 //
 //  Time Complexity :
 //  Space Complexity :
