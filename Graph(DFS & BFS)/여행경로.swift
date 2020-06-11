3c00f3e8731f4794afe8fd102fb36e5c9f2bc627
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-11 20:31:22 +0900

    fix : 여행경로 문제 풀이 아이디어 (DFS & BFS - DFS)
    //  Primary idea:       <DFS & BFS> - DFS
    //                      1. DFS로 문제 풀이
    //                      2. 중요한 요구 사항 Check
    //                          i) 항상 ICN 공항에서 출발
    //                          ii) 모든 도시를 방문할 수 없는 경우는 주어지지 않습니다. 주어진 항공권은 모두 사용해야 합니다.
    //                          iii) 만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 합니다. --> 정렬 필요
    //                      3. 종료 조건 : 주어진 항공권을 통하여 모든 경로를 탐색하여 롼료한 경우
    //                      4. visited([Bool])을 통해 방문했던 항공사인지 확인

--- Graph(DFS & BFS)/여행경로.swift
+++ Graph(DFS & BFS)/여행경로.swift
@@ -1,8 +1,14 @@
 //
 //  여행경로.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/43164
-//  Primary idea:       <DFS & BFS> -
-//                      1.
+//  Primary idea:       <DFS & BFS> - DFS
+//                      1. DFS로 문제 풀이
+//                      2. 중요한 요구 사항 Check
+//                          i) 항상 ICN 공항에서 출발
+//                          ii) 모든 도시를 방문할 수 없는 경우는 주어지지 않습니다. 주어진 항공권은 모두 사용해야 합니다.
+//                          iii) 만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 합니다. --> 정렬 필요
+//                      3. 종료 조건 : 주어진 항공권을 통하여 모든 경로를 탐색하여 롼료한 경우
+//                      4. visited([Bool])을 통해 방문했던 항공사인지 확인
 //
 //  Time Complexity :
 //  Space Complexity :
