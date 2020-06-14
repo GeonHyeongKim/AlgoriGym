74e00eed715d5b34bb9a2e83e8635fedc89da286
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-14 23:05:42 +0900

    finish :  Best Time to Buy and Sell Stock 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    *  Time Complexity : O(n)
    *  Space Complexity : O(1)
    *  Runtime: 50 ms
    *  Memory Usage: 21.1 MB

--- String&Array/Best Time to Buy and Sell Stock.swift
+++ String&Array/Best Time to Buy and Sell Stock.swift
@@ -1,14 +1,14 @@
 /*
 *  Best Time to Buy and Sell Stock.swift
-*  Question Link: https://leetcode.com/problems/jewels-and-stones/
+*  Question Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 *  Primary idea:    <String & Array>
 *                   1. 2중 for문을 통해 풀기 -> Time Complexity가 O(n^2) (x)
 *                   2. 반복문 하나로 풀기 -> 배열의 최소값(minPrice)을 가지고 있기 -> max()를 통해 최소값이 발견된 시점 부터 최대값 저장하기
 *
 *  Time Complexity : O(n)
 *  Space Complexity : O(1)
-*  Runtime: 40 ms
-*  Memory Usage: 21.4 MB
+*  Runtime: 50 ms
+*  Memory Usage: 21.1 MB
 *
 *  Created by gunhyeong on 2020/04/24.
 */
@@ -17,7 +17,22 @@ import Foundation
 
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
-
+        if prices.isEmpty {
+            return 0
+        }
+        
+        var result = 0
+        var minPrice = prices.max()!
+        
+        for price in prices {
+            if price < minPrice {
+                minPrice = price
+            }
+            
+            result = max(result, price - minPrice)
+        }
+        
         return result
     }
 }
+
