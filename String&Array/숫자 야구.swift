b1f20a6fb50289bf4a0da8a7fdc9ec16bef42989
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-17 23:31:56 +0900

    fix : 숫자 야구 문제 풀이 아이디어 (String & Array - Brute force)
    //  Primary idea:       <String & Array> - Brute force
    //                      1. 최소 123 ~ 최대 987
    //                      2. 모든 경우의 수를 생각해보자
    //                          i) 1~9사의 숫자가 각각1개씩 사용한다
    //                          ii) 0이 들어간경우와 숫자가 겹치는 경우는 경우의 수에서 제거
    //                          iii) 비교해서 스트라이크혹은 볼의 수가 맞지않는다면 경우의 수에서 제거

--- String&Array/숫자 야구.swift
+++ String&Array/숫자 야구.swift
@@ -2,7 +2,11 @@
 //  숫자 야구.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42841
 //  Primary idea:       <String & Array> - Brute force
-//                      1.
+//                      1. 최소 123 ~ 최대 987
+//                      2. 모든 경우의 수를 생각해보자
+//                          i) 1~9사의 숫자가 각각1개씩 사용한다
+//                          ii) 0이 들어간경우와 숫자가 겹치는 경우는 경우의 수에서 제거
+//                          iii) 비교해서 스트라이크혹은 볼의 수가 맞지않는다면 경우의 수에서 제거
 //
 //  Time Complexity :
 //  Space Complexity :
