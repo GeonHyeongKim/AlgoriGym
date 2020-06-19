60b0ebc4085480cadaa58e4e3933a377ef184616
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-19 09:52:17 +0900

    feat : 카페 문제 풀이 구현 (String & Array - brute force)

--- String&Array/카펫.swift
+++ String&Array/카펫.swift
@@ -13,3 +13,33 @@
 //
 
 import Foundation
+
+func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
+    guard 1 <= n && n <= 16 else {
+        return []
+    }
+    
+    let mapSize = n
+    var answer: [String] = []
+    
+    for row in 0..<mapSize {
+        var bit = Array(String(arr1[row]|arr2[row], radix: 2))
+        var str = "" // result
+
+        while bit.count < mapSize {
+            bit.insert("0", at: 0)
+        }
+
+        for block in bit { // transform
+            if block == "1" {
+                str += "#"
+            } else {
+                str += " "
+            }
+        }
+        
+        answer.append(str) 
+    }
+    
+    return answer
+}
