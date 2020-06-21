d3689ab551a649acaf21f20edde4377cc21e16dd
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-21 23:33:00 +0900

    feat : 다리를 지나가는 트럭 문제 풀이 아이디어
    //  Primary idea:       <String & Array> - Stack & Queue
    //                      1. queue를 이용한다.
    //			 2. 시간 변수(time)를 둔다.
    //			 3. 경우의 수를 생각한다.

--- String&Array/다리를 지나는 트럭.swift
+++ String&Array/다리를 지나는 트럭.swift
@@ -2,7 +2,7 @@
 //  다리를 지나는 트럭.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42583
 //  Primary idea:       <String & Array> - Stack & Queue
-//                      1.
+//                      1. queue를 이용한다. 
 //
 //  Time Complexity :
 //  Space Complexity :
@@ -15,5 +15,24 @@
 import Foundation
 
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
-    return 0
+    var trucks = truck_weights
+    var bridge = [Int]() // queue
+    var curWeight = 0
+    var time = 0
+        
+    while !trucks.isEmpty {
+        time += 1
+        
+        if !bridge.isEmpty { // 다리에 truck이 없는 경우
+            
+        }
+        
+        if curWeight + trucks.first! <= weight {
+            curWeight += trucks.first!
+            
+            trucks.removeFirst()
+        }
+    }
+    
+    return time
 }
