6a2a3e1947f2b88da11b460861d24359206e518e
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-21 19:49:33 +0900

    feat : 다리를 지나는 트럭 문제 풀이 시작 (코딩테스트 고득점 Kit - 프로그래머스)

---
+++ String&Array/다리를 지나는 트럭.swift
@@ -0,0 +1,19 @@
+//
+//  다리를 지나는 트럭.swift
+//  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42583
+//  Primary idea:       <String & Array> - Stack & Queue
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/06/21.
+//
+
+import Foundation
+
+func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
+    return 0
+}
