2bdf3ee5016316a1dea9a8cf860135c4d86e5b21
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-22 17:33:33 +0900

    non-finish : 다리를 지나는 트럭 문제 풀이 - 테스트 4, 6에서 실패 [미해결]
    질문하기에 질문했음 - 기다리는중

--- String&Array/다리를 지나는 트럭.swift
+++ String&Array/다리를 지나는 트럭.swift
@@ -3,14 +3,15 @@
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42583
 //  Primary idea:       <String & Array> - Stack & Queue
 //                      1. queue를 이용한다.
-//                      2. 다리에 올라가 있는 무게의 합(bridge.reduce(0, +)) + 다음 트럭의 무게(trucks.first!)가 <= 다리가 버틸수 있는 무게(weight)
+//                      2. 다리에 올라가 있는 무게의 합(bridgeQueue.reduce(0, +)) + 다음 트럭의 무게(trucks.first!)가 <= 다리가 버틸수 있는 무게(weight)
 //                          i) 참
-//                              가) 다리에 다음 차량이 올라간다 : bridge.append(trucks.first!)
-//                              나) 다리에 올라가는 차량이 내려올 시간을 계산 : times.append(time+bridge_length)
+//                              가) 다리에 다음 차량이 올라간다 : bridgeQueue.append(trucks.first!)
+//                              나) 다리에 올라가는 차량이 내려올 시간을 계산 : timesQueue.append(time+bridge_length)
 //                              다) 대기중인 트럭중 첫번째 트럭을 제거 : trucks.removeFirst()
 //                          ii) 거짓
-//                              가) 시간(time)을 차량이 내려올 시간으로 변경 : times.removeFirst(), 단 다음차량이 올라가는 것을 고려하여 1을 빼준다
-//                              나) 다리에서 차량이 내려온다 : bridge.removeFirst()
+//                              가) 시간(time)을 차량이 내려올 시간으로 변경 : timesQueue.removeFirst(), 단 다음차량이 올라가는 것을 고려하여 1을 빼준다
+//                                  point! : firstQueue.first < time (시간을 정지하는 개념)
+//                              나) 다리에서 차량이 내려온다 : bridgeQueue.removeFirst()
 //
 //  Time Complexity :
 //  Space Complexity :
@@ -22,28 +23,27 @@
 
 import Foundation
 
-// 85.7점 - 테스트 4,6,14 실패
+// 85.7점 - 테스트 4,6 실패
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
     var trucks = truck_weights
-    var bridge = [Int]() // queue
-    var times = [Int]()
-    var cross = [Int]()
+    var bridgeQueue = [Int]()
+    var timesQueue = [Int]() // 트럭의 도착 시간
     var time = 0
     
     while !trucks.isEmpty {
         time += 1
         
-        if bridge.reduce(0, +) + (trucks.first!) <= weight {
-            bridge.append(trucks.first!)
-            times.append(time+bridge_length)
+        if bridgeQueue.reduce(0, +) + trucks.first! <= weight {
+            bridgeQueue.append(trucks.first!)
+            timesQueue.append(time+bridge_length)
             trucks.removeFirst()
         } else {
-            time = times.first! < time ? times.removeFirst() : times.removeFirst() - 1
-            cross.append(bridge.removeFirst())
+            time = timesQueue.first! < time ? timesQueue.removeFirst() : timesQueue.removeFirst() - 1
+            bridgeQueue.removeFirst()
         }
     }
     
-    return times.removeLast()
+    return timesQueue.removeLast()
 }
 
-//print("\(time) \(cross) \(bridge) \(trucks) \(times)")
+//print("\(time) \(cross) \(bridgeQueue) \(trucks) \(timesQueue)")
