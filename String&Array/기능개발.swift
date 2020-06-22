d61bb63e33cc345a44f14659df9d4cb5c4f0d9a2
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-22 23:25:08 +0900

    refactor : 기능개발 문제 풀이 직관적인 코드로 변경 아이디어 2 (String & Array - Queue)
    //  Primary idea:       <String & Array> - Queue
    //                      1. 첫 번째 반복문을 통해 각 progresse의 최종 배포일을 알아내 배열에 담아낸다.
    //                      2. 배포일을 알아냄과 동시에 queue에 넣어 더 큰 배포일이 올때까지 반복한다.
    //                         더 큰 배포일이 왔을 경우 : 결과값에 방금 넣은 더 큰 배포일을 제외하고 값을 넣어준다. 그후 큐를 클리어하고 방금 제외한 더 큰 배포일을 queue의 가장 앞에 넣는다.

--- String&Array/기능개발.swift
+++ String&Array/기능개발.swift
@@ -3,7 +3,8 @@
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42586
 //  Primary idea:       <String & Array> - Queue
 //                      1. 첫 번째 반복문을 통해 각 progresse의 최종 배포일을 알아내 배열에 담아낸다.
-//                      2. 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포 된다는 점을 이용해 계산 하는 값보다 작은 큰 값이 나오기 전까지 count를 세면서 result에 넣는다.
+//                      2. 배포일을 알아냄과 동시에 queue에 넣어 더 큰 배포일이 올때까지 반복한다.
+//                         더 큰 배포일이 왔을 경우 : 결과값에 방금 넣은 더 큰 배포일을 제외하고 값을 넣어준다. 그후 큐를 클리어하고 방금 제외한 더 큰 배포일을 queue의 가장 앞에 넣는다.
 //
 //  Time Complexity : O(n)
 //  Space Complexity : O(n)
