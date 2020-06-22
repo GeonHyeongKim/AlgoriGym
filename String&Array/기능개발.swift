12a95cff7befcf96db8cbcbc93ad94f6c446a799
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-22 23:21:06 +0900

    finish : 기능개발 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    //  Time Complexity : O(n)
    //  Space Complexity : O(n)
    //  Runtime: 0.19 ~ 0.33 ms
    //  Memory Usage: 24.2. 24.4 MB

--- String&Array/기능개발.swift
+++ String&Array/기능개발.swift
@@ -5,10 +5,10 @@
 //                      1. 첫 번째 반복문을 통해 각 progresse의 최종 배포일을 알아내 배열에 담아낸다.
 //                      2. 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포 된다는 점을 이용해 계산 하는 값보다 작은 큰 값이 나오기 전까지 count를 세면서 result에 넣는다.
 //
-//  Time Complexity :
-//  Space Complexity :
-//  Runtime:  ms
-//  Memory Usage:  MB
+//  Time Complexity : O(n)
+//  Space Complexity : O(n)
+//  Runtime: 0.19 ~ 0.33 ms
+//  Memory Usage: 24.2. 24.4 MB
 //
 //  Created by gunhyeong on 2020/06/22.
 //
