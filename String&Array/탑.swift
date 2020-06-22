1bdd6d877f9d91e7cd4501765b6a0c027929e592
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-22 20:45:30 +0900

    fix : 탑 문제 풀이 구현 아이디어 (String & Array - Stack)
    //  Primary idea:       <String & Array> - Stack & Queue
    //                      1. 준비 : 0으로 초기화된 결과값을 담을 배열 (result)
    //                      2. 2중 for문으로
    //                          i) 시작을 가장 오른쪽 값부터 시작한여 1씩 작아진다.
    //                          ii) 해당값(i)부터 왼쪽으로 가면서 값이 커질때, 결과값에 넣고 반복문 중지

--- String&Array/탑.swift
+++ String&Array/탑.swift
@@ -2,7 +2,10 @@
 //  탑.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42588
 //  Primary idea:       <String & Array> - Stack & Queue
-//                      1.
+//                      1. 준비 : 0으로 초기화된 결과값을 담을 배열 (result)
+//                      2. 2중 for문으로
+//                          i) 시작을 가장 오른쪽 값부터 시작한여 1씩 작아진다.
+//                          ii) 해당값(i)부터 왼쪽으로 가면서 값이 커질때, 결과값에 넣고 반복문 중지
 //
 //  Time Complexity :
 //  Space Complexity :
