a59c09df15c1e971398e49f2524522b328a81830
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-22 15:44:04 +0900

    fix : 다리를 지나는 트럭 문제풀이 아이디어2
    //  Primary idea:       <String & Array> - Stack & Queue
    //                      1. queue를 이용한다.
    //                      2. 다리에 올라가 있는 무게의 합(bridge.reduce(0, +)) + 다음 트럭의 무게(trucks.first!)가 <= 다리가 버틸수 있는 무게(weight)
    //                          i) 참
    //                              가) 다리에 다음 차량이 올라간다 : bridge.append(trucks.first!)
    //                              나) 다리에 올라가는 차량이 내려올 시간을 계산 : times.append(time+bridge_length)
    //                              다) 대기중인 트럭중 첫번째 트럭을 제거 : trucks.removeFirst()
    //                          ii) 거짓
    //                              가) 시간(time)을 차량이 내려올 시간으로 변경 : times.removeFirst(), 단 다음차량이 올라가는 것을 고려하여 1을 빼준다
    //                              나) 다리에서 차량이 내려온다 : bridge.removeFirst()

--- String&Array/다리를 지나는 트럭.swift
+++ String&Array/다리를 지나는 트럭.swift
@@ -2,7 +2,15 @@
 //  다리를 지나는 트럭.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42583
 //  Primary idea:       <String & Array> - Stack & Queue
-//                      1. queue를 이용한다. 
+//                      1. queue를 이용한다.
+//                      2. 다리에 올라가 있는 무게의 합(bridge.reduce(0, +)) + 다음 트럭의 무게(trucks.first!)가 <= 다리가 버틸수 있는 무게(weight)
+//                          i) 참
+//                              가) 다리에 다음 차량이 올라간다 : bridge.append(trucks.first!)
+//                              나) 다리에 올라가는 차량이 내려올 시간을 계산 : times.append(time+bridge_length)
+//                              다) 대기중인 트럭중 첫번째 트럭을 제거 : trucks.removeFirst()
+//                          ii) 거짓
+//                              가) 시간(time)을 차량이 내려올 시간으로 변경 : times.removeFirst(), 단 다음차량이 올라가는 것을 고려하여 1을 빼준다
+//                              나) 다리에서 차량이 내려온다 : bridge.removeFirst()
 //
 //  Time Complexity :
 //  Space Complexity :
