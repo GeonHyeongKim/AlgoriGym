bc70d780c80979bbe52623679bc1733618b2cbbb
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-23 21:34:49 +0900

    fix : 쇠막대기 문제 풀이 아이디어 (String & Array - Stack)
    //  Primary idea:       <String & Array> - Stack
    //                      1. '(' 가 나오면
    //                          i) 스택에 Push() 해준다.
    //                          ii) ')'이 나오면 스택의 사이즈 만큼 정답에 더해주고, 다음 index는 skip
    //                      2. ')'가 나오면 스택에 removeLast() (즉, pop())을 해서 짝을 맞춰주고 1을 더해준다.

--- String&Array/쇠막대기.swift
+++ String&Array/쇠막대기.swift
@@ -1,8 +1,11 @@
 //
 //  쇠막대기.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42585
-//  Primary idea:       <String & Array> - Stack & Queue
-//                      1.
+//  Primary idea:       <String & Array> - Stack
+//                      1. '(' 가 나오면
+//                          i) 스택에 Push() 해준다.
+//                          ii) ')'이 나오면 스택의 사이즈 만큼 정답에 더해주고, 다음 index는 skip
+//                      2. ')'가 나오면 스택에 removeLast() (즉, pop())을 해서 짝을 맞춰주고 1을 더해준다.
 //
 //  Time Complexity :
 //  Space Complexity :
