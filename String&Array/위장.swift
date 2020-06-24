25586cf85d011cc4ba1324dc49ee9586a509599e
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-24 19:48:51 +0900

    fix : 위장 문제 풀이 아이디어 (String & Array - Hash)
    //  Primary idea:       <String & Array> - Hash
    //                      1. Dictionary를 이용해 [종류 : [상품명]]을 수집
    //                      2. 종류별 갯수를 배열로 만든다.
    //                      3. reduce를 이용해 모든 경우의 수를 구한다.
    //                         i) 각 원소에 +1을 하여 안입은 경우를 표현한다.
    //                         ii) -1을 하여 모두 다 입지 않는 경우를 고려한다.

--- String&Array/위장.swift
+++ String&Array/위장.swift
@@ -2,7 +2,11 @@
 //  위장.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42578
 //  Primary idea:       <String & Array> - Hash
-//                      1.
+//                      1. Dictionary를 이용해 [종류 : [상품명]]을 수집
+//                      2. 종류별 갯수를 배열로 만든다.
+//                      3. reduce를 이용해 모든 경우의 수를 구한다.
+//                         i) 각 원소에 +1을 하여 안입은 경우를 표현한다.
+//                         ii) -1을 하여 모두 다 입지 않는 경우를 고려한다.
 //
 //  Time Complexity :
 //  Space Complexity :
