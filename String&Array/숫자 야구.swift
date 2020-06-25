43608d1e1447c578c58457e71ea64e82c0788453
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-25 16:33:53 +0900

    fix : 숫자 야구 문제 풀이 아이디어 (String & Array - Brute force)
    /  Primary idea:       <String & Array> - Brute force
    //                      1. 최소 123 ~ 최대 987
    //                      2. point!!!
    //                          i) 123 ~ 987까지 각 자릿수가 겹치지 않은 숫자들을 어떻게 만들 것인가? -> 건너 뛴다
    //                              가) 1~9사의 숫자가 각각1개씩 사용한다 : (number[0] == number[1]) || (number[1] == number[2]) || (number[0] == number[2])
    //                              나) 0이 들어간경우와 숫자가 겹치는 경우는 경우의 수 : for char in number where char == "0"
    //                          ii) request, candidate를 비교해서 스트라이크 개수와, 볼 개수를 어떻게 구할 것인가?
    //                              가) 스트라이크 : request[i] == candidate[i]
    //                              나) 볼 : request[i] != candidate[i] && candidate.contains(request[i])

--- String&Array/숫자 야구.swift
+++ String&Array/숫자 야구.swift
@@ -3,10 +3,13 @@
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42841
 //  Primary idea:       <String & Array> - Brute force
 //                      1. 최소 123 ~ 최대 987
-//                      2. 모든 경우의 수를 생각해보자
-//                          i) 1~9사의 숫자가 각각1개씩 사용한다
-//                          ii) 0이 들어간경우와 숫자가 겹치는 경우는 경우의 수에서 제거
-//                          iii) 비교해서 스트라이크혹은 볼의 수가 맞지않는다면 경우의 수에서 제거
+//                      2. point!!!
+//                          i) 123 ~ 987까지 각 자릿수가 겹치지 않은 숫자들을 어떻게 만들 것인가? -> 건너 뛴다
+//                              가) 1~9사의 숫자가 각각1개씩 사용한다 : (number[0] == number[1]) || (number[1] == number[2]) || (number[0] == number[2])
+//                              나) 0이 들어간경우와 숫자가 겹치는 경우는 경우의 수 : for char in number where char == "0"
+//                          ii) request, candidate를 비교해서 스트라이크 개수와, 볼 개수를 어떻게 구할 것인가?
+//                              가) 스트라이크 : request[i] == candidate[i]
+//                              나) 볼 : request[i] != candidate[i] && candidate.contains(request[i])
 //
 //  Time Complexity :
 //  Space Complexity :
