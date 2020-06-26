26978d25e3e77f1d9ca55583411fb8d0eaa5cf5e
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-26 13:50:05 +0900

    feat : 크레인 인형뽑기 게임 시작 (2019 카카오 개발자 겨울 인턴십)

---
+++ String&Array/크레인 인형뽑기 게임.swift
@@ -0,0 +1,14 @@
+//
+//  크레인 인형뽑기 게임.swift
+//  Question Link: https://programmers.co.kr/learn/courses/30/lessons/64061
+//  Primary idea:       <String & Array>
+//                      1.
+//
+//  Created by gunhyeong on 2020/06/26.
+//
+
+import Foundation
+
+func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
+    return 0
+}
