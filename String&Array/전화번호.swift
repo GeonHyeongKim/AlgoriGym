cf35179a3032da4e6a37c01990a75700a75603ee
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-27 22:22:48 +0900

    feat : 전화번호 문제 풀이 시작 (코딩테스트 고득점 Kit - 프로그래머스)

---
+++ String&Array/전화번호.swift
@@ -0,0 +1,19 @@
+//
+//  전화번호.swift
+//  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42577
+//  Primary idea:       <String & Array> - 해쉬
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage: MB
+//
+//  Created by gunhyeong on 2020/06/27.
+//
+
+import Foundation
+
+func solution(_ phone_boo: [String]) -> Bool {
+    return true
+}
