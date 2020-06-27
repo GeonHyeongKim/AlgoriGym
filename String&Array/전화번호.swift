d38284e2c0490ffa0c70957481a999126b9bfbe8
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-27 22:26:15 +0900

    fix : 전화번호 문제 풀이 아이디어 (String & Array - Hash)
    //  Primary idea:       <String & Array> - 해쉬
    //                      1. 2중 반복문을 통해 모든 경우의 수를 계산
    //                      2. 전화번호(number)와 비교할 전화번호가 같은 번호이면 continue
    //                      3. 접두사인지 판별 starts()를 이용

--- String&Array/전화번호.swift
+++ String&Array/전화번호.swift
@@ -2,7 +2,9 @@
 //  전화번호.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42577
 //  Primary idea:       <String & Array> - 해쉬
-//                      1.
+//                      1. 2중 반복문을 통해 모든 경우의 수를 계산
+//                      2. 전화번호(number)와 비교할 전화번호가 같은 번호이면 continue
+//                      3. 접두사인지 판별 starts()를 이용
 //
 //  Time Complexity :
 //  Space Complexity :
