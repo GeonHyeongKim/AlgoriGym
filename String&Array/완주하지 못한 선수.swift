f19e27a37112479a69062932f9577d314c503fd3
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-29 00:25:27 +0900

    fix : 완주하지 못한 선수 문제 풀이 아이디어 (String & Array)
    //  Primary idea:       <String & Array> - Hash
    //                      1. 반복문을 통해 participant의 값을 한번씩 체크한다.
    //                      2. if문과 contains를 이용해 값이 없는지 확인한다.

--- String&Array/완주하지 못한 선수.swift
+++ String&Array/완주하지 못한 선수.swift
@@ -2,7 +2,8 @@
 //  완주하지 못한 선수.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42576
 //  Primary idea:       <String & Array> - Hash
-//                      1.
+//                      1. 반복문을 통해 participant의 값을 한번씩 체크한다.
+//                      2. if문과 contains를 이용해 값이 없는지 확인한다.
 //
 //  Time Complexity :
 //  Space Complexity :
