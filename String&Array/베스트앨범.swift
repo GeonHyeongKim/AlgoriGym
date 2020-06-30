e1338923be4fe090ace30e887e5fdefa22c91a86
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-06-30 14:48:23 +0900

    fix : 베스트앨범 문제 풀이 아이디어 (String & Array - Hash)
    //  Primary idea:       <String & Array> - Hash
    //                      1. 종류별 노래 리스트를 Dictionary에 저장한다.
    //                      2. Dictionary에서 종류에 따른 노래 리스트를 오름차순으로 정렬한다.
    //                      3. 정렬 후, 각 노래리스트의 플레이수를 합친값이 큰 종류순으로 정렬하여 종류 리스트를 알아낸다.
    //                      4. 정렬된 종류 리스트를 반복문으로 돌면서 play에서 해당 index값을 결과에 넣는다. 이때, 두개씩만 넣기 위해 카운트 변수를 둔다

--- String&Array/베스트앨범.swift
+++ String&Array/베스트앨범.swift
@@ -1,8 +1,11 @@
 //
 //  베스트앨범.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42579
-//  Primary idea:       <String & Array> - Brute force
-//                      1.
+//  Primary idea:       <String & Array> - Hash
+//                      1. 종류별 노래 리스트를 Dictionary에 저장한다.
+//                      2. Dictionary에서 종류에 따른 노래 리스트를 오름차순으로 정렬한다.
+//                      3. 정렬 후, 각 노래리스트의 플레이수를 합친값이 큰 종류순으로 정렬하여 종류 리스트를 알아낸다.
+//                      4. 정렬된 종류 리스트를 반복문으로 돌면서 play에서 해당 index값을 결과에 넣는다. 이때, 두개씩만 넣기 위해 카운트 변수를 둔다
 //
 //  Time Complexity :
 //  Space Complexity :
