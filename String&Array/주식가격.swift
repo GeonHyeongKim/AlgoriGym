ca3188818a8d9d74f01c5fc576f5e51f79dacd91
김건형 <show11837@gmail.com>
2020-07-01 22:09:58 +0900

    fix : 주식가격 문제 풀이 아이디어 (String & Array)
    //  Primary idea:       <String & Array>
    //                      1. 반복문을 통해 1초가 흐를떄마다 각각의 방의 값을 1씩 늘려간다.
    //                      2. 값이 작아지는 구간에 반복문으로 멈춘다.

--- String&Array/주식가격.swift
+++ String&Array/주식가격.swift
@@ -1,8 +1,9 @@
 //
 //  주식가격.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42584
-//  Primary idea:       <String & Array> - Stack & Queue
-//                      1.
+//  Primary idea:       <String & Array>
+//                      1. 반복문을 통해 1초가 흐를떄마다 각각의 방의 값을 1씩 늘려간다.
+//                      2. 값이 작아지는 구간에 반복문으로 멈춘다.
 //
 //  Time Complexity :
 //  Space Complexity :
