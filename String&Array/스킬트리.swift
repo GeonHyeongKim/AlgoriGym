a74150b2ad591298e3294905c9457f1212de3fcb
김건형 <show11837@gmail.com>
2020-07-01 17:21:34 +0900

    fix : 스킬트리 문제 풀이 아이디어 (String&Array)
    //  Primary idea:       <String & Array>
    //                      1. skill tree 배열의 엘리먼트 수 만큼 for문을 돌리며 순차적으로 각 엘리먼트에 접근한다.
    //                      2. 1번에서 순차적으로 접근한 엘리먼트들을 각 skillTree 라고 하자.
    //                      3. 이 skillTree에서 skill에 포함되는 요소를 learned에 담는다. (skill : abc, skillTree : zdawcb라면 learned는 acb가 된다)
    //                      4. 만약 skill이 learned로 시작한다면 correctCount를 1 증가시켜준다.

--- String&Array/스킬트리.swift
+++ String&Array/스킬트리.swift
@@ -1,8 +1,12 @@
 //
 //  스킬트리.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/49993
-//  Primary idea:       <> -
-//                      1.
+//  Primary idea:       <String & Array>
+//                      1. skill tree 배열의 엘리먼트 수 만큼 for문을 돌리며 순차적으로 각 엘리먼트에 접근한다.
+//                      2. 1번에서 순차적으로 접근한 엘리먼트들을 각 skillTree 라고 하자.
+//                      3. 이 skillTree에서 skill에 포함되는 요소를 learned에 담는다. (skill : abc, skillTree : zdawcb라면 learned는 acb가 된다)
+//                      4. 만약 skill이 learned로 시작한다면 correctCount를 1 증가시켜준다. (스킬을 배우는 순서도 고려해야하기 때문에 -> 만약 skill : abc 이고 learned : bc 라면 skill은 learned를 포함하고 순서도 맞지만, 선행스킬 a를 배우지 않았기 때문에 이는 틀린 예시가 된다.)
+
 //
 //  Time Complexity :
 //  Space Complexity :
