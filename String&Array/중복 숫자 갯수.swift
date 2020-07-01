bfed6c9e470cea54b78d041ba599d0b58be4ac0b
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-07-01 13:51:34 +0900

    fix : 중복 숫자 갯수 문제 풀이 아이디어 (String & Array) - Hash

--- String&Array/중복 숫자 갯수.swift
+++ String&Array/중복 숫자 갯수.swift
@@ -2,7 +2,9 @@
 //  중복 숫자 갯수.swift
 //  Question Link: https://blog.naver.com/boostcamp_official/221978031932
 //  Primary idea:       <String & Array> - Hash
-//                      1.
+//                      1. dictionary를 사용해 종류 별 갯수를 구별한다.
+//                      2. dictionary를 정렬후, 1이 아닌 값들을 모은다.
+//                      3. 만약 중복된 갯수가 없을 경우 -1을 return한다
 //
 //  Time Complexity :
 //  Space Complexity :
