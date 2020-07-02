31a293578c41a2c8062981d58b00ffdd0d57b9cb
김건형 <show11837@gmail.com>
2020-07-02 22:03:23 +0900

    fix : h-1 문제 풀이 구현

--- Test/h-1.swift
+++ Test/h-1.swift
@@ -13,3 +13,36 @@
 //
 
 import Foundation
+
+func solution(_ phone_number: [String]) -> [Int] {
+    var answer = [Int](repeating: 0, count: phone_number.count)
+
+    for (i,number) in phone_number.enumerated() {
+        let removeChar = number.components(separatedBy: ["-", "+"])
+        let firstNumber = removeChar.first!
+
+        if removeChar.count == 1 { // 010XXXXXXXX
+            if firstNumber.count == 11 {
+                answer[i] = 2
+                continue
+            }
+        } else if firstNumber == "010" { // 010-XXXX-XXXX
+            if removeChar[1].count == 4 && removeChar[2].count == 4 {
+                answer[i] = 1
+                continue
+            }
+        } else if removeChar[1] == "82" && removeChar[2] == "10" { // +82-10-XXXX-XXXX
+            if removeChar[3].count == 4 && removeChar[4].count == 4 {
+                answer[i] = 3
+                continue
+            }
+        }
+
+        answer[i] = -1
+    }
+
+    return answer
+}
+
+
+print(solution(["01012345678", "010-1212-333", "+82-10-3434-2323", "+82-010-3434-2323"])) // [2, -1, 3, -1]
