6e7b30c861630d6d186d159ae31b86cf98edc967
김건형 <show11837@gmail.com>
2020-07-02 22:03:00 +0900

    fix : h-1 문제 풀이 아이디어

--- Test/h-1.swift
+++ Test/h-1.swift
@@ -1,7 +1,10 @@
 //
 //  h-1.swift
 //  Primary idea:       <String & Array>
-//                      1.
+//                      1. 문자를 다 제거한다.
+//                      2. 숫자만 남은 number에서 첫번째 배열값이 1개면서 그 배열값의 길이가 11개 이면 2유형 // 010XXXXXXXX
+//                      3. 첫번재 배열값이 3개이면서 나머지 배열값의 길이가 4개일 경우 1유형 // 010-XXXX-XXXX
+//                      4. "", "82", "10"으로 시작하면서 나머지 배열값의 길이가 4개일 경우 3유형 // +82-10-XXXX-XXXX
 //
 //  Time Complexity :
 //  Space Complexity :
