5244cf4eedeff16add20d96182e0b272b3f23741
김건형 <show11837@gmail.com>
2020-07-03 21:32:29 +0900

    fix : 가운데 글자 가져오기 문제 풀이 아이디어 (String & Array)
    //  Primary idea:       <String & Array>
    //                      1. 글자수가 짝수인지 아닌지 확인한다
    //                          i) 짝수 : 중간 인덱스와 중간 인덱스보다 1작은 값
    //                          i) 홀수 : 중간 인덱스의 값

--- String&Array/가운데 글자 가져오기.swift
+++ String&Array/가운데 글자 가져오기.swift
@@ -2,7 +2,9 @@
 //  가운데 글자 가져오기.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12903
 //  Primary idea:       <String & Array>
-//                      1.
+//                      1. 글자수가 짝수인지 아닌지 확인한다
+//                          i) 짝수 : 중간 인덱스와 중간 인덱스보다 1작은 값
+//                          i) 홀수 : 중간 인덱스의 값
 //
 //  Time Complexity :
 //  Space Complexity :
@@ -15,5 +17,13 @@
 import Foundation
 
 func solution(_ s:String) -> String {
-    return ""
+    let size = s.count
+    let arrS = Array(s)
+    let middle = size/2
+
+    if size % 2 == 0 {
+        return String(arrS[middle-1...middle])
+    } else {
+        return String(arrS[middle])
+    }
 }
