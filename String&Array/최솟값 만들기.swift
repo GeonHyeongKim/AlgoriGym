7192971479a1d9329f6eb607db50f532fc10d0e7
김건형 <show11837@gmail.com>
2020-07-03 16:01:59 +0900

    fix : 최솟값 만들기 문제 풀이 아이디어 (String & Array - sort)
    //  Primary idea:       <String & Array> - 정렬
    //                      1. 매개변수 A,B를 각각 오름차순으로 정렬한다.
    //                      2. 배열의 크기만큼 돌면서 A는 가장 앞에서 부터, B는 가장 뒤에서부터 곱해간다
    //                      3. 곱한값의 누적합을 구한다.

--- String&Array/최솟값 만들기.swift
+++ String&Array/최솟값 만들기.swift
@@ -2,7 +2,9 @@
 //  최솟값 만들기.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12941
 //  Primary idea:       <String & Array>
-//                      1.
+//                      1. 매개변수 A,B를 각각 오름차순으로 정렬한다.
+//                      2. 배열의 크기만큼 돌면서 A는 가장 앞에서 부터, B는 가장 뒤에서부터 곱해간다
+//                      3. 곱한값의 누적합을 구한다.
 //
 //  Time Complexity :
 //  Space Complexity :
