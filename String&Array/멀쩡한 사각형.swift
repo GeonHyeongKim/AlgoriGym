a788e623befcec8ebad3679f572a6acf884c6dbc
김건형 <show11837@gmail.com>
2020-07-03 15:45:00 +0900

    feat : 멀쩡한 사각형 문제 풀이 시작 (코딩테스트 고득점 Kit - 프로그래머스)

---
+++ String&Array/멀쩡한 사각형.swift
@@ -0,0 +1,20 @@
+//
+//  멀쩡한 사각형.swift
+//  Question Link: https://programmers.co.kr/learn/courses/30/lessons/62048?language=swift
+//  Primary idea:       <String & Array>
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/07/03.
+//
+
+import Foundation
+
+func solution(_ w:Int, _ h:Int) -> Int64{
+
+    return Int64(0)
+}
