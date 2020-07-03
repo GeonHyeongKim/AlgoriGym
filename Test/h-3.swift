4ab3cd9272d5ec876af6c3f814c9178bed376735
김건형 <show11837@gmail.com>
2020-07-03 15:28:19 +0900

    fix : h-3 문제 풀이 아이디어 (String & Array) - contains & replacingOccurrences

--- Test/h-3.swift
+++ Test/h-3.swift
@@ -1,7 +1,9 @@
 //
 //  h-3.swift
 //  Primary idea:       <String & Array>
-//                      1.
+//                      1. 문자에 "01"과 "10"이 포함되어 있는지 contains로 확인한다
+//                      2. 01과 10을 ""로 대체한다.
+//                      3. 남은 문자열의 길이를 결과값에 넣는다.
 //
 //  Time Complexity :
 //  Space Complexity :
