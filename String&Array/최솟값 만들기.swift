3ab249469dd990bab068bb29f0631630c80bf108
김건형 <show11837@gmail.com>
2020-07-03 16:03:15 +0900

    finish : 최솟값 만들기 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    //  Time Complexity : O(n)
    //  Space Complexity : O(1)
    //  Runtime: 0.19 ~ 0.74 ms
    //  Memory Usage: 24.0 ~ 24.4 MB

--- String&Array/최솟값 만들기.swift
+++ String&Array/최솟값 만들기.swift
@@ -1,15 +1,15 @@
 //
 //  최솟값 만들기.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12941
-//  Primary idea:       <String & Array>
+//  Primary idea:       <String & Array> - 정렬
 //                      1. 매개변수 A,B를 각각 오름차순으로 정렬한다.
 //                      2. 배열의 크기만큼 돌면서 A는 가장 앞에서 부터, B는 가장 뒤에서부터 곱해간다
 //                      3. 곱한값의 누적합을 구한다.
 //
-//  Time Complexity :
-//  Space Complexity :
-//  Runtime:  ms
-//  Memory Usage: MB
+//  Time Complexity : O(n)
+//  Space Complexity : O(1)
+//  Runtime: 0.19 ~ 0.74 ms
+//  Memory Usage: 24.0 ~ 24.4 MB
 //
 //  Created by gunhyeong on 2020/07/03.
 //
