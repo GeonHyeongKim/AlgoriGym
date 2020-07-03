0c03ebe2a0ba3b49f70849acdcd7eef303277f5f
김건형 <show11837@gmail.com>
2020-07-03 16:16:58 +0900

    fix : 정수 제곱근 판별 문제 풀이 아이디어 (String & Array - sqrt, pow)
    //  Primary idea:       <String & Array> - sqrt, pow
    //                      1. n을 double로 바꾼다.
    //                      2. sqrt를 이용해 제곱근을 구한다.
    //                      3. 제곱급관 소수점을 버린값을 비교하여 제곱근이 더 클 경우 -1을 return, 아닌 경우 제곱근의 1을 더한값의 제곱을 구한다.

--- String&Array/정수 제곱근 판별.swift
+++ String&Array/정수 제곱근 판별.swift
@@ -2,7 +2,9 @@
 //  정수 제곱근 판별.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12934?language=swift
 //  Primary idea:       <String & Array> - sqrt, pow
-//                      1.
+//                      1. n을 double로 바꾼다.
+//                      2. sqrt를 이용해 제곱근을 구한다.
+//                      3. 제곱급관 소수점을 버린값을 비교하여 제곱근이 더 클 경우 -1을 return, 아닌 경우 제곱근의 1을 더한값의 제곱을 구한다.
 //
 //  Time Complexity :
 //  Space Complexity :
