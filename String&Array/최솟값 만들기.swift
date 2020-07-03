64f52cad59384d53bc2ff288cae3ace1b3abdeb4
김건형 <show11837@gmail.com>
2020-07-03 15:56:16 +0900

    feat : 최솟값 만들기 문제 풀이 시작

---
+++ String&Array/최솟값 만들기.swift
@@ -0,0 +1,25 @@
+//
+//  최솟값 만들기.swift
+//  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12941
+//  Primary idea:       <String & Array>
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage: MB
+//
+//  Created by gunhyeong on 2020/07/03.
+//
+
+import Foundation
+
+func solution(_ A:[Int], _ B:[Int]) -> Int
+{
+    var ans = 0
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print("Hello Swift")
+
+    return ans
+}
