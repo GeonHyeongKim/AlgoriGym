bbeb3a5e46ca15b03f0b006ad89a75d3e8ece902
김건형 <show11837@gmail.com>
2020-07-03 17:41:28 +0900

    fix : 두 정수 사이의 합 문제 풀이 아이디어 (String & Array - max(), min())
    //  Primary idea:       <String & Array> - max(), min()
    //                      1. max(), min()을 이용해 큰수와 작은수를 비교한다.
    //                      2. 반복문을 통해 작은수에서 큰수까지의 수를 모두 누적한다.

--- String&Array/두 정수 사이의 합.swift
+++ String&Array/두 정수 사이의 합.swift
@@ -1,8 +1,9 @@
 //
 //  두 정수 사이의 합.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12912
-//  Primary idea:       <String & Array> -
-//                      1.
+//  Primary idea:       <String & Array> - max(), min()
+//                      1. max(), min()을 이용해 큰수와 작은수를 비교한다.
+//                      2. 반복문을 통해 작은수에서 큰수까지의 수를 모두 누적한다.
 //
 //  Time Complexity :
 //  Space Complexity :
