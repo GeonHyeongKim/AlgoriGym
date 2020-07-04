1dab6a332761db59f1b7f9554f57dfb34b9c28cb
김건형 <show11837@gmail.com>
2020-07-04 12:15:16 +0900

    fix : 문자열 내 pdhk y의 개수 문제 풀이 아이디어
    //  Primary idea:       <String & Array>
    //                      1. P(p)와 Y(y)를 담는 변수 두개를 선언
    //                      2. filter() 통해 p,y를 구분해서 배열로 만든다
    //                      3. count를 통해 갯수를 파악

--- String&Array/문자열 내 p와 y의 개수.swift
+++ String&Array/문자열 내 p와 y의 개수.swift
@@ -2,7 +2,9 @@
 //  문자열 내 p와 y의 개수.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12916
 //  Primary idea:       <String & Array>
-//                      1.
+//                      1. P(p)와 Y(y)를 담는 변수 두개를 선언
+//                      2. filter() 통해 p,y를 구분해서 배열로 만든다
+//                      3. count를 통해 갯수를 파악
 //
 //  Time Complexity :
 //  Space Complexity :
