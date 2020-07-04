515829c038807ced4862058831bb7c92328d2bf3
김건형 <show11837@gmail.com>
2020-07-04 13:34:40 +0900

    fix : JadenCase 문자열 만들기 아이디어 (String & Array - lowercased() & isFirst:Bool)
    //  Primary idea:       <String & Array> - lowercased() & isFirst:Bool
    //                      1. lowercased()을 통해 문자열을 소문자로 바꾸어 준다.
    //                      2. isFirst를 통해 공백 뒤에 올 첫문자를 대문자로 바꾸어 준다.
    //                      * 주의! 공백이 연속으로 나올수도 있다. 그렇다면 그 문자열 형태로 반환해야한다.

--- String&Array/JadenCase 문자열 만들기.swift
+++ String&Array/JadenCase 문자열 만들기.swift
@@ -1,8 +1,10 @@
 //
 //  JadenCase 문자열 만들기.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12951
-//  Primary idea:       <String & Array> -
-//                      1.
+//  Primary idea:       <String & Array> - lowercased() & isFirst:Bool
+//                      1. lowercased()을 통해 문자열을 소문자로 바꾸어 준다.
+//                      2. isFirst를 통해 공백 뒤에 올 첫문자를 대문자로 바꾸어 준다.
+//                      * 주의! 공백이 연속으로 나올수도 있다. 그렇다면 그 문자열 형태로 반환해야한다.
 //
 //  Time Complexity :
 //  Space Complexity :
