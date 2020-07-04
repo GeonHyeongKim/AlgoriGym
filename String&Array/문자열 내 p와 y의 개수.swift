ebfad43c4326850a9e0bf368a71b836b2196c178
김건형 <show11837@gmail.com>
2020-07-04 12:16:40 +0900

    fix : 문자열 내 p와 y의 갯수 문제 풀이 구현 (String & Array - filter)

--- String&Array/문자열 내 p와 y의 개수.swift
+++ String&Array/문자열 내 p와 y의 개수.swift
@@ -1,7 +1,7 @@
 //
 //  문자열 내 p와 y의 개수.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12916
-//  Primary idea:       <String & Array>
+//  Primary idea:       <String & Array> - filter
 //                      1. P(p)와 Y(y)를 담는 변수 두개를 선언
 //                      2. filter() 통해 p,y를 구분해서 배열로 만든다
 //                      3. count를 통해 갯수를 파악
@@ -15,3 +15,11 @@
 //
 
 import Foundation
+
+func solution(_ s:String) -> Bool
+{
+    let cntP = s.filter({$0 == "p" || $0 == "P"}).count
+    let cntY = s.filter({$0 == "y" || $0 == "Y"}).count
+
+    return cntP == cntY ? true : false
+}
