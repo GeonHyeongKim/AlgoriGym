72153fe71e06eb48469c7fe92ed7df6b596015e6
김건형 <show11837@gmail.com>
2020-07-05 22:30:48 +0900

    fix : 최댓값 최솟값 문제 풀이 아이디어 (String & Array - split(separator:), map{Int($0)!} )
    //  Primary idea:       <String & Array>
    //                      1. 공백을 제거하고 배열로 만든다. 이떄, Int형으로 바꾸어 배열로 만든다.
    //                      2. min()과 max()를 이용해 최솟값과 최댓값을 알아낸다.
    //                      3. "\()"를 이용해 답을 반환한다.

--- String&Array/최댓값 최솟값.swift
+++ String&Array/최댓값 최솟값.swift
@@ -1,8 +1,11 @@
 //
 //  최댓값 최솟값.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12939
-//  Primary idea:       <String & Array>
-//                      1.
+//  Primary idea:       <String & Array> - split(separator:), map{Int($0)!}
+//                      1. 공백을 제거하고 배열로 만든다. 이떄, Int형으로 바꾸어 배열로 만든다.
+//                      2. min()과 max()를 이용해 최솟값과 최댓값을 알아낸다.
+//                      3. "\()"를 이용해 답을 반환한다.
+//
 //
 //  Time Complexity :
 //  Space Complexity :
