742f7962bf5d620d79e628d2c63fe7b155c81163
김건형 <show11837@gmail.com>
2020-07-05 21:34:24 +0900

    fix : 다음 큰 숫자 문제 풀이 아이디어 (String & Array - String(Int, radix: 2), filter({$0 == "1"}))

--- String&Array/다음 큰 숫자.swift
+++ String&Array/다음 큰 숫자.swift
@@ -2,7 +2,10 @@
 //  다음 큰 숫자.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12911
 //  Primary idea:       <String & Array>
-//                      1.
+//                      1. 10진수인 n을 2진수로 바꿔준다
+//                      2. 1의 갯수를 센다
+//                      3. 반복문으로 n+1 ~ 1,000,001까지 반복하면서 결과 값이 나올때까지 반복한다.
+//                      4. 반복문의 변수를 2진수로 바꾸엇을때 1의 갯수를 세고 n의 2진수로 바꾸엇을때의 갯수와 같은지 비교한다.
 //
 //  Time Complexity :
 //  Space Complexity :
