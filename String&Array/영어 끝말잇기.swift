de14e1ca3e366321c2c0ec100114324c78ab5a10
김건형 <show11837@gmail.com>
2020-07-06 22:03:27 +0900

    fix : 영어 끝말잇기 문제 풀이 수정 (String & Array)
    //  Primary idea:       <String & Array>
    //                      1. words를 한개씩 꺼내며 게임을 진행할 배열에 넣는다.
    //                      2. 첫단어를 제외하고 게임을 진행할 배열에서 마지막으로 게임했던 단어를 가져온다.
    //                      3. 종료 조건
    //                          i) 단어가 게임에 속해있을 경우
    //                          ii) 현재 단어의 첫 알파벳와 마지막 단어의 마지막 알파벳이 다를 경우
    //                      4. 종료 조건에 해당하지않고 반복문이 끝났을 경우, [0,0]을 return

--- String&Array/영어 끝말잇기.swift
+++ String&Array/영어 끝말잇기.swift
@@ -1,8 +1,13 @@
 //
 //  영어 끝말잇기.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12981
-//  Primary idea:       <String & Array> -
-//                      1.
+//  Primary idea:       <String & Array>
+//                      1. words를 한개씩 꺼내며 게임을 진행할 배열에 넣는다.
+//                      2. 첫단어를 제외하고 게임을 진행할 배열에서 마지막으로 게임했던 단어를 가져온다.
+//                      3. 종료 조건
+//                          i) 단어가 게임에 속해있을 경우
+//                          ii) 현재 단어의 첫 알파벳와 마지막 단어의 마지막 알파벳이 다를 경우
+//                      4. 종료 조건에 해당하지않고 반복문이 끝났을 경우, [0,0]을 return
 //
 //
 //  Time Complexity :
