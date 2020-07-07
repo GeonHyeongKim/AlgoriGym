8850ee4dd5a9dd7a8b657af964513414dd3e0029
김건형 <show11837@gmail.com>
2020-07-07 20:32:33 +0900

    feat : N개의 최소 공배수 문제 풀이 구현 - GCD, LCM

--- String&Array/N개의 최소공배수.swift
+++ String&Array/N개의 최소공배수.swift
@@ -2,7 +2,9 @@
 //  N개의 최소공배수.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12953
 //  Primary idea:       <String & Array>
-//                      1.
+//                      1. 두 수의 최대 공약수(GCD)를 구하는 함수를 구현한다.
+//                      2. 두 수의 최소 공배수(LCM)을 구하는 함수를 구현한다.
+//                      3. 초기값 1로 배열의 원소들의 최소 공배수를 구해 답을 return한다.
 //
 //  Time Complexity : O(n)
 //  Space Complexity : O(1)
