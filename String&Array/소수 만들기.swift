62c54a3967325707219176389b027d79629584d2
김건형 <show11837@gmail.com>
2020-07-07 21:05:53 +0900

    feat : 소수 만들기 문제 풀이 시작 (Summer/Winter Coding(~2018))

---
+++ String&Array/소수 만들기.swift
@@ -0,0 +1,24 @@
+//
+//  소수 만들기.swift
+//  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12977
+//  Primary idea:       <String & Array>
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/07/07.
+//
+
+import Foundation
+
+func solution(_ nums:[Int]) -> Int {
+    var answer = 0
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print("Hello Swift")
+
+    return answer
+}
