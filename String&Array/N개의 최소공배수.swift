9088573683137feb8ebf7cb25f9abd73044d5be9
김건형 <show11837@gmail.com>
2020-07-07 20:31:41 +0900

    fix : N개의 최소공배수 문제 풀이 아이디어
    //  Primary idea:       <String & Array>
    //                      1. 두 수의 최대 공약수(GCD)를 구하는 함수를 구현한다.
    //                      2. 두 수의 최소 공배수(LCM)을 구하는 함수를 구현한다.
    //                      3. 초기값 1로 배열의 원소들의 최소 공배수를 구해 답을 return한다.

--- String&Array/N개의 최소공배수.swift
+++ String&Array/N개의 최소공배수.swift
@@ -4,10 +4,10 @@
 //  Primary idea:       <String & Array>
 //                      1.
 //
-//  Time Complexity :
-//  Space Complexity :
-//  Runtime:  ms
-//  Memory Usage:  MB
+//  Time Complexity : O(n)
+//  Space Complexity : O(1)
+//  Runtime: 0.11 ~ 0.18 ms
+//  Memory Usage: 23.6 ~ 24.3 MB
 //
 //  Created by gunhyeong on 2020/07/07.
 //
@@ -15,5 +15,22 @@
 import Foundation
 
 func solution(_ arr:[Int]) -> Int {
-    return 0
+    var lcm = 1
+    
+    for element in arr {
+        lcm = LCM(lcm, element)
+    }
+    
+    return lcm
+}
+
+// 최대 공약수
+func GCD(_ min: Int, _ max: Int) -> Int {
+    let remain = min % max
+    return remain == 0 ? max : GCD(max, remain)
+}
+
+// 최소 공배수
+func LCM(_ base: Int, _ other: Int) -> Int {
+    return base * other / GCD(base, other)
 }
