c6bd907512794e0c659c3baac7ca8898f4c693c1
김건형 <show11837@gmail.com>
2020-07-07 20:10:19 +0900

    feat : N개의 최소공배수 문제 풀이 시작

---
+++ String&Array/N개의 최소공배수.swift
@@ -0,0 +1,19 @@
+//
+//  N개의 최소공배수.swift
+//  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12953
+//  Primary idea:       <String & Array>
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/07/07.
+//
+
+import Foundation
+
+func solution(_ arr:[Int]) -> Int {
+    return 0
+}
