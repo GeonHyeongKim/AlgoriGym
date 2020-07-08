85ad106d9d7cb03defaf896610ca9bd04763f129
김건형 <show11837@gmail.com>
2020-07-08 21:13:00 +0900

    fix : 소수 만들기 문제 풀이 구현 (String & Array) - 3중 for문

--- String&Array/소수 만들기.swift
+++ String&Array/소수 만들기.swift
@@ -16,37 +16,27 @@
 
 import Foundation
 
-// 42.3점 - 나머지 시간초과
 func solution(_ nums:[Int]) -> Int {
-    var result = [[Int]]()
-    var visited = [Bool](repeating: false, count: nums.count)
-    var combination = [Int]()
+    var answer = 0
     
-    dfs(nums.sorted(), &result, &combination, &visited)
-
-    return result.count
-}
-
-func dfs(_ nums:[Int], _ result: inout [[Int]], _ combination: inout [Int], _ visited: inout [Bool]){
-    if combination.count == 3 && !result.contains(combination.sorted()){
-        if isPrime(combination.reduce(0, +)) {
-            result.append(combination)
-        }
-        return
-    }
-    
-    for i in 0..<nums.count {
-        if !visited[i] {
-            visited[i] = true
-            combination.append(nums[i])
-            dfs(nums, &result, &combination, &visited)
-            combination.removeLast()
-            visited[i] = false
+    for first in 0..<nums.count {
+        for second in first+1..<nums.count {
+            for third in second+1..<nums.count {
+                let sum = nums[first] + nums[second] + nums[third]
+                if isPrime(sum) {
+                    answer += 1
+                }
+            }
         }
     }
+
+    return answer
 }
 
 // 소수 인지 판별
 func isPrime(_ number: Int) -> Bool {
     return number > 1 && !(2..<number).contains { number % $0 == 0 }
 }
+
+print(solution([1,2,3,4])) // 1
+print(solution([1,2,7,6,4])) // 4
