3621c82d9ca6cb62cd7cb8583964756f946a9fc2
김건형 <show11837@gmail.com>
2020-07-08 13:57:08 +0900

    fix : 소수 만들기 문제 풀이 아이디어 (DFS)
    //  Primary idea:       <DFS>
    //                      1. DFS를 통해 모든 경우의 수를 구한다.
    //                      2. 경우의 수 중 3개일 경우, 결과값에 포함된 값이 아닌지 확인하고 소수인지 확인 후 저장

--- String&Array/소수 만들기.swift
+++ String&Array/소수 만들기.swift
@@ -1,8 +1,9 @@
 //
 //  소수 만들기.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/12977
-//  Primary idea:       <String & Array>
-//                      1.
+//  Primary idea:       <DFS>
+//                      1. DFS를 통해 모든 경우의 수를 구한다.
+//                      2. 경우의 수 중 3개일 경우, 결과값에 포함된 값이 아닌지 확인하고 소수인지 확인 후 return
 //
 //  Time Complexity :
 //  Space Complexity :
@@ -15,10 +16,35 @@
 import Foundation
 
 func solution(_ nums:[Int]) -> Int {
-    var answer = 0
+    var result = [[Int]]()
+    var visited = [Bool](repeating: false, count: nums.count)
+    var combination = [Int]()
+    
+    dfs(nums.sorted(), &result, &combination, &visited)
 
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print("Hello Swift")
+    return result.count
+}
 
-    return answer
+func dfs(_ nums:[Int], _ result: inout [[Int]], _ combination: inout [Int], _ visited: inout [Bool]){
+    if combination.count == 3 && !result.contains(combination.sorted()){
+        if isPrime(combination.reduce(0, +)) {
+            result.append(combination)
+        }
+        return
+    }
+    
+    for i in 0..<nums.count {
+        if !visited[i] {
+            visited[i] = true
+            combination.append(nums[i])
+            dfs(nums, &result, &combination, &visited)
+            combination.removeLast()
+            visited[i] = false
+        }
+    }
+}
+
+// 소수 인지 판별
+func isPrime(_ number: Int) -> Bool {
+    return number > 1 && !(2..<number).contains { number % $0 == 0 }
 }
