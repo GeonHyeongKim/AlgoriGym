3cd9e4b8ec3701fe089e239a358539bbcbca33e9
김건형 <show11837@gmail.com>
2020-07-09 10:17:05 +0900

    fix : 소수 찾기 문제 풀이 아이디어 (DFS & BFS - DFS)
    //  Primary idea:       <DFS & BFS> - DFS
    //                      1. DFS와 에라토스테네스의 체 이용
    //                      2. DFS를 통해 모든 경우의 수를 구한다.
    //                      3. 해당 숫자가 소수(Prime)인지 아닌지를 판단 후 맞으면 결과값에 저장한다.

---
+++ Graph(DFS & BFS)/소수 찾기.swift
@@ -0,0 +1,23 @@
+//
+//  소수 찾기.swift
+//  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42839?language=swift
+//  Primary idea:       <DFS & BFS> - DFS
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/06/02.
+//
+
+import Foundation
+
+func solution(_ numbers:String) -> Int {
+
+    return result.count
+}
+
+print(solution("17")) // 3
+print(solution("011")) // 2
