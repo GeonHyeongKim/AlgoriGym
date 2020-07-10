8a0ab7d08fbde4468bb0d575d5042c70b48efef1
김건형 <show11837@gmail.com>
2020-07-10 18:06:20 +0900

    feat : 오픈채팅방 문제 풀이 시작(2019 KAKAO BLIND RECRUITMENT)

---
+++ String&Array/오픈채팅방.swift
@@ -0,0 +1,19 @@
+//
+//  오픈채팅방.swift
+//  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42888
+//  Primary idea:       <String & Array>
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/07/10.
+//
+
+import Foundation
+
+func solution(_ record:[String]) -> [String] {
+    return []
+}
