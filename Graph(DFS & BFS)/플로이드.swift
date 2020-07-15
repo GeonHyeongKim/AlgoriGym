80e65444da1f5b85b62588650179bfc5fea66e72
김건형 <show11837@gmail.com>
2020-07-15 14:33:07 +0900

    fix : 플로이드 문제 풀이 아이디어 (Graph)
    //  Primary idea:       (Graph)
    //                      1. 플로이드 워셜을 적용
    //                      2. 입력받은 버스 정보 cntBus개에 대해서 A도시에서 B도시로 가는 기존 백만과 비용의 최솟값을 저장
    //                      3. i에서 k, k에서 j를 가는 경우와 i,k를 가는 경우를 비교

--- Graph(DFS & BFS)/플로이드.swift
+++ Graph(DFS & BFS)/플로이드.swift
@@ -1,8 +1,10 @@
 //
 //  플로이드.swift
 //  Question Link: https://www.acmicpc.net/problem/11404
-//  Primary idea:       <DFS & BFS>
-//                      1.
+//  Primary idea:       <Graph>
+//                      1. 플로이드 워셜을 적용
+//                      2. 입력받은 버스 정보 cntBus개에 대해서 A도시에서 B도시로 가는 기존 백만과 비용의 최솟값을 저장
+//                      3. i에서 k, k에서 j를 가는 경우와 i,k를 가는 경우를 비교
 //
 //  Time Complexity :
 //  Space Complexity :
