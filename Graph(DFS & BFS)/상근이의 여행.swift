c1c2c297d77c32ea2c807179a800df4f17799a40
김건형 <show11837@gmail.com>
2020-07-15 20:38:54 +0900

    fix : 상근이의 여행 문제 풀이 (Graph - BFS)

--- Graph(DFS & BFS)/상근이의 여행.swift
+++ Graph(DFS & BFS)/상근이의 여행.swift
@@ -1,8 +1,9 @@
 //
 //  상근이의 여행.swift
 //  Question Link: https://www.acmicpc.net/problem/9372
-//  Primary idea:       <Graph>
-//                      1.
+//  Primary idea:       <Graph> - BFS
+//                      1. BFS로 풀이한다.
+//                      2. 현재 baseAirplane에서 다른 otherAirplane로 왕복 가능한 비행기들이 주어지고 출발점부터 모든 노드까지 이동 가능한                         경로를 센다
 //
 //  Time Complexity :
 //  Space Complexity :
