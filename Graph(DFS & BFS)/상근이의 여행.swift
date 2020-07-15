514dae3ea4d740f693c41071ace13b273c3177be
김건형 <show11837@gmail.com>
2020-07-15 20:43:07 +0900

    finish : 상근이의 여행 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    //  Time Complexity : O(n^n)
    //  Space Complexity : O(n)
    //  Runtime: 500 ms
    //  Memory Usage: 81.888 MB

--- Graph(DFS & BFS)/상근이의 여행.swift
+++ Graph(DFS & BFS)/상근이의 여행.swift
@@ -5,10 +5,10 @@
 //                      1. BFS로 풀이한다.
 //                      2. 현재 baseAirplane에서 다른 otherAirplane로 왕복 가능한 비행기들이 주어지고 출발점부터 모든 노드까지 이동 가능한                         경로를 센다
 //
-//  Time Complexity :
-//  Space Complexity :
-//  Runtime:  ms
-//  Memory Usage:  MB
+//  Time Complexity : O(n^n)
+//  Space Complexity : O(n)
+//  Runtime: 500 ms
+//  Memory Usage: 81.888 MB
 //
 //  Created by gunhyeong on 2020/07/15.
 //
