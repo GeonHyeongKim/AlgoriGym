a7abe12f7b6bb24e46bb3e985d2ee8a9bde67b4b
김건형 <show11837@gmail.com>
2020-07-20 01:41:10 +0900

    feat : 행성 터널 문제 풀이 시작

---
+++ Greedy/행성 터널.swift
@@ -0,0 +1,15 @@
+//
+//  행성 터널.swift
+//  Question Link: https://www.acmicpc.net/problem/2887
+//  Primary idea:       <Greedy> & <MST>
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/07/20.
+//
+
+import Foundation
