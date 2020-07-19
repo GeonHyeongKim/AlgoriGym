5b72876fa95a87c06211539e580a3b5b7b7db824
김건형 <show11837@gmail.com>
2020-07-19 23:16:19 +0900

    feat : 우주신과의 교감 문제 풀이 시작

---
+++ Greedy/우주신과의 교감.swift
@@ -0,0 +1,15 @@
+//
+//  우주신과의 교감.swift
+//  Question Link: https://www.acmicpc.net/problem/1774
+//  Primary idea:       <Greedy> & <MST>
+//
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage: MB
+//
+//  Created by gunhyeong on 2020/07/19.
+//
+
+import Foundation
