9e32aa3fe78163001fe0bb6e459ee6420c7777c2
김건형 <show11837@gmail.com>
2020-07-20 01:36:25 +0900

    fix : 우주신과의 교감 문제 풀이 구현 (Greedy & MST)

--- Greedy/우주신과의 교감.swift
+++ Greedy/우주신과의 교감.swift
@@ -21,3 +21,93 @@
 //
 
 import Foundation
+
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+let n = input[0]
+let m = input[1]
+
+var edges = [Edge]()
+var universe = [Int](repeating: 0, count: n+1)
+var gods = [Int:[Int]]()
+
+var needDistance = 0.0
+
+// 신들의 위치 저장
+for i in 1...n {
+    let god = readLine()!.split(separator: " ").map{Int($0)!}
+    gods[i] = god
+}
+
+for i in 1...n {
+    universe[i] = i
+}
+
+// 이미 연결되어 있음
+for _ in 0..<m {
+    let connect = readLine()!.split(separator: " ").map{Int($0)!}
+    let start = connect[0]
+    let end = connect[1]
+    
+    union(&universe, start, end)
+}
+
+for start in 1..<n {
+    for end in start+1...n {
+        let distance = getDistance(gods[start]!, gods[end]!)
+        edges.append(Edge(start, end, distance))
+    }
+}
+
+edges = edges.sorted(by: {$0.distance < $1.distance})
+
+for edge in edges {
+    let start = find(&universe, edge.start)
+    let end = find(&universe, edge.end)
+    
+    if start != end {
+        union(&universe, start, end)
+        needDistance += edge.distance
+    }
+}
+
+print(String(format: "%.2f", needDistance))
+
+class Edge {
+    var start: Int
+    var end: Int
+    var distance: Double
+    
+    init(_ start: Int, _ end: Int, _ distance: Double) {
+        self.start = start
+        self.end = end
+        self.distance = distance
+    }
+}
+
+func getDistance(_ start: [Int], _ end: [Int]) -> Double {
+    let startDx = Double(start.first!)
+    let endDx = Double(end.first!)
+    let startDy = Double(start.last!)
+    let endDy = Double(end.last!)
+    
+    return sqrt(pow(startDx - endDx, 2) + pow(startDy - endDy, 2))
+}
+
+func find(_ universe: inout [Int], _ vertex: Int) -> Int {
+    if universe[vertex] == vertex {
+        return vertex
+    }
+    
+    universe[vertex] = find(&universe, universe[vertex])
+    
+    return universe[vertex]
+}
+
+func union(_ universe: inout [Int], _ start: Int, _ end: Int) {
+    let startVertex = find(&universe, start)
+    let endVertex = find(&universe, end)
+    
+    if startVertex != endVertex {
+        universe[startVertex] = endVertex
+    }
+}
