cbfce3669de7a30d6c4050d05f1401dc313ae86b
김건형 <show11837@gmail.com>
2020-07-19 21:46:23 +0900

    fix : 별자리 만들기 문제 풀이 아이디어 (Greedy & MST & 강한 연결 요소 & 최대 독립 집합)
    //  Primary idea:       <Greedy> & <MST> & <강한 연결 요소> & <최대 독립 집합>
    //                      1. 좌표평면에서 MST를 만드는 문제
    //                      2. MST를 적용하여, 좌표 평명 값의 떨어진 거리를 Double 타입의 값으로 저장하고 비교

--- Greedy/별자리 만들기.swift
+++ Greedy/별자리 만들기.swift
@@ -1,8 +1,9 @@
 //
 //  별자리 만들기.swift
 //  Question Link: https://www.acmicpc.net/problem/4386
-//  Primary idea:       <Greedy> & <MST> & <강한 연결 요소> <최대 독립 집합>
-//                      1.
+//  Primary idea:       <Greedy> & <MST> & <강한 연결 요소> & <최대 독립 집합>
+//                      1. 좌표평면에서 MST를 만드는 문제
+//                      2. MST를 적용하여, 좌표 평명 값의 떨어진 거리를 Double 타입의 값으로 저장하고 비교
 //
 //  Time Complexity :
 //  Space Complexity :
