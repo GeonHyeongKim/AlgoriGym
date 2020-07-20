5e5aa9a7cfb274389d022cd138c725616ec90282
김건형 <show11837@gmail.com>
2020-07-20 14:36:29 +0900

    unfinish : 행성 터널 문제 풀이 미해결 - 시간초과(언어적 한계)

--- Greedy/행성 터널.swift
+++ Greedy/행성 터널.swift
@@ -4,6 +4,7 @@
 //  Primary idea:       <Greedy> & <MST>
 //                      1. 문제의 특성을 활용하여 고려할 간선의 개수를 줄임으로써 푸는 문제
 //                         point : n이 100,000개까지 가능 -> 메모리 초과
+//                         해결 방안 : 좌표값을 각각 정렬하여 활용하는 방법
 //                      2. MST(최소 스패닝 트리) 중 Kruskal로 해결한다.
 //                      3. Kruskal 알고리즘을 위한 Union-find를 구현한다.
 //                      4. 간선(edge.cost)을 기준으로 오름차순으로 정렬
@@ -11,8 +12,8 @@
 //                      6. 모든 간선을 검사해서 start와 end가 연결되지 않았으면 연결해준다.
 //                         이 때 오름차순으로 정렬했기 때문에 간선의 최소비용을 구할 수 있다.
 //
-//  Time Complexity :
-//  Space Complexity :
+//  Time Complexity : O(E logE)
+//  Space Complexity : O(E)
 //  Runtime:  ms
 //  Memory Usage:  MB
 //
@@ -23,27 +24,35 @@ import Foundation
 
 let cntPlanet = Int(readLine()!)!
 var universe = [Int](repeating: 0, count: cntPlanet)
-var posList = [[Int]]()
+var planets = [Planet]()
 var edges = [Edge]()
 var cost = 0
 
 for i in 0..<cntPlanet {
+    let pos = readLine()!.split(separator: " ").map{Int($0)!}
+    planets.append(Planet(i, pos[0], pos[1], pos[2]))
     universe[i] = i
 }
 
-for _ in 0..<cntPlanet {
-    let pos = readLine()!.split(separator: " ").map{Int($0)!}
-    posList.append(pos)
+planets.sort(by: {$0.x < $1.x})
+for i in 1..<cntPlanet {
+    let cost = abs(planets[i].x - planets[i-1].x)
+    edges.append(Edge(planets[i].id, planets[i-1].id, cost))
 }
 
-for start in 0..<cntPlanet {
-    for end in start+1..<cntPlanet {
-        let cost = minCost(posList[start], posList[end])
-        edges.append(Edge(start, end, cost))
-    }
+planets.sort(by: {$0.y < $1.y})
+for i in 1..<cntPlanet {
+    let cost = abs(planets[i].y - planets[i-1].y)
+    edges.append(Edge(planets[i].id, planets[i-1].id, cost))
 }
 
-edges = edges.sorted(by: {$0.cost < $1.cost})
+planets.sort(by: {$0.z < $1.z})
+for i in 1..<cntPlanet {
+    let cost = abs(planets[i].z - planets[i-1].z)
+    edges.append(Edge(planets[i].id, planets[i-1].id, cost))
+}
+
+edges.sort(by: {$0.cost < $1.cost})
 
 for edge in edges {
     let start = find(&universe, edge.start)
@@ -57,6 +66,20 @@ for edge in edges {
 
 print(cost)
 
+class Planet {
+    var id: Int
+    var x: Int
+    var y: Int
+    var z: Int
+    
+    init(_ id: Int, _ x: Int, _ y: Int, _ z: Int) {
+        self.id = id
+        self.x = x
+        self.y = y
+        self.z = z
+    }
+}
+
 class Edge {
     var start: Int
     var end: Int
@@ -67,14 +90,6 @@ class Edge {
         self.end = end
         self.cost = cost
     }
-}
-
-func minCost(_ start: [Int], _ end: [Int]) -> Int {
-    let absDx = abs(start[0] - end[0])
-    let absDy = abs(start[1] - end[1])
-    let absDz = abs(start[2] - end[2])
-    
-    return min(min(absDx, absDy), absDz)
 }
 
 func find(_ universe: inout [Int], _ vertex: Int) -> Int {
@@ -90,7 +105,5 @@ func union(_ universe: inout [Int], _ start: Int, _ end: Int) {
     let startVertex = find(&universe, start)
     let endVertex = find(&universe, end)
     
-    if startVertex != endVertex {
-        universe[startVertex] = endVertex
-    }
+    universe[startVertex] = endVertex
 }
