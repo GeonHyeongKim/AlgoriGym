29585d37a5af240eb4c58394b704613136ed61c9
김건형 <show11837@gmail.com>
2020-07-21 12:55:38 +0900

    fix :  친구 네트워크 문제 풀이 구현

--- Greedy/친구 네트워크.swift
+++ Greedy/친구 네트워크.swift
@@ -19,3 +19,76 @@
 //
 
 import Foundation
+
+var cntTestCase = Int(readLine()!)!
+let maxRelation = 100000 + 1
+var network = [Int](repeating: 0, count: maxRelation)
+var setSize = [Int](repeating: 0, count: maxRelation)
+
+while cntTestCase > 0 {
+    cntTestCase -= 1
+    var cntRelationship = Int(readLine()!)!
+    var infoFriend = [String:Int]()
+    var id = 0
+
+    // 초기화
+    for i in 0..<maxRelation {
+        network[i] = i
+        setSize[i] = 1
+    }
+    
+    while cntRelationship > 0 {
+        cntRelationship -= 1
+        let friends = readLine()!.split(separator: " ").map{String($0)}
+        
+        // friend 정보 입력
+        if !infoFriend.keys.contains(friends[0]) {
+            infoFriend[friends[0]] = id
+            id += 1
+        }
+        
+        // friend 정보 입력
+        if !infoFriend.keys.contains(friends[1]) {
+            infoFriend[friends[1]] = id
+            id += 1
+        }
+                
+        let one = find(&network, infoFriend[friends[0]]!)
+        let other = find(&network, infoFriend[friends[1]]!)
+    
+        if one == other { // 다른 그룹이라면
+            print(max(setSize[one], setSize[other]))
+        } else {
+            merge(&network, &setSize, one, other)
+            print(max(setSize[one], setSize[other]))
+        }
+    }
+}
+
+func find(_ network: inout [Int], _ friend: Int) -> Int {
+    if network[friend] == friend {
+        return friend
+    }
+    
+    network[friend] = find(&network, network[friend])
+    return network[friend]
+}
+
+func merge(_ network: inout [Int], _ setSize: inout [Int], _ one: Int, _ other: Int) {
+    var one = find(&network, one)
+    var other = find(&network, other)
+    
+    if one != other { // 같은 그룹이 아닐 경우,
+        
+        // 크기가 더 큰 그룹에 작은 그룹이 합류
+        if setSize[one] < setSize[other] {
+            let temp = one
+            one = other
+            other = temp
+        }
+        
+        network[other] = one // other 그룹(작은 그룹)이 one 그룹(더큰 그룸)에 합쳐짐
+        setSize[one] += setSize[other] // one의 집합 크기가 other의 집합 크기를 가져옴
+        setSize[other] = 0
+    }
+}
