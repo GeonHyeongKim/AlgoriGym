ce361c4983e64aa7a3ca1a0eaf94a5b49be15292
김건형 <show11837@gmail.com>
2020-07-22 20:07:56 +0900

    fix : 가장 먼 노드 문제 풀이 아이디어  ( Graph & Dijstra & BFS )
    //  Primary idea:       ( Graph & Dijstra & BFS )
    //                      1. 1번 정점부터 각 정점까지의 최단거리 -> 다익스트라
    //                      2. 마지막에 최단거리 배열값에 있는 값중에 가장 먼 노드들 찾아 출력

--- Graph(DFS & BFS)/가장 먼 노드.swift
+++ Graph(DFS & BFS)/가장 먼 노드.swift
@@ -2,7 +2,8 @@
 //  가장 먼 노드.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/49189
 //  Primary idea:       ( Graph & Dijstra & BFS )
-//                      1.
+//                      1. 1번 정점부터 각 정점까지의 최단거리 -> 다익스트라
+//                      2. 마지막에 최단거리 배열값에 있는 값중에 가장 먼 노드들 찾아 출력
 //
 //  Time Complexity :
 //  Space Complexity :
