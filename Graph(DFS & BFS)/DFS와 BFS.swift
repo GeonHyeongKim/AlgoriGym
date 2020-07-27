6c20725d3c6dc1adce2ccc42d8299bb4e4845069
김건형 <show11837@gmail.com>
2020-07-27 21:38:05 +0900

    feat : DFS와 BFS 문제 풀이 아이디어 (DFS & BFS)
    DFS : Stack
    BFS : Queue
    visited : 방문을 check 하는 배열

--- Graph(DFS & BFS)/DFS와 BFS.swift
+++ Graph(DFS & BFS)/DFS와 BFS.swift
@@ -1,7 +1,7 @@
 //
 //  DFS와 BFS(DFS).swift
 //  Question Link: https://www.acmicpc.net/problem/1260
-//  Primary idea:       <DFS & BFS> - dfs
+//  Primary idea:       <DFS & BFS>
 //                      1.
 //
 //  Time Complexity :
@@ -13,3 +13,55 @@
 //
 
 import Foundation
+
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+let n = input[0]
+let m = input[1]
+let v = input[2]
+
+var visited = [Bool](repeating: false, count: 100001)
+var graph = [[Int]](repeating: [Int](repeating: 0, count: 1001), count: 1001)
+
+
+for _ in 1...m {
+    let input = readLine()!.split(separator: " ").map{Int($0)!}
+    let start = input[0]
+    let end = input[1]
+    graph[start][end] = 1
+    graph[end][start] = 1
+}
+
+dfs(v)
+print()
+visited = [Bool](repeating: false, count: 100001)
+bfs(v)
+
+func dfs(_ start: Int) {
+    visited[start] = true
+    
+    print(start, terminator: " ")
+    
+    for i in 1...m {
+        if !visited[i] && graph[start][i] == 1 {
+            dfs(i)
+        }
+    }
+}
+
+
+func bfs(_ start: Int) {
+    var queue = [Int]()
+    queue.append(start)
+    visited[start] = true
+    
+    while !queue.isEmpty {
+        let vertex = queue.removeFirst()
+        print(vertex, terminator: " ")
+        for i in 1...m {
+            if !visited[i] && graph[start][i] == 1 {
+                queue.append(i)
+                visited[i] = true
+            }
+        }
+    }
+}
