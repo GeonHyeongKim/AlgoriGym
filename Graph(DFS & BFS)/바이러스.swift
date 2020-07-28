049b5be586f71f1484bb411069f932f50b0f73ea
김건형 <show11837@gmail.com>
2020-07-29 01:16:57 +0900

    feat : 바이러스 문제 풀이 아이디어 (DFS & BFS)

---
+++ Graph(DFS & BFS)/바이러스.swift
@@ -0,0 +1,20 @@
+//
+//  바이러스.swift
+//  Question Link: https://www.acmicpc.net/problem/2606
+//  Primary idea:       (DFS & BFS)
+//                      1. DFS로 문제를 풀어본다.
+//                      2. 인접행렬로 그래프를 만든다.
+//                      3. 방문한 장소를 구별하기 위해 visited라는 Bool 배열을 만든다.
+//                      4. 1번과 연결된 컴퓨터의 숫자만 count하면 되기 때문에 dfs를 활용
+//
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime: ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/07/29.
+//
+
+import Foundation
+
