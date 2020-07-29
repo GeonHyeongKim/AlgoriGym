f9fd840cffc3e3af02390d667acf8012d31ccbec
김건형 <show11837@gmail.com>
2020-07-29 13:36:47 +0900

    feat : 단지번호붙이기 문제 풀이 구현 (DFS)

--- Graph(DFS & BFS)/단지번호붙이기.swift
+++ Graph(DFS & BFS)/단지번호붙이기.swift
@@ -18,4 +18,52 @@
 //  Created by gunhyeong on 2020/07/29.
 //
 
-import Foundation
+let size = Int(readLine()!)!
+var mapSquare = [[Character]](repeating: [Character](), count: size)
+var visited = [[Bool]](repeating: [Bool](repeating: false, count: size), count: size)
+let dx = [-1, 0, 1, 0]
+let dy = [0, 1, 0, -1]
+var answer = [Int]()
+var cnt = 0
+var cntHouse = 0
+
+for i in 0..<size {
+    let input = readLine()!
+    for num in input {
+        mapSquare[i].append(num)
+    }
+}
+
+for i in 0..<size {
+    for j in 0..<size {
+        if !visited[i][j] && mapSquare[i][j] == "1" {
+            dfs(i, j)
+            cnt += 1
+            answer.append(cntHouse)
+            cntHouse = 0
+        }
+    }
+}
+
+print(cnt)
+for house in answer.sorted() {
+    print(house)
+}
+
+func dfs(_ i: Int, _ j: Int) {
+    visited[i][j] = true
+    cntHouse += 1
+    
+    for dir in 0..<4 {
+        let x = dx[dir] + i
+        let y = dy[dir] + j
+        
+        if x < 0 || x >= size || y < 0 || y >= size {
+            continue
+        }
+        
+        if !visited[x][y] && mapSquare[x][y] == "1" {
+            dfs(x, y)
+        }
+    }
+}
