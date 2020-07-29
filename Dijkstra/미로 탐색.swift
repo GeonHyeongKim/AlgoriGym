b8f213db68e59aaf3a11d8db949208e5d7a2bbd4
김건형 <show11837@gmail.com>
2020-07-29 19:27:55 +0900

    feat : 미로 탐색 문제 풀이 구현 (Graph & BFS & Dijkstra)

--- Dijkstra/미로 탐색.swift
+++ Dijkstra/미로 탐색.swift
@@ -22,3 +22,74 @@
 //
 
 import Foundation
+
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+let row = input.first!
+let col = input.last!
+let dx = [-1, 0, 1, 0]
+let dy = [0, -1, 0, 1]
+let INF = Int.max
+
+var maze = [[Character]](repeating: [Character](), count: row)
+var dist = [[[Int]]](repeating: [[Int]](repeating: [Int](repeating: INF, count: 4), count: 101), count: 101)
+
+for i in 0..<row {
+    let input = readLine()!
+    
+    for num in input {
+        maze[i].append(num)
+    }
+}
+
+// dist[x][y][dirction]
+dist[0][0][0] = 0
+dist[0][0][1] = 0
+dist[0][0][2] = 0
+dist[0][0][3] = 0
+
+//초기화
+var queue = [Point]()
+queue.append(Point(0, 0, 1, 0))
+queue.append(Point(0, 0, 1, 1))
+queue.append(Point(0, 0, 1, 2))
+queue.append(Point(0, 0, 1, 3))
+
+while !queue.isEmpty {
+    let cur = queue.removeFirst()
+    let x = cur.x
+    let y = cur.y
+    let cost = cur.cost
+    let dir = cur.direction
+    
+    for i in 0..<4 {
+        let x = x + dx[dir]
+        let y = y + dy[dir]
+        
+        if abs(dir - i) == 2 { continue }
+        
+        if x < 0 || y < 0 || x >= row || y >= col || maze[x][y] == "0" { continue }
+        
+        let nextCost = cost + 1
+        if dist[x][y][i] > nextCost {
+            dist[x][y][i] = nextCost
+            queue.append(Point(x, y, nextCost, i))
+        }
+    }
+}
+
+print(dist[row-1][col-1].min()!)
+
+struct Point {
+    var x: Int
+    var y: Int
+    var cost: Int
+    var direction: Int
+    
+    init(_ x: Int, _ y: Int, _ cost: Int, _ direction: Int) {
+        self.x = x
+        self.y = y
+        self.cost = cost
+        self.direction = direction
+    }
+}
+
