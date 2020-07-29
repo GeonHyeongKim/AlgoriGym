1fcf576741f7f9c507f569b675ac232b03043282
김건형 <show11837@gmail.com>
2020-07-29 17:43:12 +0900

    feat : 유기농 배추 문제 풀이 아이디어 (DFS)
    //  Primary idea:       (DFS & BFS)
    //                      0. 땅의 모습이 아니라 배추의 위치가 주어지는 문제
    //                      1. DFS로 문제를 풀어본다.
    //                      2. 인접행렬로 그래프를 만든다.
    //                      3. 방문한 장소를 구별하기 위해 visited라는 Bool 배열을 만든다.
    //                      4. 1번과 연결된 컴퓨터의 숫자만 count하면 되기 때문에 dfs를 활용

--- Graph(DFS & BFS)/유기농 배추.swift
+++ Graph(DFS & BFS)/유기농 배추.swift
@@ -2,7 +2,12 @@
 //  유기농 배추.swift
 //  Question Link: https://www.acmicpc.net/problem/1012
 //  Primary idea:       (DFS & BFS)
-//                      1.
+//                      0. 땅의 모습이 아니라 배추의 위치가 주어지는 문제
+//                      1. DFS로 문제를 풀어본다.
+//                      2. 인접행렬로 그래프를 만든다.
+//                      3. 방문한 장소를 구별하기 위해 visited라는 Bool 배열을 만든다.
+//                      4. 1번과 연결된 컴퓨터의 숫자만 count하면 되기 때문에 dfs를 활용
+//
 //  Time Complexity :
 //  Space Complexity :
 //  Runtime: ms
