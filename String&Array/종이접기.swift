0f1f070dad6e8828c138301561db61637348d95d
김건형 <show11837@gmail.com>
2020-07-31 16:19:55 +0900

    feat : 종이 접기 문제 풀이 아이디어 (String & Array & Brute force)
    //  Primary idea:       (String & Array & Brute force)
    //                      1. 규칙성
    //                          i) 무조건 0으로 시작한다.
    //                          ii) 결과 값 중간은 무조건 0이다.
    //                          iii) n은 n-1의 패턴을 가지고있다
    //                          iv) 가운데 0을 기준으로 왼쪽 오른쪽이 보수 관계

--- String&Array/종이접기.swift
+++ String&Array/종이접기.swift
@@ -2,7 +2,11 @@
 //  종이접기.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/62049
 //  Primary idea:       (String & Array & Brute force)
-//                      1.
+//                      1. 규칙성
+//                          i) 무조건 0으로 시작한다.
+//                          ii) 결과 값 중간은 무조건 0이다.
+//                          iii) n은 n-1의 패턴을 가지고있다
+//                          iv) 가운데 0을 기준으로 왼쪽 오른쪽이 보수 관계이
 //
 //  Time Complexity :
 //  Space Complexity :
