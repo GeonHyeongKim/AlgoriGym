78b03785054670b66ffc846003651610a79e313c
김건형 <show11837@gmail.com>
2020-08-01 13:27:45 +0900

    finish : 단속카메라 문제 풀이 끝 (Time&Space Complexity)
    //  Time Complexity : O(n^2)
    //  Space Complexity : O(1)

--- Greedy/단속카메라.swift
+++ Greedy/단속카메라.swift
@@ -5,8 +5,8 @@
 //                      1. 차량이 고속도로에서 나간 지점을 기준으로 routes를 정렬한다.
 //                      2. 차량이 들어온 지점이 기존 카메라 위치보다 우측에 있으면 차량이 나가는 지점에 카메라를 설치한다.
 //
-//  Time Complexity :
-//  Space Complexity :
+//  Time Complexity : O(n^2)
+//  Space Complexity : O(1)
 //  Runtime:  ms
 //  Memory Usage:  MB
 //
