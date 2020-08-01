65eb1247cf293f8cf7b0ce273b23a3797c80bb20
김건형 <show11837@gmail.com>
2020-08-01 13:26:04 +0900

    feat : 단속카메라 문제 풀이 아이디어 (Greedy)
    //  Primary idea:       (Greedy)
    //                      1. 차량이 고속도로에서 나간 지점을 기준으로 routes를 정렬한다.
    //                      2. 차량이 들어온 지점이 기존 카메라 위치보다 우측에 있으면 차량이 나가는 지점에 카메라를 설치한다.

--- Greedy/단속카메라.swift
+++ Greedy/단속카메라.swift
@@ -2,7 +2,8 @@
 //  단속카메라.swift
 //  Question Link: https://programmers.co.kr/learn/courses/30/lessons/42884
 //  Primary idea:       (Greedy)
-//                      1.
+//                      1. 차량이 고속도로에서 나간 지점을 기준으로 routes를 정렬한다.
+//                      2. 차량이 들어온 지점이 기존 카메라 위치보다 우측에 있으면 차량이 나가는 지점에 카메라를 설치한다.
 //
 //  Time Complexity :
 //  Space Complexity :
