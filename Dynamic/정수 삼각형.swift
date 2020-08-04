e7d4bd1b97cb9a2d434739ffffbd4c2e0062e165
김건형 <show11837@gmail.com>
2020-08-04 21:54:40 +0900

    feat : 정수 삼각형 문제 풀이 아이디어 (DP)
    //  Primary idea:       (DP)
    //                      1. DP 2차원 문제
    //                      2. dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j]
    //                         이떄, j(열의 범위)가 j > 0 이여야한다. 이를 방지하기 위해 최대 배열의 값을 501로 한다.

--- Dynamic/정수 삼각형.swift
+++ Dynamic/정수 삼각형.swift
@@ -1,8 +1,10 @@
 //
 //  정수 삼각형.swift
 //  Question Link: https://www.acmicpc.net/problem/1932
-//  Primary idea: (Dynamic)
-//                      1.
+//  Primary idea:       (DP)
+//                      1. DP 2차원 문제
+//                      2. dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j]
+//                         이떄, j(열의 범위)가 j > 0 이여야한다. 이를 방지하기 위해 최대 배열의 값을 501로 한다.
 //
 //  Time Complexity :
 //  Space Complexity :
