f7141910e92c2cb4778adc1473aa7478b2468951
김건형 <show11837@gmail.com>
2020-08-04 21:55:16 +0900

    feat : 정수 삼각형 문제 풀이 구현 (DP)

--- Dynamic/정수 삼각형.swift
+++ Dynamic/정수 삼각형.swift
@@ -14,4 +14,24 @@
 //  Created by gunhyeong on 2020/08/04.
 //
 
-import Foundation
+let row = Int(readLine()!)!
+var arr = [[Int]](repeating: [Int](repeating: 0, count: 501), count: 501)
+var dp = [[Int]](repeating: [Int](repeating: 0, count: 501), count: 501)
+
+for i in 1...row {
+    let col = readLine()!.split(separator: " ").map{Int($0)!}
+    
+    for (j, value) in col.enumerated() {
+        arr[i][j+1] = value
+    }
+}
+
+dp[1][1] = arr[1][1] // 초기화
+
+for i in 2...row {
+    for j in 1...i {
+        dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j]
+    }
+}
+
+print(dp[row].max()!)
