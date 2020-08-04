8d4de351ced540ea178967bd79478ccea7a3a15b
김건형 <show11837@gmail.com>
2020-08-04 20:51:21 +0900

    feat : 정수 삼각형 문제 풀이 시작

---
+++ Dynamic/정수 삼각형.swift
@@ -0,0 +1,15 @@
+//
+//  정수 삼각형.swift
+//  Question Link: https://www.acmicpc.net/problem/1932
+//  Primary idea: (Dynamic)
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/08/04.
+//
+
+import Foundation
