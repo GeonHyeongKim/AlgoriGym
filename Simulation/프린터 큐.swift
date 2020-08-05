71d244292760dc6c5272240fd581d208b51eb3ae
김건형 <show11837@gmail.com>
2020-08-05 17:03:09 +0900

    feat : 프린터 큐 문제 풀이 구현 (Simulation & Brute force & Queue)

--- Simulation/프린터 큐.swift
+++ Simulation/프린터 큐.swift
@@ -16,3 +16,48 @@
 //
 
 import Foundation
+
+var testCase = Int(readLine()!)!
+
+while testCase > 0 {
+    testCase -= 1
+    var queue = [Document]()
+    var answer = 0
+    let input = readLine()!.split(separator: " ").map{Int($0)!}
+//    let n = input.first!
+    let m = input.last!
+    
+    let order = readLine()!.split(separator: " ").map{Int($0)!}
+    
+    for (id, priority) in order.enumerated() {
+        queue.append(Document(id, priority))
+    }
+    
+    var priorityOrder = order.sorted(by: >) // 우선순위
+    
+    while !queue.isEmpty {
+        let id = queue.first!.id
+        let priority = queue.removeFirst().priority
+        
+        if priorityOrder.first! == priority {
+            priorityOrder.removeFirst()
+            answer += 1
+            if id == m {
+                print(answer)
+                break
+            }
+        } else {
+            queue.append(Document(id, priority))
+        }
+    }
+}
+
+struct Document {
+    var id: Int
+    var priority: Int
+    
+    init(_ id: Int, _ priority: Int) {
+        self.id = id
+        self.priority = priority
+    }
+}
