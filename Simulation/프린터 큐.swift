9be606f355e828bd3f457c3317a19aeb0e057707
김건형 <show11837@gmail.com>
2020-08-05 16:03:38 +0900

    feat : 프린터 큐 문제 풀이 시작

---
+++ Simulation/프린터 큐.swift
@@ -0,0 +1,15 @@
+//
+//  프린터 큐.swift
+//  Question Link: https://www.acmicpc.net/problem/1966
+//  Primary idea:       <Simulation & Brute force & Queue>
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/08/05.
+//
+
+import Foundation
