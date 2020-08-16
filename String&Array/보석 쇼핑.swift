8d21e039cd5e9ebdfaaf796e3ff269839008e427
김건형 <show11837@gmail.com>
2020-08-16 23:14:23 +0900

    feat : 보석 쇼핑 문제 풀이 구현
    정확성: 33.3
    효율성: 53.3
    합계: 86.7 / 100.0

--- String&Array/보석 쇼핑.swift
+++ String&Array/보석 쇼핑.swift
@@ -15,5 +15,32 @@
 import Foundation
 
 func solution(_ gems:[String]) -> [Int] {
-    return []
+    var answer = [[Int]]()
+    let setGem = Set(gems)
+    let count = setGem.count
+    var dic = [String:Int]()
+    
+    for (i, gem) in gems.enumerated() {
+        dic[gem] = i + 1
+        
+        if count == dic.count {
+            answer.append([dic.values.min()!, dic.values.max()!])
+        }
+    }
+    
+    answer.sort(by: {
+        if ($0.first! - $0.last!) == ($1.first! - $1.last!) {
+            return $0.first! < $1.first!
+        } else {
+            return ($0.first! - $0.last!) > ($1.first! - $1.last!)
+        }
+        
+    })
+    
+    return answer.first!
 }
+
+print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"])) // [3, 7]
+print(solution(["AA", "AB", "AC", "AA", "AC"])) // [1, 3]
+print(solution(["XYZ", "XYZ", "XYZ"])) // [1, 1]
+print(solution(["ZZZ", "YYY", "NNNN", "YYY", "BBB"])) // [1, 5]
