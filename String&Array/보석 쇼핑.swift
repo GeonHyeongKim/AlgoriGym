52d2e6ea096ded45eb026e96d5651b31f7d48586
김건형 <show11837@gmail.com>
2020-08-16 21:31:05 +0900

    feat : 보석 쇼핑 문제 풀이 시작 (2020 카카오 인턴십)

---
+++ String&Array/보석 쇼핑.swift
@@ -0,0 +1,19 @@
+//
+//  보석 쇼핑.swift
+//  Question Link: https://programmers.co.kr/learn/courses/30/lessons/67258
+//  Primary idea:       (String & Array & Dictionary)
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/08/16.
+//
+
+import Foundation
+
+func solution(_ gems:[String]) -> [Int] {
+    return []
+}
