e02932d114c9a352d31119d72c40135b916f44ba
김건형 <show11837@gmail.com>
2020-08-20 22:50:00 +0900

    feat : 점프 문제 풀이 아이디어 (Dynamic Programming)
    1. 2차원 배열을 사용하여 DP를 만든다. dp[i][j] 배열의 값의 의미는 (i,j)까지 올때까지 걸린 갯수를 말한다.
    2. dp값이 0일 경우와 size를 넘길경우 continue
    3. 값의 변경은 해당값이 size보다 작을 경우 누적값으로 저장해놓는다.
    4. 정답은 dp[size-1][size-1]에 저장되게 된다.

--- Dynamic/점프.swift
+++ Dynamic/점프.swift
@@ -2,7 +2,10 @@
 //  점프.swift
 //  Question Link: https://www.acmicpc.net/problem/1890
 //  Primary idea:       (Dynamic Programming)
-//                      1.
+//                      1. 2차원 배열을 사용하여 DP를 만든다. dp[i][j] 배열의 값의 의미는 (i,j)까지 올때까지 걸린 갯수를 말한다.
+//                      2. dp값이 0일 경우와 size를 넘길경우 continue
+//                      3. 값의 변경은 해당값이 size보다 작을 경우 누적값으로 저장해놓는다.
+//                      4. 정답은 dp[size-1][size-1]에 저장되게 된다.
 //
 //  Time Complexity :
 //  Space Complexity :
