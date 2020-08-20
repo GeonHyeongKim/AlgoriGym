64c76ed067e7f6f2f72fa0aa2633c7ab1e675dfd
김건형 <show11837@gmail.com>
2020-08-20 22:18:55 +0900

    feat : 점프 문제 풀이 시작

---
+++ Dynamic/점프.swift
@@ -0,0 +1,15 @@
+//
+//  점프.swift
+//  Question Link: https://www.acmicpc.net/problem/1890
+//  Primary idea:       (Dynamic Programming)
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/08/20.
+//
+
+import Foundation
