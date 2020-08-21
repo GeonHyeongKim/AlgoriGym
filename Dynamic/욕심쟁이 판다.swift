a2a377d8920b6c0bf19125a539eec7f28eb79eda
김건형 <show11837@gmail.com>
2020-08-21 19:32:40 +0900

    feat : 욕심쟁이 판다 문제 풀이 아이디어 (Dynamic Programming)
    //  Primary idea:
    //                      1. 시작지점에서 동, 서, 남, 북 4방향을 살펴 봅니다.
    //                      2. 4방향 중 숫자가 큰 y, x 를 재귀함수로 call한 후 +1 해줍니다.
    //                      3. 더 큰 숫자가 나타나지 않을 때까지 재귀함수를 call한 후 가장 큰 숫자를 저장하여 출력합니다.

--- Dynamic/욕심쟁이 판다.swift
+++ Dynamic/욕심쟁이 판다.swift
@@ -2,7 +2,9 @@
 //  욕심쟁이 판다.swift
 //  Question Link: https://www.acmicpc.net/problem/1937
 //  Primary idea:       (Dynamic Programming)
-//                      1.
+//                      1. 시작지점에서 동, 서, 남, 북 4방향을 살펴 봅니다.
+//                      2. 4방향 중 숫자가 큰 y, x 를 재귀함수로 call한 후 +1 해줍니다.
+//                      3. 더 큰 숫자가 나타나지 않을 때까지 재귀함수를 call한 후 가장 큰 숫자를 저장하여 출력합니다.
 //
 //  Time Complexity :
 //  Space Complexity :
