ea9b9772c182cb6bb506f4a792c1b82eb20eb48f
김건형 <show11837@gmail.com>
2020-08-24 22:02:49 +0900

    feat : 상자넣기 문제 풀이 아이디어 (DP)
    //  Primary idea:       (DP)
    //                      1. LIS에 대한 개념
    //                      2. 자기보다 이전의 원소들 중 작은 원소들을 하나씩 모두 비교
    //                      3. 선택한 작은 원소의 최댓값 + 1을 한 값이, 현재 자기자신의 최댓값 보다 크다면 그 값으로 자기자신의 값을 갱신

--- Dynamic/상자넣기.swift
+++ Dynamic/상자넣기.swift
@@ -2,7 +2,9 @@
 //  상자넣기.swift
 //  Question Link: https://www.acmicpc.net/problem/1965
 //  Primary idea:       (DP)
-//                      1.
+//                      1. LIS에 대한 개념
+//                      2. 자기보다 이전의 원소들 중 작은 원소들을 하나씩 모두 비교
+//                      3. 선택한 작은 원소의 최댓값 + 1을 한 값이, 현재 자기자신의 최댓값 보다 크다면 그 값으로 자기자신의 값을 갱신
 //
 //  Time Complexity :
 //  Space Complexity :
