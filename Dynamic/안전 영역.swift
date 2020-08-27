c2019edb1e0d6b36a83727c80497694bd1012efd
김건형 <show11837@gmail.com>
2020-08-27 20:33:40 +0900

    feat : 안전 영역 문제 풀이 구현

--- Dynamic/안전 영역.swift
+++ Dynamic/안전 영역.swift
@@ -2,7 +2,7 @@
 //  안전 영역.swift
 //  Question Link: https://www.acmicpc.net/problem/1932
 //  Primary idea:       (DP)
-//                      1. 
+//                      1.
 //
 //  Time Complexity :
 //  Space Complexity :
@@ -13,3 +13,25 @@
 //
 
 import Foundation
+
+let row = Int(readLine()!)!
+var arr = [[Int]](repeating: [Int](repeating: 0, count: 501), count: 501)
+var dp = [[Int]](repeating: [Int](repeating: 0, count: 501), count: 501)
+
+for i in 1...row {
+    let col = readLine()!.split(separator: " ").map{Int($0)!}
+    
+    for (j, value) in col.enumerated() {
+        arr[i][j+1] = value
+    }
+}
+
+dp[1][1] = arr[1][1] // 초기화
+
+for i in 2...row {
+    for j in 1...i {
+        dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j]
+    }
+}
+
+print(dp[row].max()!)
