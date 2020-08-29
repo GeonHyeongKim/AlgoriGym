0b0d5506fb22794377c7789a251ecfc12516a6ee
김건형 <show11837@gmail.com>
2020-08-29 10:21:00 +0900

    feat : 내려가기 문제 풀이 시작

---
+++ Dynamic/내려가기.swift
@@ -0,0 +1,15 @@
+//
+//  내려가기.swift
+//  Question Link: https://www.acmicpc.net/problem/2096
+//  Primary idea:       <Dynamic programming>
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/08/29.
+//
+
+import Foundation
