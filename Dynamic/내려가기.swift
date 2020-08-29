1d0b3c2098f7202916705ca375c2e20b486056de
김건형 <show11837@gmail.com>
2020-08-29 14:24:10 +0900

    feat : 내려가기 문제 풀이 아이디어

--- Dynamic/내려가기.swift
+++ Dynamic/내려가기.swift
@@ -2,7 +2,8 @@
 //  내려가기.swift
 //  Question Link: https://www.acmicpc.net/problem/2096
 //  Primary idea:       <Dynamic programming>
-//                      1.
+//                      1. RGB 거리와 같이 각 위치의 최대값과 최소값을 저장하면서 최종 위치의 최대와 최소를 출력하면 되는 문제이다.
+//                      2. 단 하나의 최대 최소 값이 아닌 3개의 길 모두의 해당하는 최대값과 최소값을 저장해야 한다.
 //
 //  Time Complexity :
 //  Space Complexity :
