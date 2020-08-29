5ff2323761c5b104e028f64047cd9fdf1f3feb06
김건형 <show11837@gmail.com>
2020-08-29 14:26:34 +0900

    feat : e-2 문제 풀이

---
+++ Test/e-2.swift
@@ -0,0 +1,31 @@
+//
+//  e-2.swift
+//  
+//
+//  Created by gunhyeong on 2020/08/29.
+//
+
+import Foundation
+
+class Solution {
+    static int answer = Integer.MAX_VALUE;
+
+    public int solution(int num, int[] cards) {
+        dfs(cards, 0, num, 0, 0);
+        return answer;
+    }
+    public static void dfs(int[] cards, int start, int target, int sum, int cardCnt){
+        if(sum > target || cardCnt >= answer){
+            return;
+        }
+        
+        if(sum==target){
+            answer = cardCnt;
+            return;
+        }
+        
+        for(int i=start; i<cards.length; i++){
+            dfs(cards, i, target, sum+cards[i], cardCnt+1);
+        }
+    }
+}
