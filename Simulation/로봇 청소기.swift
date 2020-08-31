6222cdc3d63566f9b3f8e13f2e9780e054283475
김건형 <show11837@gmail.com>
2020-08-31 23:33:39 +0900

    feat : 로봇청소기 문제 풀이 아이디어
    //  Primary idea:       <Simulation>
    //                      1. 주변에 방문할 칸이 있는 경우
    //                        - 방향을 4방향으로 바꾸면서 방문할 수 있으면 방문 후 표시
    //                        - 방문 후 DFS() 호출, 결과++;
    //                        - 리턴
    //                      2. 방문할 칸이 없는 경우
    //                        - 후진
    //                        - 후진했는데 벽인 경우 : 종료
    //                        - 후진 가능한 경우 : 후진한 위치를 기준으로 DFS() 호출

--- Simulation/로봇 청소기.swift
+++ Simulation/로봇 청소기.swift
@@ -2,7 +2,14 @@
 //  로봇 청소기.swift
 //  Question Link: https://www.acmicpc.net/problem/14503
 //  Primary idea:       <Simulation>
-//                      1.
+//                      1. 주변에 방문할 칸이 있는 경우
+//                        - 방향을 4방향으로 바꾸면서 방문할 수 있으면 방문 후 표시
+//                        - 방문 후 DFS() 호출, 결과++;
+//                        - 리턴
+//                      2. 방문할 칸이 없는 경우
+//                        - 후진
+//                        - 후진했는데 벽인 경우 : 종료
+//                        - 후진 가능한 경우 : 후진한 위치를 기준으로 DFS() 호출
 //
 //  Time Complexity :
 //  Space Complexity :
