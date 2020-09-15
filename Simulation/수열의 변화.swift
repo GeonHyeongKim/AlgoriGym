8d30cd418b553102f18371bc6dd1f51b7ca767b6
김건형 <show11837@gmail.com>
2020-09-15 16:11:11 +0900

    feat : 수열의 변화 문제 풀이 아이디어 (Simulation)
    //  Primary idea:       (Simulation)
    1. 입력과 출력에 주의하면서 B[i] = A[i+1]-A[i]를 계산

--- Simulation/수열의 변화.swift
+++ Simulation/수열의 변화.swift
@@ -2,7 +2,7 @@
 //  수열의 변화.swift
 //  Question Link: https://www.acmicpc.net/problem/1551
 //  Primary idea:       (Simulation)
-//                      1.
+//                      1. 입력과 출력에 주의하면서 B[i] = A[i+1]-A[i]를 계산
 //
 //  Time Complexity :
 //  Space Complexity :
