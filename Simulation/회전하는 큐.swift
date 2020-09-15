30826e200055f5c7fff850f48ede1bf6c5f1c406
김건형 <show11837@gmail.com>
2020-09-15 14:17:32 +0900

    feat : 회전하는 큐 문제 풀이 아이디어 (Simulation & queue)
    //  Primary idea:       (Simulation & 양방향 queue)
    1. 문제 이해에 도움을 준 site : http://dailyddubby.blogspot.com/2018/05/128-1021.html
    2. 양방향으로 회전한다는 점을 인지
    3. Queue의 길이 계산이 중요한 문제! - 뽑으려는 값을 기준으로
    	 i) 왼쪽이 더 짧은 경우 : '왼쪽'으로 밀어가며 첫번째로 위치 할 수 있도록 한다.
    	ii) 오른쪽이 더 짧은 경우 : '오른쪽'으로 밀어가며 첫번째로 위치 할 수 있도록 한다.

--- Simulation/회전하는 큐.swift
+++ Simulation/회전하는 큐.swift
@@ -1,8 +1,12 @@
 //
 //  회전하는 큐.swift
 //  Question Link: https://www.acmicpc.net/problem/1021
-//  Primary idea:       (Simulation)
-//                      1.
+//  Primary idea:       (Simulation & 양방향 queue)
+//                      1. 문제 이해에 도움을 준 site : http://dailyddubby.blogspot.com/2018/05/128-1021.html
+//                      2. 양방향으로 회전한다는 점을 인지
+//                      3. Queue의 길이 계산이 중요한 문제! - 뽑으려는 값을 기준으로
+//                          i) 왼쪽이 더 짧은 경우 : '왼쪽'으로 밀어가며 첫번째로 위치 할 수 있도록 한다.
+//                          ii) 오른쪽이 더 짧은 경우 : '오른쪽'으로 밀어가며 첫번째로 위치 할 수 있도록 한다.
 //
 //  Time Complexity :
 //  Space Complexity :
