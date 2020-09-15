3d843e8e82ce2339fdb0ef74efa7e8708e064aa6
김건형 <show11837@gmail.com>
2020-09-15 15:39:56 +0900

    feat : 수열의 변화 문제 풀이 시작

---
+++ Simulation/수열의 변화.swift
@@ -0,0 +1,15 @@
+//
+//  수열의 변화.swift
+//  Question Link: https://www.acmicpc.net/problem/1551
+//  Primary idea:       (Simulation)
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/09/15.
+//
+
+import Foundation
