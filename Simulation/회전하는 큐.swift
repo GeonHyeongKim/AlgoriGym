5097c594270b4166edcd56f955951c1dc65e17e6
김건형 <show11837@gmail.com>
2020-09-15 15:08:55 +0900

    finish : 회전하는 큐 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    //  Time Complexity : ?
    //  Space Complexity : O(n)
    //  Runtime: 12 ms
    //  Memory Usage: 81.336 MB

--- Simulation/회전하는 큐.swift
+++ Simulation/회전하는 큐.swift
@@ -8,10 +8,10 @@
 //                          i) 왼쪽이 더 짧은 경우 : '왼쪽'으로 밀어가며 첫번째로 위치 할 수 있도록 한다.
 //                          ii) 오른쪽이 더 짧은 경우 : '오른쪽'으로 밀어가며 첫번째로 위치 할 수 있도록 한다.
 //
-//  Time Complexity :
-//  Space Complexity :
-//  Runtime:  ms
-//  Memory Usage: MB
+//  Time Complexity : ?
+//  Space Complexity : O(n)
+//  Runtime: 12 ms
+//  Memory Usage: 81.336 MB
 //
 //  Created by gunhyeong on 2020/09/15.
 //
