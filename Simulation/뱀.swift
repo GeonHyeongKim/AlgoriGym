5fac16e570d02d82a1f34502dace381a209219a4
김건형 <show11837@gmail.com>
2020-09-18 02:34:42 +0900

    feat : 뱀 문제 풀이 아이디어 - (Simulation, Queue)
    //  Primary idea:       (Simulation)
    1. 'Dummy'게임 : 사과를 먹으면 뱀의 길이가 길어진다.
    2. 뱀은 1초부터 움직임을 시작한다.
    3. 뱀의 몸통은 움직이기 전까지 그 자리에 정지해있다. 즉, 길이가 5인 뱀의 움직임은 ----- => ---- - => --- -- => -- --- => - ---- => ----- 식으로 움직인다는 것이다.
    4. 뱀의 이동 방향은 뱀의 머리 기준 오른쪽 왼쪽이다.

--- Simulation/뱀.swift
+++ Simulation/뱀.swift
@@ -3,7 +3,9 @@
 //  Question Link: https://www.acmicpc.net/problem/3190
 //  Primary idea:       (Simulation)
 //                      1. 'Dummy'게임 : 사과를 먹으면 뱀의 길이가 길어진다.
-//                      2. 
+//                      2. 뱀은 1초부터 움직임을 시작한다.
+//                      3. 뱀의 몸통은 움직이기 전까지 그 자리에 정지해있다. 즉, 길이가 5인 뱀의 움직임은 ----- => ---- - => --- -- => -- --- => - ---- => ----- 식으로 움직인다는 것이다.
+//                      4. 뱀의 이동 방향은 뱀의 머리 기준 오른쪽 왼쪽이다.
 //
 //  Time Complexity :
 //  Space Complexity :
