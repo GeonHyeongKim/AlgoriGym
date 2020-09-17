ec19b59ebad853068268ec8efb89860ee55bb847
김건형 <show11837@gmail.com>
2020-09-17 23:16:02 +0900

    feat : 주사위 굴리기 문제 풀이 시작

---
+++ Simulation/뱀.swift
@@ -0,0 +1,15 @@
+//
+//  뱀.swift
+//  Question Link: https://www.acmicpc.net/problem/3190
+//  Primary idea:       (Simulation)
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/09/17.
+//
+
+import Foundation
