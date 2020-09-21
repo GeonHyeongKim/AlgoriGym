726d1b1d7ec507f2d1256c884e80c4c3f6d25384
김건형 <show11837@gmail.com>
2020-09-21 17:46:26 +0900

    feat : 블랙잭 문제 풀이 아이디어 - String & Array & Brute force
    //  Primary idea:       <String & Array & Brute force>
    //                      1. 완전탐색으로 삼중반복문을 통해 모든 3가지 조합를 구한뒤 m 보다 작은 합을 색출
    //                      2. 색출된 값중 가장 큰 값을 저장

--- String&Array/블랙잭.swift
+++ String&Array/블랙잭.swift
@@ -1,8 +1,9 @@
 //
 //  블랙잭.swift
 //  Question Link: https://www.acmicpc.net/problem/2798
-//  Primary idea:       <String & Array & Brute force>
-//                      1.
+//  Primary idea:       (String & Array & Brute force)
+//                      1. 완전탐색으로 삼중반복문을 통해 모든 3가지 조합를 구한뒤 m 보다 작은 합을 색출
+//                      2. 색출된 값중 가장 큰 값을 저장
 //
 //  Time Complexity :
 //  Space Complexity :
