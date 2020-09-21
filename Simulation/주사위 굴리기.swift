abd9eb205382e403e6b345371c46c2ab0c8eaf56
김건형 <show11837@gmail.com>
2020-09-21 14:37:37 +0900

    feat : 주사위 굴리기 문제 풀이 구현 - 주석 있음

--- Simulation/주사위 굴리기.swift
+++ Simulation/주사위 굴리기.swift
@@ -26,8 +26,8 @@ import Foundation
 let input = readLine()!.split(separator: " ").map{Int($0)!}
 let n = input[0] // 지도의 세로크기
 let m = input[1] // 지도의 가로크기
-let x = input[2] // 주사위를 놓는곳 x
-let y = input[3] // 주사위를 놓는곳 y
+var x = input[2] // 주사위를 놓는곳 x
+var y = input[3] // 주사위를 놓는곳 y
 var map = [[Int]]()
 
 // init - insert map value
@@ -37,4 +37,56 @@ for _ in 0..<n {
 }
 
 let k = input[4] // 명령의 갯수
-let orderList = readLine()!.split(separator: " ").map{Int($0)!}
+let operList = readLine()!.split(separator: " ").map{Int($0)! - 1}
+let dx = [0, 0, -1, 1] // 동, 서, 북, 남
+let dy = [1, -1, 0, 0]
+var dice = [0, 0, 0, 0, 0, 0]
+
+// 주사위의 현재 인덱스를 정합니다.
+let direction = [
+    [4, 1, 0, 3, 5, 2], // 동쪽
+    [2, 1, 5, 3, 0, 4], // 서쪽
+    [1, 5, 2, 0, 4, 3], // 북쪽
+    [3, 0, 2, 5, 4, 1]  // 남쪽
+]
+
+for i in 0..<k {
+    let oper = operList[i]
+    let nextX = x + dx[oper]
+    let nextY = y + dy[oper]
+    
+    // map 보다 커질경우
+    if nextX < 0 || nextX >= n || nextY < 0 || nextY >= m { continue }
+    
+    // 주사위 굴리기 전 - 값 복사
+    let temp = dice
+    switch oper {
+    case 0:
+        print("동", terminator: " ")
+    case 1:
+        print("서", terminator: " ")
+    case 2:
+        print("남", terminator: " ")
+    case 3:
+        print("북", terminator: " ")
+    default:
+        print("에러", terminator: " ")
+    }
+    print("굴리기 전 : \(dice) -> ", terminator: "")
+
+    // 주사위 굴린 후 - 값 전달
+    for j in 0..<direction[0].count { dice[j] = temp[direction[oper][j]] }
+    print("\(dice)")
+    
+    if map[nextX][nextY] == 0 { // 주사위 놓은 칸에 쓰여있는 숫자가 0일 때
+        map[nextX][nextY] = dice[5]
+    } else { // 주사위 놓은 칸에 쓰여있는 숫자가 0이 아닐 때
+        dice[5] = map[nextX][nextY]
+        map[nextX][nextY] = 0
+    }
+    
+    x = nextX
+    y = nextY
+    // 주사위 윗 면에 쓰여있는 숫자 출력
+    print(dice.first!)
+}
