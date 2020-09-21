cca67b45be6e2767d7195a8159f5b2f5d2b75083
김건형 <show11837@gmail.com>
2020-09-21 17:22:19 +0900

    feat : 음계 문제 풀이 아이디어 - 구현
    //  Primary idea:       <구현>
    //                      1. 오름차순과 내림차순을 담당하는 Bool을 선언후 true로 초기화한다.
    //                          이때, 오름차순과 내림차순이 아닐때, flase로 바꾸어 준다.

--- String&Array/음계.swift
+++ String&Array/음계.swift
@@ -2,6 +2,8 @@
 //  음계.swift
 //  Question Link: https://www.acmicpc.net/problem/2920
 //  Primary idea:       <구현>
+//                      1. 오름차순과 내림차순을 담당하는 Bool을 선언후 true로 초기화한다.
+//                          이때, 오름차순과 내림차순이 아닐때, flase로 바꾸어 준다.
 //
 //
 //  Time Complexity :
