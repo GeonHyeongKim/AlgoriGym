6e4f318e27504a8c14261b8b5b876d2b7744cfd7
김건형 <show11837@gmail.com>
2020-09-22 13:06:10 +0900

    feat : 수빈이의 수열 문제 풀이 아이디어 - math
    //  Primary idea:       (String & Array & 구현 & Math)
    //                      1. 수학적 원리
    //                         해당값 = 지금까지 평균값(sequence[i]) * (i+1) - 지금까지의 합

--- String&Array/수빈이의 수열.swift
+++ String&Array/수빈이의 수열.swift
@@ -2,7 +2,8 @@
 //  수빈이의 수열.swift
 //  Question Link: https://www.acmicpc.net/problem/10539
 //  Primary idea:       (String & Array & 구현 & Math)
-//                      1.
+//                      1. 수학적 원리
+//                         해당값 = 지금까지 평균값(sequence[i]) * (i+1) - 지금까지의 합
 //
 //  Time Complexity :
 //  Space Complexity :
