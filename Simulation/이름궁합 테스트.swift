d3993769161179e18de88295feceaa69bb0ddd4b
김건형 <show11837@gmail.com>
2020-09-22 13:43:47 +0900

    feat : 이름궁합 테스트 문제 풀이 시작

---
+++ Simulation/이름궁합 테스트.swift
@@ -0,0 +1,15 @@
+//
+//  이름궁합 테스트.swift
+//  Question Link: https://www.acmicpc.net/problem/17269
+//  Primary idea:       (Simulation)
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/09/22.
+//
+
+import Foundation
