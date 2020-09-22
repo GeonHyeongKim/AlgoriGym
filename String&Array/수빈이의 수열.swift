9a2895e02c6f7eb260cf210650e244681fbcf69a
김건형 <show11837@gmail.com>
2020-09-22 11:21:36 +0900

    feat : 수빈이의 수열 문제 풀이 시작

---
+++ String&Array/수빈이의 수열.swift
@@ -0,0 +1,15 @@
+//
+//  수빈이의 수열.swift
+//  Question Link: https://www.acmicpc.net/problem/10539
+//  Primary idea:       (String & Array & 구현 & Math)
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/09/22.
+//
+
+import Foundation
