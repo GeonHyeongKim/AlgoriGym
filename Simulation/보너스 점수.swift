f29b707e8f905c16a869104346be1a26afa32aba
김건형 <show11837@gmail.com>
2020-09-22 17:31:42 +0900

    feat : 보너스 점수 문제 풀이 아이디어
    //  Primary idea:       (Simulation)
    //                      1. 전체 점수(score)와 보너스 점수(bonus)를 변수로 선언한다.
    //                      2. 반복문을 통해 "O","X"일 경우를 나누어
    //                          i) "O"일경우, 전체 점수에 (i+1)과 보너스 점수(bonus)를 누적한다
    //                          ii) "X"일경우, bonus를 0으로 초기화한다.

--- Simulation/보너스 점수.swift
+++ Simulation/보너스 점수.swift
@@ -2,7 +2,10 @@
 //  보너스 점수.swift
 //  Question Link: https://www.acmicpc.net/problem/17389
 //  Primary idea:       (Simulation)
-//                      1.
+//                      1. 전체 점수(score)와 보너스 점수(bonus)를 변수로 선언한다.
+//                      2. 반복문을 통해 "O","X"일 경우를 나누어
+//                          i) "O"일경우, 전체 점수에 (i+1)과 보너스 점수(bonus)를 누적한다
+//                          ii) "X"일경우, bonus를 0으로 초기화한다.
 //
 //  Time Complexity :
 //  Space Complexity :
