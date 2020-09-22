909210de3a37d91e82339844dbc544794c3aa89c
김건형 <show11837@gmail.com>
2020-09-22 16:34:17 +0900

    feat : 이름궁합 테스트 문제 풀이 구현 - 반복문의 범위

--- Simulation/이름궁합 테스트.swift
+++ Simulation/이름궁합 테스트.swift
@@ -16,3 +16,32 @@
 //
 
 import Foundation
+
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+let n = input.first! // 첫번째 이름의 길이
+let m = input.last! // 두번째 이름의 길이
+let names = readLine()!.split(separator: " ")
+let firstName = Array(names.first!)
+let secondName = Array(names.last!)
+let table: [Character:Int] = ["A":3, "B":2, "C":1, "D":2, "E":4, "F":3, "G":1, "H":3, "I":1, "J":1, "K":3, "L":1, "M":3, "N":2, "O":1, "P":2, "Q":2, "R":2, "S":1, "T":2, "U":1, "V":1, "W":1, "X":2, "Y":2, "Z":1]
+var sumName = ""
+let minLen = min(n, m)
+
+for i in 0..<minLen {
+    sumName += String(firstName[i]) + String(secondName[i])
+}
+
+// 나머지 문자열 삽입
+sumName += n >= m ? firstName[minLen..<n] : secondName[minLen..<m]
+
+// 문자에 맞는 숫자로 대체
+var answer = sumName.map{table[$0]!}
+
+for i in 0..<(n+m-2) {
+    for j in 0..<(n+m-1-i) {
+        answer[j] += answer[j+1]
+        answer[j] %= 10
+    }
+}
+
+print(answer[0] == 0 ? "\(answer[1])%" : "\(answer[0])\(answer[1])%")
