2641a124fd4680e3659946e30e937bdb6eb9c4aa
김건형 <show11837@gmail.com>
2020-09-23 15:54:45 +0900

    feat : APC는 왜 서브태스크 대회가 되었을까? 문제 풀이 구현

--- Greedy/APC는 왜 서브태스크 대회가 되었을까?.swift
+++ Greedy/APC는 왜 서브태스크 대회가 되었을까?.swift
@@ -14,3 +14,55 @@
 //
 
 import Foundation
+
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+let n = input[0] // 문제 개수
+let l = input[1] // 역량
+let k = input[2] // 풀수 있는 최대 개수
+var problem = [Problem]()
+var count = 0 // 풀어낸 문제의 수
+var sum = 0
+
+for _ in 0..<n {
+    let sub = readLine()!.split(separator: " ").map{Int($0)!}
+    problem.append(Problem(sub.first!, sub.last!))
+}
+
+// 내림차순 정렬 - 1순위. sub2, 2순위 sub1
+problem.sort(by: {
+    if $0.sub2 > $1.sub2 {
+        return true
+    } else if $0.sub2 < $1.sub2{
+        return false
+    } else {
+        if $0.sub1 > $1.sub1 {
+            return true
+        }
+    }
+    
+    return false
+})
+
+// 풀이
+for p in problem {
+    if count > k { break } // 풀수 있는 최대 개수를 넘지 못함
+    count += 1
+    
+    if p.sub1 <= l && p.sub2 <= l {
+        sum += 140
+    } else if p.sub1 <= l {
+        sum += 100
+    }
+}
+
+print(sum)
+
+struct Problem {
+    var sub1: Int
+    var sub2: Int
+    
+    init(_ sub1: Int, _ sub2: Int) {
+        self.sub1 = sub1
+        self.sub2 = sub2
+    }
+}
