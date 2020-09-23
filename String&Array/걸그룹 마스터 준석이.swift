f1b8b986b90fbcdc5a75215a5d0ae6d75b21b9b0
김건형 <show11837@gmail.com>
2020-09-23 14:16:27 +0900

    feat :  걸그룹 마스터 준석이 문제 풀이 아이디어 - Dictionary
    //  Question Link: https://www.acmicpc.net/problem/16165
    //  Primary idea:       (Hash)
    //                      1. Dictionary를 이용해, 그룹명과 그룹멤버의 이름을 저장한다.
    //                      2. 저장된 Dictionary에서 퀴즈 유형에 따른 String을 출력한다
    //                          i) 0 일때, 오름차순으로 정렬된 걸그룹 멤버들의 이름을 출력한다.
    //                          ii) 1 일떄, 멤버가 속해있는 걸그룹의 이름을 출력한다.

--- String&Array/걸그룹 마스터 준석이.swift
+++ String&Array/걸그룹 마스터 준석이.swift
@@ -1,8 +1,11 @@
 //
 //  걸그룹 마스터 준석이.swift
 //  Question Link: https://www.acmicpc.net/problem/16165
-//  Primary idea:       (Hash)
-//                      1.
+//  Primary idea:       (Dictionary)
+//                      1. Dictionary를 이용해, 그룹명과 그룹멤버의 이름을 저장한다.
+//                      2. 저장된 Dictionary에서 퀴즈 유형에 따른 String을 출력한다
+//                          i) 0 일때, 오름차순으로 정렬된 걸그룹 멤버들의 이름을 출력한다.
+//                          ii) 1 일떄, 멤버가 속해있는 걸그룹의 이름을 출력한다.
 //
 //  Time Complexity :
 //  Space Complexity :
