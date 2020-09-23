ecdbdbf1815e3c4438a7984037d6b0994d30e914
김건형 <show11837@gmail.com>
2020-09-23 15:54:18 +0900

    feat : APC는 왜 서브태스크 대회가 되었을까? 문제 풀이 아이디어 - sort & greedy
    //  Primary idea:       (Greedy & sort)
    //                      1. 문제를 입력받은 후 내림차순으로 정렬한다.
    //                      2. 그리고 역량을 넘지 않은 선에서 문제를 풀어낸다.

--- Greedy/APC는 왜 서브태스크 대회가 되었을까?.swift
+++ Greedy/APC는 왜 서브태스크 대회가 되었을까?.swift
@@ -2,7 +2,8 @@
 //  APC는 왜 서브태스크 대회가 되었을까?.swift
 //  Question Link: https://www.acmicpc.net/problem/17224
 //  Primary idea:       (Greedy & sort)
-//                      1.
+//                      1. 문제를 입력받은 후 내림차순으로 정렬한다.
+//                      2. 그리고 역량을 넘지 않은 선에서 문제를 풀어낸다.
 //
 //  Time Complexity :
 //  Space Complexity :
