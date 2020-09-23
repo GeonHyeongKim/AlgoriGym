986a0abfdcda04eb8bed1290ea2f4bc4e8f68e72
김건형 <show11837@gmail.com>
2020-09-23 17:50:09 +0900

    feat : The candy war 문제 풀이 아이디어 - Simulation
    //  Primary idea:       (Simulation)
    //                      1. testCase만큼 반복문을 돈다.
    //                      2. check 합수를 만들어 홀수개의 candy를 가진 아이에게 사탕을 채워줄때 모두 값이 같아지는지 확인
    //                      3. rightCandy : 오른쪽에 채워줘야하는 값을 새로운 배열값을 만들어 저장해 놓는다.
    //                      4. candy의 개수가 홀수인 아이에의 값을 1을 추가해준다.
    //                      5. candy를 반으로 나누고 오른쪽에 채워줘야하는 값을 배열에 저장해 놓는다.

--- Simulation/The candy war.swift
+++ Simulation/The candy war.swift
@@ -2,7 +2,11 @@
 //  The candy war.swift
 //  Question Link: https://www.acmicpc.net/problem/9037
 //  Primary idea:       (Simulation)
-//                      1.
+//                      1. testCase만큼 반복문을 돈다.
+//                      2. check 합수를 만들어 홀수개의 candy를 가진 아이에게 사탕을 채워줄때 모두 값이 같아지는지 확인
+//                      3. rightCandy : 오른쪽에 채워줘야하는 값을 새로운 배열값을 만들어 저장해 놓는다.
+//                      4. candy의 개수가 홀수인 아이에의 값을 1을 추가해준다.
+//                      5. candy를 반으로 나누고 오른쪽에 채워줘야하는 값을 배열에 저장해 놓는다.
 //
 //  Time Complexity :
 //  Space Complexity :
