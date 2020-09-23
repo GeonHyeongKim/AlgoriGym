46abf636deb736df37731e0b4948f83b3c7ad231
김건형 <show11837@gmail.com>
2020-09-23 13:15:51 +0900

    feat : 수 찾기 문제 풀이 구현 - 시간초과 해결
    탐색 속도 개선
    Contains : O(n)
    BinarySearch : O(logn)

--- String&Array/수 찾기.swift
+++ String&Array/수 찾기.swift
@@ -1,8 +1,9 @@
 //
 //  수 찾기.swift
 //  Question Link: https://www.acmicpc.net/problem/1920
-//  Primary idea:       (이분 탐색)
-//                      1.배열값들을 받고 A,B에 들어 있는지 contains()를 통해 확인한다.
+//  Primary idea:       (Binary Search)
+//                      1. 배열값들을 받고 A,B에 들어 있는지 contains()를 통해 확인한다. (x)
+//                      2. containsO(n)로 탐색하지 않고 BinarySearch(O(logN))를 직접 구현하여 값을 찾음
 //
 //  Time Complexity :
 //  Space Complexity :
@@ -15,12 +16,31 @@
 import Foundation
 
 let n = Int(readLine()!)!
-let nList = readLine()!.split(separator: " ").map{Int($0)!}
+let nList = readLine()!.split(separator: " ").map{Int($0)!}.sorted()
 let m = Int(readLine()!)!
 let mList = readLine()!.split(separator: " ").map{Int($0)!}
-var answer = ""
 
-for num in mList.map({nList.contains($0) ? 1 : 0}) {
-    answer += "\(num) "
+for num in mList {
+    if binarySearch(nList, num, n) == -1 {
+        print(0)
+    } else {
+        print(1)
+    }
 }
 
+func binarySearch(_ nList: [Int], _ target: Int, _ n: Int) -> Int {
+    var low = 0
+    var high = n - 1
+    
+    while low <= high {
+        let mid = (low + high) / 2
+        if nList[mid] > target {
+            high = mid - 1
+        } else if nList[mid] < target {
+            low = mid + 1
+        } else {
+            return mid
+        }
+    }
+    return -1
+}
