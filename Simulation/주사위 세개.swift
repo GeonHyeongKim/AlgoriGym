68577fce8b716a83d573465040ec0dc7c140217f
김건형 <show11837@gmail.com>
2020-09-25 10:16:24 +0900

    feat : 주사위 세개 문제 풀이 시작

---
+++ Simulation/주사위 세개.swift
@@ -0,0 +1,15 @@
+//
+//  주사위 세개.swift
+//  Question Link: https://www.acmicpc.net/problem/2480
+//  Primary idea:       (Simulation & Math & 사칙연산)
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/09/25.
+//
+
+import Foundation
