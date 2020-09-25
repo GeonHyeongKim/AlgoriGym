0b0b80565c9da42d5b92e83ba63c0b6c3ec02102
김건형 <show11837@gmail.com>
2020-09-25 10:08:00 +0900

    feat : Z 문제 풀이 아이디어 - 분할 정복 && 재귀
    //                      1. n을 통해 pow를 쓰지 않고 2의 제곱을 구한다. (1>>n)
    //                      2. z()함수 : 사각형의 왼쪽 상단의 꼭지점의 좌표와 한변의 길이를 밴개변수로 받는다.
    //                         i) 온쪽 상단의 꼭지점 : x, y
    //                         ii) 한변의 길이, 즉 size
    //                      3. 종료 조건은 한 변의 길이가 1이 됐을 경우, cnt를 증가시키고 false를 반환
    //                      4. x, y가 r, c가 되었을 경우 true
    //                      5. 2중 포문으로 4번 호출해주는 것과 동일하게 계산한다. ( r+i*m, c+j*m )
    //                         i) getAnswer(r, c, m);
    //                         ii) getAnswer(r, c+m, m);
    //                         iii) getAnswer(r+m, c, m);
    //                         iv) getAnswer(r+m, c+m, m);

--- String&Array/Z.swift
+++ String&Array/Z.swift
@@ -2,7 +2,17 @@
 //  Z.swift
 //  Question Link: https://www.acmicpc.net/problem/1074
 //  Primary idea:       (분할 정복 && 재귀)
-//                      1.
+//                      1. n을 통해 pow를 쓰지 않고 2의 제곱을 구한다. (1>>n)
+//                      2. z()함수 : 사각형의 왼쪽 상단의 꼭지점의 좌표와 한변의 길이를 밴개변수로 받는다.
+//                         i) 온쪽 상단의 꼭지점 : x, y
+//                         ii) 한변의 길이, 즉 size
+//                      3. 종료 조건은 한 변의 길이가 1이 됐을 경우, cnt를 증가시키고 false를 반환
+//                      4. x, y가 r, c가 되었을 경우 true
+//                      5. 2중 포문으로 4번 호출해주는 것과 동일하게 계산한다. ( r+i*m, c+j*m )
+//                         i) getAnswer(r, c, m);
+//                         ii) getAnswer(r, c+m, m);
+//                         iii) getAnswer(r+m, c, m);
+//                         iv) getAnswer(r+m, c+m, m);
 //
 //  Time Complexity :
 //  Space Complexity :
