340e4d776b2a5f9cac75f8de517254c2fe16a8af
김건형 <show11837@gmail.com>
2020-09-25 10:47:29 +0900

    feat : 주사위 세개 문제 풀이 아이디어 - Simulation & Math & 사칙연산
    //  Primary idea:       (Simulation & Math & 사칙연산)
    //                      1. Set을 통해 unique한 갯수를 찾아낸다.
    //                      2. 정렬을 통해 개수가 2개일 경우를 계산한다.

--- Simulation/주사위 세개.swift
+++ Simulation/주사위 세개.swift
@@ -2,7 +2,8 @@
 //  주사위 세개.swift
 //  Question Link: https://www.acmicpc.net/problem/2480
 //  Primary idea:       (Simulation & Math & 사칙연산)
-//                      1.
+//                      1. Set을 통해 unique한 갯수를 찾아낸다.
+//                      2. 정렬을 통해 개수가 2개일 경우를 계산한다.
 //
 //  Time Complexity :
 //  Space Complexity :
