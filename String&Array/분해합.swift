c869e5ea0c0a2608a7c9ed557b1331145dc8acb6
김건형 <show11837@gmail.com>
2020-09-28 23:25:58 +0900

    finish : 분해합 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    //  Time Complexity : O(n)
    //  Space Complexity : O(1)
    //  Runtime: 24 ms
    //  Memory Usage: 81.324 MB

--- String&Array/분해합.swift
+++ String&Array/분해합.swift
@@ -1,12 +1,13 @@
 //
 //  분해합.swift
 //  Question Link: https://www.acmicpc.net/problem/2231
-//  Time Complexity :
-//  Space Complexity :
-//  Runtime: ms
-//  Memory Usage:  MB
 //  Primary idea:       (String & Array & Bruth force)
 //                      1. 1~n 까지 반복하면 모든 경우의 수를 구한다.
+//
+//  Time Complexity : O(n)
+//  Space Complexity : O(1)
+//  Runtime: 24 ms
+//  Memory Usage: 81.324 MB
 //
 //  Created by gunhyeong on 2020/09/28.
 //
