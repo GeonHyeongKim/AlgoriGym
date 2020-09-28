5e09fef408fe56ceef500584f61b0b1232041f49
김건형 <show11837@gmail.com>
2020-09-28 23:25:03 +0900

    fix : 분해합 문제 풀이 아이디어 - String & Array & Bruth force
    //  Primary idea:       (String & Array & Bruth force)
    //                      1. 1~n 까지 반복하면 모든 경우의 수를 구한다.

--- String&Array/분해합.swift
+++ String&Array/분해합.swift
@@ -1,12 +1,12 @@
 //
 //  분해합.swift
 //  Question Link: https://www.acmicpc.net/problem/2231
-//  Primary idea:       <String & Array>
-//                      1.
 //  Time Complexity :
 //  Space Complexity :
 //  Runtime: ms
 //  Memory Usage:  MB
+//  Primary idea:       (String & Array & Bruth force)
+//                      1. 1~n 까지 반복하면 모든 경우의 수를 구한다.
 //
 //  Created by gunhyeong on 2020/09/28.
 //
