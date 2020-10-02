f666118aabc6565db67d4db43c6002e5f048f9a1
김건형 <show11837@gmail.com>
2020-10-02 18:30:15 +0900

    feat : 근우의 다이어리 꾸미기 문제 풀이 - 아이디어

--- Greedy/근우의 다이어리 꾸미기.swift
+++ Greedy/근우의 다이어리 꾸미기.swift
@@ -2,7 +2,10 @@
 //  근우의 다이어리 꾸미기.swift
 //  Question Link: https://www.acmicpc.net/problem/16676
 //  Primary idea:       (Greedy)
-//                      1.
+//                      1. 문자열을 입력 받는다.
+//                      2. 문자열의 길이를 계산한다.
+//                      3. 문자열의 길이만큼 1이 포함되는 경우가 존재하는지 판단한다.
+//                      4. 3의 조건을 만족하면 문자열의 길이를, 만족하지 않으면 문자열 길이 -1을 출력한다.
 //
 //  Time Complexity :
 //  Space Complexity :
