47161e7de4b584ed9957865aa332463534876837
김건형 <show11837@gmail.com>
2020-10-09 18:01:47 +0900

    feat : 저울 문제 풀이 아이디어 - Greedy
    //  Primary idea:       (Greedy)
    //                      1. 저울추를 오름차순으로 정렬
    //                      2. 저울추의 총 합을 재는 변수는 sum
    //                      3. 이제 저울추를 하나씩 올려보며 주어진 저울추로 무게는 측정할 수 있는지 검사

--- Greedy/저울.swift
+++ Greedy/저울.swift
@@ -2,7 +2,9 @@
 //  저울.swift
 //  Question Link: https://www.acmicpc.net/problem/2437
 //  Primary idea:       (Greedy)
-//                      1.
+//                      1. 저울추를 오름차순으로 정렬
+//                      2. 저울추의 총 합을 재는 변수는 sum
+//                      3. 이제 저울추를 하나씩 올려보며 주어진 저울추로 무게는 측정할 수 있는지 검사
 //
 //  Time Complexity :
 //  Space Complexity :
