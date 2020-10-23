7f1096f70a13472421576dc7181f5e65e6132cce
김건형 <show11837@gmail.com>
2020-10-23 17:38:06 +0900

    finish : Minimum Number of Vertices to Reach All Nodes 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    //  Time Complexity : O(n^n)
    //  Space Complexity : O(n)
    //  Runtime: 1628 ms
    //  Memory Usage: 25.9 MB

--- Graph(DFS & BFS)/Minimum Number of Vertices to Reach All Nodes.swift
+++ Graph(DFS & BFS)/Minimum Number of Vertices to Reach All Nodes.swift
@@ -5,10 +5,10 @@
 //                      1. 0~<n 까지의 요소를 가진 set
 //                      2. edges의 각 연결된 값의 last!값이 target임으로, set의 값에서 제거
 //
-//  Time Complexity :
-//  Space Complexity :
-//  Runtime:  ms
-//  Memory Usage:  MB
+//  Time Complexity : O(n^n)
+//  Space Complexity : O(n)
+//  Runtime: 1628 ms
+//  Memory Usage: 25.9 MB
 //
 //  Created by gunhyeong on 2020/10/23.
 //
