2a75439289dce6149fbe914759a0873ab0d4c6d1
김건형 <show11837@gmail.com>
2020-10-25 23:12:25 +0900

    feat : Sort Integers by The Power Value 문제 풀이 아이디어

--- Graph(DFS & BFS)/Sort Integers by The Power Value.swift
+++ Graph(DFS & BFS)/Sort Integers by The Power Value.swift
@@ -1,8 +1,9 @@
 //
 //  Sort Integers by The Power Value.swift
 //  Question Link: https://leetcode.com/problems/sort-integers-by-the-power-value/
-//  Primary idea:       (Graph)
-//                      1.
+//  Primary idea:       (Graph, DP)
+//                      1. 홀수인경우 kx3 + 1 , 짝수인경우 k/2를 해서 1이 될때까지 만드는 문제
+//                      2. 
 //
 //  Time Complexity :
 //  Space Complexity :
@@ -13,3 +14,9 @@
 //
 
 import Foundation
+
+class Solution {
+    func getKth(_ lo: Int, _ hi: Int, _ k: Int) -> Int {
+        
+    }
+}
