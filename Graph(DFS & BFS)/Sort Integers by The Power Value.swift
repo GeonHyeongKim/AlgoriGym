0a3ef562d8332146538d004acc1e2a358b8280cd
김건형 <show11837@gmail.com>
2020-10-26 22:34:34 +0900

    finish : Sort Integers by The Power Value 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
    //  Time Complexity : O(n^2)
    //  Space Complexity : O(n^2)
    //  Runtime: 644 ms
    //  Memory Usage: 14.1 MB

--- Graph(DFS & BFS)/Sort Integers by The Power Value.swift
+++ Graph(DFS & BFS)/Sort Integers by The Power Value.swift
@@ -3,12 +3,11 @@
 //  Question Link: https://leetcode.com/problems/sort-integers-by-the-power-value/
 //  Primary idea:       (Graph, DP)
 //                      1. 홀수인경우 kx3 + 1 , 짝수인경우 k/2를 해서 1이 될때까지 만드는 문제
-//                      2. 
 //
-//  Time Complexity :
-//  Space Complexity :
-//  Runtime:  ms
-//  Memory Usage:  MB
+//  Time Complexity : O(n^2)
+//  Space Complexity : O(n^2)
+//  Runtime: 644 ms
+//  Memory Usage: 14.1 MB
 //
 //  Created by gunhyeong on 2020/10/25.
 //
@@ -17,6 +16,31 @@ import Foundation
 
 class Solution {
     func getKth(_ lo: Int, _ hi: Int, _ k: Int) -> Int {
+        var arr = [[Int]]()
         
+        for num in lo...hi {
+            arr.append([num, steps(num)])
+        }
+        
+        arr.sort(by: {
+            if $0.last! == $1.last! {
+                return $0.first! < $0.first!
+            }
+            return $0.last! < $1.last!
+        })
+        
+        return arr[k-1][0]
+    }
+    
+    func steps(_ num:Int) -> {
+        var num = num
+        var count = 1
+        
+        while num != 1 {
+            num = num % 2 == 0 ? num / 2 : num * 3 + 1
+            count += 1
+        }
+        
+        return count
     }
 }
