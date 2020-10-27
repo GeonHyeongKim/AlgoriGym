bb80b66d0a9b65615bfde90ba83457e0ee67aa10
김건형 <show11837@gmail.com>
2020-10-27 15:18:54 +0900

    feat : Keys and Rooms 문제 풀이 아이디어
    Primary idea:       (Graph, dfs)
                          1. 전체를 모두 방문하는지 확인하는 문제
                          2. dfs로 모든 방문 확인

--- Graph(DFS & BFS)/Keys and Rooms.swift
+++ Graph(DFS & BFS)/Keys and Rooms.swift
@@ -1,8 +1,9 @@
 //
 //  Keys and Rooms.swift
 //  Question Link: https://leetcode.com/problems/keys-and-rooms/
-//  Primary idea:       (Graph)
-//                      1.
+//  Primary idea:       (Graph, dfs)
+//                      1. 전체를 모두 방문하는지 확인하는 문제
+//                      2. dfs로 모든 방문 확인
 //
 //  Time Complexity :
 //  Space Complexity :
