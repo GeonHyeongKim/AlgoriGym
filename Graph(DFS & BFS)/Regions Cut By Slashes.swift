d4d2d1f9163b0b34c20b958e2864728c9c98eeed
김건형 <show11837@gmail.com>
2020-10-27 13:17:10 +0900

    feat : Regions Cut By Slashes 문제 풀이 구현

--- Graph(DFS & BFS)/Regions Cut By Slashes.swift
+++ Graph(DFS & BFS)/Regions Cut By Slashes.swift
@@ -21,7 +21,51 @@
 import Foundation
 
 class Solution {
+    var row = 0
+    var col = 0
+    
+    let convertDirt: [Character : [[Int]]] = [
+        "/": [[0,0,1], [0,1,0], [1,0,0]],
+        "\\": [[1,0,0], [0,1,0], [0,0,1]]
+    ]
+            
     func regionsBySlashes(_ grid: [String]) -> Int {
+        var map = [[Int]]()
         
+        for rows in grid {
+            var line1 = [Int](), line2 = [Int](), line3 = [Int]()
+            for char in rows {
+                let convert = convertDirt[char] ?? [[0,0,0], [0,0,0], [0,0,0]]
+                line1 += convert[0]
+                line2 += convert[1]
+                line3 += convert[2]
+            }
+            map += [line1, line2, line3]
+        }
+        
+        var result = 0
+        row = map.count
+        col = map[0].count
+        for i in 0..<row {
+            for j in 0..<col {
+                if map[i][j] == 0 {
+                    result += 1
+                    dfs(&map, i, j)
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    func dfs(_ map: inout [[Int]], _ i: Int, _ j: Int) {
+        if i < 0 || i >= row || j < 0 || j >= col || map[i][j] != 0 { return }
+        
+        map[i][j] = -1
+        
+        dfs(&map, i+1, j)
+        dfs(&map, i-1, j)
+        dfs(&map, i, j+1)
+        dfs(&map, i, j-1)
     }
 }
