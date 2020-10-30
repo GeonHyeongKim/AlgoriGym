61a90bb5b50ca7b318a002750d96e3910fcdbe5b
김건형 <show11837@gmail.com>
2020-10-30 14:39:39 +0900

    feat : f-1

--- Test/j-1.swift
+++ Test/j-1.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+func a(_ consumers: [String]) -> [Int] {
+    var dict = [Int:[Int]]()
+    var answer = [Int]()
+    
+    for info in consumer {
+        if dict.keys.contains(info) {
+            dict[info]! += 1
+        } else {
+            dict[info] = 0
+        }
+    }
+    
+    for info in dict {
+        let id = info.keys
+        let b = info.value
+        
+        while b >= 4 {
+            answer.append(id)
+            b -= 4
+        }
+    }
+    
+    return answer.count > 0 ? answer.sorted(by : <) : [-1]
+}
