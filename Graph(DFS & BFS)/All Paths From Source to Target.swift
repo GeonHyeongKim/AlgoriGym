78b137b5b21b1f10678ab30feffe740590bbaa6b
김건형 <show11837@gmail.com>
2020-11-01 22:42:15 +0900

    remove : All Paths From Source to Target 문제 풀이 초기화

--- Graph(DFS & BFS)/All Paths From Source to Target.swift
+++ Graph(DFS & BFS)/All Paths From Source to Target.swift
@@ -14,31 +14,7 @@
 
 import Foundation
 
-class Solution {
-    func allPathsSourceTarget(_ graph: [[Int]]) -> [[Int]] {
-        var result = [[Int]]()
-        
-        addPath(graph, path: [Int](), node: 0, &result)
-        
-        return result
-    }
-    
-    func addPath(_ graph: [[Int]], path: [Int], node: Int, _ result: inout [[Int]]) {
-        var path = path
-        
-        if node == graph.count - 1 {
-            path.append(node)
-            result.append(path)
-            return
-        }
-        
-        for e in graph[node] {
-            path.append(node)
-            addPath(graph, path: path, node: e, &result)
-            path.removeLast()
-        }
-    }
-}
+
 
 //for i in Solution().allPathsSourceTarget([[1,2],[3],[3],[]]) {
 //    print(i)
