1a7abc1c9f9f3ae29e5cfda28a89b7fb9105b1dc
김건형 <show11837@gmail.com>
2020-11-02 14:34:43 +0900

    feat : Count Servers that Communicate 문제 풀이 아이디어 -
    Primary idea:       (Graph)
                      1. dfs로 동서남북 탐색
                         단, 반복문을 통해 row, col의 0..size값으로 반복
                      2. connect 변수를 통해, 각각의 섬의 갯수를 저장
                      3. 저장된 connect 변수의 값이 2이상이면 결과값(answer)에 누적

--- Graph(DFS & BFS)/Count Servers that Communicate.swift
+++ Graph(DFS & BFS)/Count Servers that Communicate.swift
@@ -2,7 +2,10 @@
 //  Count Servers that Communicate.swift
 //  Question Link: https://leetcode.com/problems/count-servers-that-communicate/
 //  Primary idea:       (Graph)
-//                      1.
+//                      1. dfs로 동서남북 탐색
+//                         단, 반복문을 통해 row, col의 0..size값으로 반복
+//                      2. connect 변수를 통해, 각각의 섬의 갯수를 저장
+//                      3. 저장된 connect 변수의 값이 2이상이면 결과값(answer)에 누적
 //
 //  Time Complexity :
 //  Space Complexity :
