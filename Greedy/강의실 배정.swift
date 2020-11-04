481b3629af4cfbee024060fb97da28d773aeed3f
김건형 <show11837@gmail.com>
2020-11-04 10:34:06 +0900

    feat : 강의실 배정 문제 풀이 시작

---
+++ Greedy/강의실 배정.swift
@@ -0,0 +1,15 @@
+//
+//  강의실 배정.swift
+//  Question Link: https://www.acmicpc.net/problem/11000
+//  Primary idea:       (Greedy)
+//                      1.
+//
+//  Time Complexity :
+//  Space Complexity :
+//  Runtime:  ms
+//  Memory Usage:  MB
+//
+//  Created by gunhyeong on 2020/11/04.
+//
+
+import Foundation
