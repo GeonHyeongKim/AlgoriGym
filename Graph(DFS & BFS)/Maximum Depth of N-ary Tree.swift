a044a194cb3af070b9cadcf9841421fe4558817e
김건형 <show11837@gmail.com>
2020-12-24 11:16:23 +0900

    feat : Maximum Depth of N-ary Tree 문제 풀이 시작

---
+++ Graph(DFS & BFS)/Maximum Depth of N-ary Tree.swift
@@ -0,0 +1,30 @@
+//
+//  Maximum Depth of N-ary Tree.swift
+//  Question Link: https://leetcode.com/problems/maximum-depth-of-n-ary-tree
+//  Primary idea:   1.
+
+//  Time Complexity :
+//  Space Complexity :
+//
+//  Created by gunhyeong on 2020/12/24.
+//
+
+import Foundation
+
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var children: [Node]
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.children = []
+ *     }
+ * }
+ */
+
+class Solution {
+    func maxDepth(_ root: Node?) -> Int {
+        
+    }
+}
