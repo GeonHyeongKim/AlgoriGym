d27b7e632201dbe8db5216a0b820636c8ca1b677
김건형 <show11837@gmail.com>
2020-12-24 13:30:37 +0900

    finish : Maximum Depth of N-ary Tree 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)
      Primary idea:   1. 루트노드의 모든 자식 노드들을 방문해서 해당 노드들을 Stack에 넣은 뒤, depth를 계산
    
      Time Complexity : O(n^n)
      Space Complexity : O(n)
      Runtime: 40 ms
      Memory Usage: 15.1 MB

--- Graph(DFS & BFS)/Maximum Depth of N-ary Tree.swift
+++ Graph(DFS & BFS)/Maximum Depth of N-ary Tree.swift
@@ -1,10 +1,12 @@
 //
 //  Maximum Depth of N-ary Tree.swift
 //  Question Link: https://leetcode.com/problems/maximum-depth-of-n-ary-tree
-//  Primary idea:   1.
+//  Primary idea:   1. 루트노드의 모든 자식 노드들을 방문해서 해당 노드들을 Stack에 넣은 뒤, depth를 계산
 
-//  Time Complexity :
-//  Space Complexity :
+//  Time Complexity : O(n^n)
+//  Space Complexity : O(n)
+//  Runtime: 40 ms
+//  Memory Usage: 15.1 MB
 //
 //  Created by gunhyeong on 2020/12/24.
 //
@@ -25,6 +27,24 @@ import Foundation
 
 class Solution {
     func maxDepth(_ root: Node?) -> Int {
+        var depth = 0
         
+        if root == nil { return depth }
+        
+        var stack = [Node]()
+        stack.append(root!)
+        
+        while !stack.isEmpty {
+            depth += 1
+            var nodes = [Node]()
+            
+            for val in stack {
+                nodes.append(constenetOf: val.children)
+            }
+            
+            stack = nodes
+        }
+        
+        return depth
     }
 }
