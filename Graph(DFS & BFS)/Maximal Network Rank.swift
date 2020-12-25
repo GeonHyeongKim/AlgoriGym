51dfd5dec4a5ab09113dbc7db6d9cdf9f28210d9
김건형 <show11837@gmail.com>
2020-12-25 18:54:26 +0900

    finish : Maximal Network Rank 문제 풀이 끝 (Time&Space Complexity, Runtime, Memory Usage)

--- Graph(DFS & BFS)/Maximal Network Rank.swift
+++ Graph(DFS & BFS)/Maximal Network Rank.swift
@@ -3,8 +3,8 @@
 //  Question Link: https://leetcode.com/problems/maximal-network-rank/
 //  Primary idea:   1.
 
-//  Time Complexity :
-//  Space Complexity :
+//  Time Complexity : O(n^2)
+//  Space Complexity : O(n^2)
 //  Runtime:  ms
 //  Memory Usage:  MB
 //
@@ -15,6 +15,25 @@ import Foundation
 
 class Solution {
     func maximalNetworkRank(_ n: Int, _ roads: [[Int]]) -> Int {
+        var dict = [Int: Set<Int>]()
+        var res = 0
+        for i in roads {
+            dict[i[0], default: []].insert(i[1])
+            dict[i[1], default: []].insert(i[0])
+        }
         
+        for i in 0...n {
+            for j in 0...n where j != i {
+                let first = dict[i] ?? []
+                let second = dict[j] ?? []
+                var total = first.count + second.count
+                if first.contains(j) && second.contains(i) {
+                    total -= 1
+                }
+                res = max(res, total)
+            }
+        }
+        
+        return res
     }
 }
