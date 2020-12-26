d970a4769ea76fef072ad3d96fb7a16f5ff032d4
김건형 <show11837@gmail.com>
2020-12-26 21:49:57 +0900

    feat : Validate Binary Tree Nodes 문제 풀이 시작

---
+++ Graph(DFS & BFS)/Validate Binary Tree Nodes.swift
@@ -0,0 +1,18 @@
+//
+//  Validate Binary Tree Nodes.swift
+//  Question Link: https://leetcode.com/problems/validate-binary-tree-nodes/
+//  Primary idea:   1.
+//
+//  Time Complexity:
+//  Space Complexity :
+//
+//  Created by gunhyeong on 2020/12/26.
+//
+
+import Foundation
+
+class Solution {
+    func validateBinaryTreeNodes(_ n: Int, _ leftChild: [Int], _ rightChild: [Int]) -> Bool {
+        
+    }
+}
