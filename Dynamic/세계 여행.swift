c70986583009a5844f5334f3a19c326596d2f951
김건형 <show11837@gmail.com>
2021-01-21 11:21:55 +0900

    finish : 세계 여행 문제 풀이 끝 (Primary idea, Time&Space Complexity)
    [Primary idea] : 동적 계획법
    1. 완전탐색(O(n!))으로는 시간이 부족할 것이다 -> 동적계획법
    2. 요약 : 모든 나라를 한 번씩 방문하고 1번 나라로 돌아오는데 걸리는 최단 시간
    
    Time Complexity : O(n^n)
    Space Complexity : O(n^2)

--- Dynamic/세계 여행.swift
+++ Dynamic/세계 여행.swift
@@ -20,14 +20,44 @@
     [출력]
     홍현이가 개의 국가를 모두 방문하고 출발했던 국가로 돌아오는 데 걸리는 최소 시간을 출력한다.
 
+    [Primary idea] : 동적 계획법
+    1. 완전탐색(O(n!))으로는 시간이 부족할 것이다 -> 동적계획법
+    2. 요약 : 모든 나라를 한 번씩 방문하고 1번 나라로 돌아오는데 걸리는 최단 시간
 
-    [Primary idea] :
- 
-
-    Time Complexity :
-    Space Complexity :
+    Time Complexity : O(n^n)
+    Space Complexity : O(n^2)
 
     Created by gunhyeong on 2021/01/19.
 */
 
 import Foundation
+
+let n = Int(readLine()!)! // 방문하고자 하는 국가의 수
+var route = [[Int]]()
+
+// input
+for _ in 0..<n {
+    let row = readLine()!.split(separator: " ").map{Int($0)!}
+    route.append(row)
+}
+var d = [[Int]](repeating: [Int](repeating: -1, count: 16), count: 1<<16)
+let inf = 1_000_000_000
+
+print(TSF(0, 1))
+
+func TSF(_ cur: Int, _ stat: Int) -> Int {
+    if stat == (1<<n - 1) { //모든 지역을 방문한 경우
+        return route[cur][0] != 0 ? route[cur][0] : inf // 현재 지역에서 출발지(0)로 이동 가능한지
+    }
+    
+    if d[stat][cur] != -1 { return d[stat][cur] } // 만약 답이 이미 메모되어있으면 답을 리턴
+    d[stat][cur] = inf
+    
+    for i in 0..<n {
+        if (stat & (1<<i)) == 0 && route[cur][i] != 0 { // i번째 지역을 방문한적이 없고, 이동 가능하면
+            d[stat][cur] = min(d[stat][cur], TSF(i, stat | (1<<i)) + route[cur][i]) // i번째 지역 방문체크 후 이동
+        }
+    }
+    
+    return d[stat][cur]
+}
