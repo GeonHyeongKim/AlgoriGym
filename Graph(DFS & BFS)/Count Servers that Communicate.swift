5adeab95678e97c0400e7cc954b0d735c6d7e4de
김건형 <show11837@gmail.com>
2021-04-20 23:54:04 +0900

    finish : Count Servers that Communicate 문제 풀이 끝
    2번째 풀이로 개선됌
    Runtime: 772 ms, faster than 9.09% of Swift online submissions for Count Servers that Communicate.
    Memory Usage: 16.9 MB, less than 18.18% of Swift online submissions for Count Servers that Communicate

--- Graph(DFS & BFS)/Count Servers that Communicate.swift
+++ Graph(DFS & BFS)/Count Servers that Communicate.swift
@@ -55,3 +55,43 @@ class Solution {
         return (i < 0 || i >= row || j < 0 || j >= col) ? true : false
     }
 }
+
+// 2번째 풀이
+//Runtime: 772 ms, faster than 9.09% of Swift online submissions for Count Servers that Communicate.
+//Memory Usage: 16.9 MB, less than 18.18% of Swift online submissions for Count Servers that Communicate.
+
+class Solution {
+    func countServers(_ grid: [[Int]]) -> Int {
+        var grid = grid
+        let row = grid.count
+        let col = grid[0].count
+        var connect = 0
+        var res = 0
+        
+        func isOutOfSize(_ i: Int, _ j: Int) -> Bool {
+            return (i < 0 || i >= row || j < 0 || j >= col)
+        }
+        
+        func dfs(_ i: Int, _ j: Int) {
+            if isOutOfSize(i, j) || grid[i][j] != 1 { return }
+            
+            grid[i][j] -= 1
+            connect += 1
+            
+            for x in 0..<row { dfs(x, j) }
+            for y in 0..<col { dfs(i, y) }
+        }
+        
+        for i in 0..<row {
+            for j in 0..<col {
+                if grid[i][j] == 1 {
+                    connect = 0
+                    dfs(i, j)
+                    if connect > 1 { res += connect}
+                }
+            }
+        }
+        
+        return res
+    }
+}
